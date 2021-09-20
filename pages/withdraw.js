2c539cfcd482183c7a02395f7071c07a37c060a0
Andrew <alo42@georgetown.edu>
2021-09-20 12:40:15 +0200

    adds input and state for issueUrl withdraw

--- pages/withdraw.js
+++ pages/withdraw.js
@@ -3,20 +3,17 @@ import axios from "axios";
 import AuthContext from "../store/AuthStore/AuthContext";
 
 function Withdraw() {
+    const [issueUrl, setIssueUrl] = useState("");
     const [authState, setAuthState] = useContext(AuthContext);
 
-    useEffect(() => {
-        console.log(authState);
+    const withdrawBounty = (event) => {
+        event.preventDefault();
+        console.log(issueUrl);
         const token = window.localStorage.getItem('token');
-        withdrawBounty(token);
-    }, []);
-
-    const withdrawBounty = (token) => {
-        console.log(token);
         axios.post(`http://localhost:8090/withdraw`, {
             username: "alo9507",
-            issueId: "sdf",
-            payoutAddress: "sdf",
+            issueId: "I_kwDOGAqhQc47ptzS",
+            payoutAddress: "0xDf9aF175CE0BB59bcAC9CbD965Cd46cfd9806277",
             oauthToken: token
         })
             .then((response) => {
@@ -28,8 +25,20 @@ function Withdraw() {
     };
 
     return (
-        <div>
-            Withdraw
+        <div className="font-mont bg-gray-100 font-normal text-gray-600">
+            <form onSubmit={(event) => withdrawBounty(event)}>
+                <input
+                    className="bg-gray-100 w-6/7 border-gray-100 outline-none"
+                    id="name"
+                    placeholder="https://github.com/OpenQDev/frontend/issues/3"
+                    type="text"
+                    onChange={(event) => setIssueUrl(event.target.value)}
+                />
+                <button type="submit">Withdraw</button>
+            </form>
+            <div>
+                {issueUrl}
+            </div>
         </div>
     );
 }
