9786aa660b6edbf21b8c1b3f89cf2e95c38b2bde
Andrew <aobrien@wolfram.com>
2021-09-29 18:01:52 +0200

    gets issueAddresses

--- components/BountyCardList.js
+++ components/BountyCardList.js
@@ -6,66 +6,54 @@ import { ethers } from 'ethers';
 const BountyCardList = () => {
   const [appState, dispatch] = useContext(StoreContext);
   const [issueIds, setIssueIds]  = useState([])
+  const [issueIdToAddress, setIssueIdToAddress]  = useState({})
 
   const bountyData = [
     {repoName: "uniswap/uniswap", issueName: "Issue name"},
     {repoName: "uniswap/uniswap2", issueName: "Issue name2"},
   ]
 
-  const issueIdToAddress = {}
-
   async function getAllIssues() {
     if (typeof window.ethereum !== 'undefined') {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(appState.openQAddress, appState.OpenQ.abi, provider);
       try {
-        const issueIds = await contract.getIssueIds();
-        setIssueIds(issueIds)
+        const allIssueIds = await contract.getIssueIds();
+        return allIssueIds
       } catch (err) {
         console.log("getAllIssues Error: ", err);
       }
     }
   }
 
-  async function getIssueAddresses() {
+  async function getIssueAddresses(issues) {
     if (typeof window.ethereum !== 'undefined') {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(appState.openQAddress, appState.OpenQ.abi, provider);
+      const issueIdToAddress = {}
       try {
-        issueIds.foreach(async (issueId) => {
+        for(const issueId of issues) {
           const issueAddress = await contract.issueToAddress(issueId);
           issueIdToAddress[issueId] = issueAddress
-        })
-        console.log(JSON.stringify(issueIdToAddress))
+        }
+        return issueIdToAddress
       } catch (err) {
         console.log("getIssueAddresses Error: ", err);
       }
     }
   }
 
-  // async function getIssueAddresses() {
-  //   if (typeof window.ethereum !== 'undefined') {
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     const contract = new ethers.Contract(appState.openQAddress, appState.OpenQ.abi, provider);
-  //     try {
-  //       for (entry in issueToAddress) {
-  //         appState.tokenAddresses.forEach(tokenAddress => {
-  //           const issueAddress = await contract.issueToAddress(issueId);
-  //         });
-  //       }
-  //     } catch (err) {
-  //       console.log("getIssueAddresses Error: ", err);
-  //     }
-  //   }
-  // }
-
   useEffect(() =>{
     async function populateBountyData() {
-      await getAllIssues()
-      await getIssueAddresses()
-      // await getIssueBalances()
+      const issues = await getAllIssues()
+      setIssueIds(issues)
+
+      const issueIdToAddresses = await getIssueAddresses(issues)
+      setIssueIdToAddress(issueIdToAddress)
+      
+      console.log(issueIdToAddresses)
     }
-    populateBountyData
+    populateBountyData()
   }, [])
 
   return (
