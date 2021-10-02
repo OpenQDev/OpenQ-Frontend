7e44ee059606ecb4a21e9204d81dff2ad3658b27
Andrew <alo42@georgetown.edu>
2021-10-02 22:35:18 +0200

    refactors abi to js class generator

--- services/utils/abiToJavascriptClass.js
+++ services/utils/abiToJavascriptClass.js
@@ -1,39 +1,53 @@
-const OpenQ = require("../../artifacts/contracts/OpenQ.sol/OpenQ.json");
+const OpenQ = require("../../artifacts/contracts/Issue.sol/Issue.json");
 
 let functionCount = 0;
 let evenCount = 0;
 
+function extractInputNames(inputs) {
+    const inputNames = [];
+    if (inputs) {
+        for (input of inputs) {
+            let inputName = "";
+
+            if (input.name) {
+                inputName = input.name;
+            } else {
+                inputName = input.type;
+            }
+
+            inputNames.push(inputName);
+        }
+    }
+    return inputNames;
+}
+
 for (member of OpenQ.abi) {
+    let memberType = "";
+    let name = "";
+    let inputNames = [];
+
     switch (member.type) {
         case "constructor":
+            memberType = member.type;
+            inputNames = extractInputNames(member.inputs);
             break;
         case "function":
             functionCount += 1;
-            const name = member.name;
-            const inputNames = [];
-            const inputs = member.inputs;
-
-            if (inputs) {
-                for (input of inputs) {
-                    let inputName = "";
-
-                    if (input.name) {
-                        inputName = input.name;
-                    } else {
-                        inputName = input.type;
-                    }
-
-                    inputNames.push(inputName);
-                }
-            }
-            console.log(`function ${name}(${inputNames.toString()}) {}`);
+            memberType = member.type;
+            name = member.name;
+            inputNames = extractInputNames(member.inputs);
             break;
         case "event":
             evenCount += 1;
+            memberType = member.type;
+            name = member.name;
+            inputNames = extractInputNames(member.inputs);
             break;
         default:
             break;
     }
+
+    console.log(`${memberType} ${name}(${inputNames.toString()}) {}`);
 }
 
 console.log("Function count: ", functionCount);
