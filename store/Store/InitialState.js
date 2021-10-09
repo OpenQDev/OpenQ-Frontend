5ab173f7c412f368824b50a29c08cff1df7c33e7
Andrew <alo42@georgetown.edu>
2021-10-09 10:05:41 -0400

    dockerfile edits

--- store/Store/InitialState.js
+++ store/Store/InitialState.js
@@ -43,7 +43,23 @@ switch (process.env.DEPLOY_ENV) {
             clientId: "5fbd39c6916b7efb63cc"
         };
         break;
+<<<<<<< Updated upstream
     case "development":
+=======
+    case "docker":
+        InitialState = {
+            githubRepository: new GithubRepository(),
+            publicAddress: "",
+            utils: new Utils(),
+            tokenAddresses: [process.env.FAKE_TOKEN_ADDRESS, process.env.MOCK_TOKEN_ADDRESS],
+            openQAddress: process.env.OPENQ_ADDRESS,
+            openQClient: new OpenQClient(),
+        };
+        break;
+    // Booting mock will spin up A) an Ethnode B) a frontend to which we inject all mocks
+    case "mock":
+        console.log(process.env.FAKE_TOKEN_ADDRESS);
+>>>>>>> Stashed changes
         InitialState = {
             githubRepository: new GithubRepository(),
             publicAddress: "",
