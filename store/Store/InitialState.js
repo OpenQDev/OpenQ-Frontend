f429659cfa80d97094f0fdcba03100f4996b6cf5
Andrew <alo42@georgetown.edu>
2021-10-09 10:52:27 -0400

    adds docker env to initial state

--- store/Store/InitialState.js
+++ store/Store/InitialState.js
@@ -8,9 +8,7 @@ import { ethers } from 'ethers';
 
 let InitialState = {};
 switch (process.env.DEPLOY_ENV) {
-    // Booting mock will spin up A) an Ethnode B) a frontend to which we inject all mocks
     case "mock":
-        console.log(process.env.FAKE_TOKEN_ADDRESS);
         InitialState = {
             githubRepository: new MockGithubRepository(),
             publicAddress: "",
@@ -26,8 +24,7 @@ switch (process.env.DEPLOY_ENV) {
             clientId: "doesntmatter"
         };
         break;
-    // Booting local will sping up A) an Ethnode B) deploy contracts C) boot API D) boot frontend to point to all three of the former
-    case "local":
+    case "docker":
         InitialState = {
             githubRepository: new GithubRepository(),
             publicAddress: "",
@@ -43,10 +40,7 @@ switch (process.env.DEPLOY_ENV) {
             clientId: "5fbd39c6916b7efb63cc"
         };
         break;
-<<<<<<< Updated upstream
     case "development":
-=======
-    case "docker":
         InitialState = {
             githubRepository: new GithubRepository(),
             publicAddress: "",
@@ -54,25 +48,12 @@ switch (process.env.DEPLOY_ENV) {
             tokenAddresses: [process.env.FAKE_TOKEN_ADDRESS, process.env.MOCK_TOKEN_ADDRESS],
             openQAddress: process.env.OPENQ_ADDRESS,
             openQClient: new OpenQClient(),
-        };
-        break;
-    // Booting mock will spin up A) an Ethnode B) a frontend to which we inject all mocks
-    case "mock":
-        console.log(process.env.FAKE_TOKEN_ADDRESS);
->>>>>>> Stashed changes
-        InitialState = {
-            githubRepository: new GithubRepository(),
-            publicAddress: "",
-            utils: new Utils(),
-            tokenAddresses: [process.env.FAKE_TOKEN_ADDRESS, process.env.MOCK_TOKEN_ADDRESS],
-            openQAddress: process.env.OPENQ_ADDRESS,
-            openQClient: new OpenQClient(),
-            baseUrl: "https://development.openq.dev",
-            frontendPort: "",
-            oauthPort: "",
-            apiPort: "",
-            githubOAuthPath: "auth",
-            clientId: "82e208319d33d8a6f6b8"
+            baseUrl: "http://localhost",
+            frontendPort: ":3000",
+            oauthPort: ":3001",
+            apiPort: ":8090",
+            githubOAuthPath: "",
+            clientId: "5fbd39c6916b7efb63cc"
         };
         break;
     case "staging":
