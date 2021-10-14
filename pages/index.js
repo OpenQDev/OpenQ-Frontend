42c50ca9738fbb8d26b5bd26e88e76f7127f9680
Andrew <alo42@georgetown.edu>
2021-10-14 19:13:39 -0400

    comments out auth code

--- pages/index.js
+++ pages/index.js
@@ -16,23 +16,23 @@ import { ApolloProvider } from "@apollo/client";
 import ProfilePicture from "../components/ProfilePicture";
 import React from "react";
 import axios from "axios";
-import StoreContext from "../store/Store/StoreContext";
+// import StoreContext from "../store/Store/StoreContext";
 
 export default function Home() {
-  const [appState, dispatch] = useContext(StoreContext);
+  // const [appState, dispatch] = useContext(StoreContext);
 
-  useEffect(() => {
-    async function checkAuth() {
-      axios.get(`${appState.baseUrl}${appState.oauthPort}/${appState.githubOAuthPath}/checkAuth`, { withCredentials: true })
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-    checkAuth();
-  }, []);
+  // useEffect(() => {
+  //   async function checkAuth() {
+  //     axios.get(`${appState.baseUrl}${appState.oauthPort}/${appState.githubOAuthPath}/checkAuth`, { withCredentials: true })
+  //       .then((res) => {
+  //         console.log(res);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   }
+  //   checkAuth();
+  // }, []);
 
   return (
     <div>
