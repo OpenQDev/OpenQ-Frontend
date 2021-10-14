39d0842fc5a170765b973291ce280f062ddfeffc
Andrew <alo42@georgetown.edu>
2021-10-14 19:53:40 -0400

    metamask onboarding

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -1,49 +1,56 @@
 import React, { useState, useContext, useEffect } from "react";
 import StoreContext from "../store/Store/StoreContext";
 import { ethers } from "ethers";
+import MetaMaskOnboarding from '@metamask/onboarding';
 
 const ConnectWallet = () => {
-  const [appState, dispatch] = useContext(StoreContext);
-  const [isConnected, setIsConnected] = useState(false);
-  const [isDeciding, setIsDeciding] = useState(false);
+  const [buttonText, setButtonText] = useState("");
+  const [isDisabled, setIsDisabled] = useState("");
+  const [onClickHandler, setOnClickHandler] = useState(() => () => console.log("default"));
 
-  const isMetaMaskConnected = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const accounts = await provider.listAccounts();
-    return accounts.length > 0;
-  };
-
-  useEffect(() => {
-    async function checkIsConnected() {
-      const mm = await isMetaMaskConnected();
-      console.log(mm);
-      setIsConnected(mm);
-    }
-    checkIsConnected();
-  }, []);
-
-  const hasMetamask = () => {
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
     const { ethereum } = window;
     return Boolean(ethereum && ethereum.isMetaMask);
   };
 
-  const requestAccounts = async () => {
-    setIsDeciding(true);
-    const hasMetamask = checkForMetamask();
-    const address = await window.ethereum.request({ method: "eth_requestAccounts" });
-    dispatch({ type: "UPDATE_ADDRESS", payload: address[0] });
-    setIsConnected(true);
-    console.log(appState.publicAddress);
+  //------Inserted Code------\\
+  const metaMaskClientCheck = () => {
+    //Now we check to see if MetaMask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      setButtonText('Click here to install MetaMask!');
+      setOnClickHandler(() => () => onClickInstall());
+      setIsDisabled(false);
+    } else {
+      //If it is installed we change our button text
+      setButtonText('Connect Wallet');
+    }
   };
 
+  //This will start the onboarding proccess
+  const onClickInstall = () => {
+    //We create a new MetaMask onboarding object to use in our app
+    const onboarding = new MetaMaskOnboarding({ forwarderOrigin: "http://localhost:3000" });
+    setButtonText('Onboarding in progress');
+    setIsDisabled(true);
+    //On this object we have startOnboarding which will start the onboarding process for our end user
+    onboarding.startOnboarding();
+  };
+
+  useEffect(() => {
+    metaMaskClientCheck();
+  }, []);
+
   return (
-    <div>
-      {(isConnected && !isDeciding) ? null : <button
-        onClick={requestAccounts}
-        className="font-mont rounded-lg border-2 border-gray-300 py-2 px-3 text-base font-bold cursor-pointer"
-      >
-        Connect Wallet
-      </button>}
+    <div><button
+      disabled={isDisabled}
+      onClick={onClickHandler}
+      className="font-mont rounded-lg border-2 border-gray-300 py-2 px-3 text-base font-bold cursor-pointer"
+    >
+      {buttonText}
+    </button>
     </div>
   );
 };
