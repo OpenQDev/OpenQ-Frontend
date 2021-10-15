94da0368fa147cc3fbf170221c08093e166415f0
Andrew <alo42@georgetown.edu>
2021-10-14 20:16:57 -0400

    connect or install depending on isMetamaskInstalled

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -4,54 +4,81 @@ import { ethers } from "ethers";
 import MetaMaskOnboarding from '@metamask/onboarding';
 
 const ConnectWallet = () => {
-  const [buttonText, setButtonText] = useState("");
-  const [isDisabled, setIsDisabled] = useState("");
-  const [onClickHandler, setOnClickHandler] = useState(() => () => console.log("default"));
+  const [isMetaMaskInstalled, setIsMetaMaskInstalled] = useState(false);
 
   //Created check function to see if the MetaMask extension is installed
-  const isMetaMaskInstalled = () => {
+  const metaMaskClientCheck = () => {
     //Have to check the ethereum binding on the window object to see if it's installed
     const { ethereum } = window;
     return Boolean(ethereum && ethereum.isMetaMask);
   };
 
-  //------Inserted Code------\\
-  const metaMaskClientCheck = () => {
-    //Now we check to see if MetaMask is installed
-    if (!isMetaMaskInstalled()) {
-      //If it isn't installed we ask the user to click to install it
-      setButtonText('Click here to install MetaMask!');
-      setOnClickHandler(() => () => onClickInstall());
-      setIsDisabled(false);
-    } else {
-      //If it is installed we change our button text
-      setButtonText('Connect Wallet');
+  const ConnectButton = () => {
+    const [buttonText, setButtonText] = useState("Connect Wallet");
+
+    const onClickConnect = async () => {
+      try {
+        // Will open the MetaMask UI
+        // You should disable this button while the request is pending!
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return (
+      <button
+        onClick={onClickConnect}
+        className="font-mont rounded-lg border-2 border-gray-300 py-2 px-3 text-base font-bold cursor-pointer"
+      >
+        {buttonText}
+      </button>
+    );
+  };
+
+  const InstallButton = () => {
+    const [buttonText, setButtonText] = useState("Install MetaMask!");
+
+    //This will start the onboarding proccess
+    const onClickInstall = () => {
+      //We create a new MetaMask onboarding object to use in our app
+      const onboarding = new MetaMaskOnboarding({ forwarderOrigin: "http://localhost:3000" });
+      setButtonText('Onboarding in progress');
+      //On this object we have startOnboarding which will start the onboarding process for our end user
+      onboarding.startOnboarding();
+    };
+
+    return (
+      <button
+        onClick={onClickInstall}
+        className="font-mont rounded-lg border-2 border-gray-300 py-2 px-3 text-base font-bold cursor-pointer"
+      >
+        {buttonText}
+      </button>
+    );
+  };
+
+  const onClickConnect = async () => {
+    try {
+      // Will open the MetaMask UI
+      // You should disable this button while the request is pending!
+      // setIsDisabled(true);
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      // setIsDisabled(false);
+      // setShowButton(false);
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  //This will start the onboarding proccess
-  const onClickInstall = () => {
-    //We create a new MetaMask onboarding object to use in our app
-    const onboarding = new MetaMaskOnboarding({ forwarderOrigin: "http://localhost:3000" });
-    setButtonText('Onboarding in progress');
-    setIsDisabled(true);
-    //On this object we have startOnboarding which will start the onboarding process for our end user
-    onboarding.startOnboarding();
-  };
-
   useEffect(() => {
-    metaMaskClientCheck();
+    setIsMetaMaskInstalled(metaMaskClientCheck());
   }, []);
 
   return (
-    <div><button
-      disabled={isDisabled}
-      onClick={onClickHandler}
-      className="font-mont rounded-lg border-2 border-gray-300 py-2 px-3 text-base font-bold cursor-pointer"
-    >
-      {buttonText}
-    </button>
-    </div>
+    <>
+      {isMetaMaskInstalled ? <ConnectButton /> : <InstallButton />}
+    </>
   );
 };
 
