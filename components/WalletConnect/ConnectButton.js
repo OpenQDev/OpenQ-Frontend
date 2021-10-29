1aa95ee692756c3663f21de48da4d71bfb117d18
Andrew <alo42@georgetown.edu>
2021-10-29 12:32:14 -0500

    uses web3react

--- components/WalletConnect/ConnectButton.js
+++ components/WalletConnect/ConnectButton.js
@@ -1,21 +1,42 @@
 import React, { useState, useContext, useEffect } from "react";
+import { ethers } from "ethers";
+import { Web3ReactProvider, useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
+import {
+    NoEthereumProviderError,
+    UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from '@web3-react/injected-connector';
+import { Web3Provider } from '@ethersproject/providers';
+import {
+    injected
+} from './connectors';
 
 const ConnectButton = () => {
+    const { connector, library, chainId, account, activate, deactivate, active, error } = useWeb3React();
 
-    async function connect() {
+    const [buttonText, setButtonText] = useState("Connect Wallet");
+    const [isDisabled, setIsDisabled] = useState(false);
 
-    }
+    const onClickConnect = async () => {
+        try {
+            setButtonText("Connecting...");
+            setIsDisabled(true);
 
-    async function disconnect() {
+            await activate(injected);
 
-    }
+            setIsDisabled(false);
+        } catch (error) {
+            setButtonText("Connect Wallet");
+            setIsDisabled(false);
+        }
+    };
 
     return (
         <button
-            onClick={connect}
+            disabled={isDisabled}
+            onClick={onClickConnect}
             className="font-mont rounded-lg border-2 border-gray-300 py-2 px-3 text-base font-bold cursor-pointer"
         >
-            Connect Wallet
+            {buttonText}
         </button>
     );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,25 @@ import Navbar from "../components/Navbar";
 import StoreProvider from "../store/Store/StoreProvider";
 import AuthProvider from "../store/AuthStore/AuthProvider";
 import Layout from "../components/Layout";
+import { Web3ReactProvider } from '@web3-react/core';
+import { ethers } from 'ethers';
 
 function MyApp({ Component, pageProps }) {
+
+  function getLibrary(provider) {
+    const library = new ethers.providers.Web3Provider(provider);
+    library.pollingInterval = 12000;
+    return library;
+  }
 
   return (
     <AuthProvider>
       <StoreProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Web3ReactProvider>
       </StoreProvider>
     </AuthProvider>
   );
