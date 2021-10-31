dfe2a3ef25aacf8d381c0698ae897513f83df17f
FlacoJones <andrew@openq.dev>
2021-10-31 16:55:49 -0500

    adds NEXT_PUBLIC

--- services/ethers/OpenQClient.js
+++ services/ethers/OpenQClient.js
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers';
 import OpenQABI from '../../artifacts/contracts/OpenQ.sol/OpenQ.json';
 import ERC20ABI from '../../artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json';
-import process from "process";
 
 class OpenQClient {
     constructor() { }
