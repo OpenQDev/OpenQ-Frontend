cb47177218629a9eb66bf7f5b7aa5e918722a393
FlacoJones <andrew@openq.dev>
2021-10-31 13:29:37 -0500

    updates supported chain ids

--- components/WalletConnect/connectors.js
+++ components/WalletConnect/connectors.js
@@ -1,3 +1,3 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 
-export const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42, 31337] });
+export const injected = new InjectedConnector({ supportedChainIds: [80001, 137, 31337] });
