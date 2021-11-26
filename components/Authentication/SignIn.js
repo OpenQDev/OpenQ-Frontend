de788689a73e281b10515713125932685eb47af0
rikkdev <l.riccardo@web.de>
2021-11-26 22:19:02 +0100

    modified claim page view

--- components/Authentication/SignIn.js
+++ components/Authentication/SignIn.js
@@ -1,28 +1,39 @@
 // Third Party
-import React, { useContext } from 'react';
-import { useRouter } from 'next/router';
+import React, { useContext } from "react";
+import { useRouter } from "next/router";
 // Custom
-import StoreContext from '../../store/Store/StoreContext';
+import StoreContext from "../../store/Store/StoreContext";
+import Image from "next/image";
 
 const SignIn = () => {
-	const [appState,] = useContext(StoreContext);
-	const router = useRouter();
+  const [appState] = useContext(StoreContext);
+  const router = useRouter();
 
-	const signIn = () => {
-		console.log(appState);
-		const clientId = `client_id=${process.env.NEXT_PUBLIC_OPENQ_ID}`;
-		const scopes = 'scope=read:user%20public_repo';
-		router.push(`https://github.com/login/oauth/authorize?${clientId}&${scopes}`);
-	};
+  const signIn = () => {
+    console.log(appState);
+    const clientId = `client_id=${process.env.NEXT_PUBLIC_OPENQ_ID}`;
+    const scopes = "scope=read:user%20public_repo";
+    router.push(
+      `https://github.com/login/oauth/authorize?${clientId}&${scopes}`
+    );
+  };
 
-	return (
-		<button
-			onClick={() => signIn()}
-			className="font-mont rounded-lg border-2 border-gray-300 py-2 px-3 text-base font-bold cursor-pointer"
-		>
-			Sign In
-		</button>
-	);
+  return (
+    <button
+      onClick={() => signIn()}
+      className="flex flex-row justify-items-center pb-1 space-x-2 font-mont rounded-lg border-2 border-gray-300 py-2 px-3 text-base font-bold cursor-pointer"
+    >
+      <div className="">
+        <Image
+          src="/BountyMaterial/github.png"
+          alt="Picture of the author"
+          width={20}
+          height={20}
+        />
+      </div>
+      <div>Sign In</div>
+    </button>
+  );
 };
 
 export default SignIn;
