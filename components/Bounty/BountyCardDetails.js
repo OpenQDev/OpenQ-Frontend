7cfc86eb00643c2fb75c73531bc67219dcf8b031
rikkdev <l.riccardo@web.de>
2021-12-15 10:52:00 +0100

    added back modal for bountyCard in bountyHomepage

--- components/Bounty/BountyCardDetails.js
+++ components/Bounty/BountyCardDetails.js
@@ -1,44 +1,48 @@
 // Third Party
-import React from 'react';
-import BountyTokenBalances from '../BountyTokenBalances/BountyTokenBalances';
+import React from "react";
+import BountyTokenBalances from "../BountyTokenBalances/BountyTokenBalances";
 
 // Custom
-import BountyCardHeader from './BountyCardHeader';
-import BountyContributors from './BountyContributors';
-import BountyLinks from './BountyLinks';
-import BountyStatus from './BountyStatus';
-import CopyBountyAddress from './CopyBountyAddress';
-import LabelsList from './LabelsList';
+import BountyCardHeader from "./BountyCardHeader";
+import BountyContributors from "./BountyContributors";
+import BountyLinks from "./BountyLinks";
+import BountyStatus from "./BountyStatus";
+import CopyBountyAddress from "./CopyBountyAddress";
+import LabelsList from "./LabelsList";
 
 const BountyCardDetails = ({ bounty, tokenValues }) => {
-	return (
-		<div className="flex flex-col font-mont pl-16 pr-16 pt-10 pb-10">
-			<div className="flex flex-col border-b border-solid rounded-t">
-				<BountyCardHeader bounty={bounty} />
-				<div className="flex flex-row pt-5 space-x-10 justify-between">
-					<div className="flex flex-col">
-						<BountyStatus bounty={bounty} />
-					</div>
+  return (
+    <div className="flex flex-col font-mont pl-16 pr-16 pt-10 pb-10">
+      <div className="flex flex-col border-b border-solid rounded-t">
+        <BountyCardHeader bounty={bounty} />
+        <div className="flex flex-row pt-5 space-x-10 justify-between">
+          <div className="flex flex-col">
+            <BountyStatus bounty={bounty} />
+          </div>
 
-					<CopyBountyAddress bounty={bounty} />
-				</div>
-				<LabelsList bounty={bounty} />
-				{bounty.deposits.length != 0 ? <BountyTokenBalances bounty={bounty} tokenValues={tokenValues} /> : 'No deposits.'}
-			</div>
-			<div className="flex flex-col pt-5">
-				<div className="flex flex-row justify-between">
-					<div className="font-bold text-xl">Description</div>
-					<BountyLinks bounty={bounty} />
-				</div>
-				<div dangerouslySetInnerHTML={{ __html: bounty.bodyHTML }}></div>
-			</div>
-			<div className="flex flex-col pt-5">
-				<div className="flex flex-row justify-between">
-					<BountyContributors bounty={bounty} />
-				</div>
-			</div>
-		</div>
-	);
+          <CopyBountyAddress bounty={bounty} />
+        </div>
+        <LabelsList bounty={bounty} />
+        {bounty.deposits.length != 0 ? (
+          <BountyTokenBalances bounty={bounty} tokenValues={tokenValues} />
+        ) : (
+          "No deposits."
+        )}
+      </div>
+      <div className="flex flex-col pt-5">
+        <div className="flex flex-row justify-between">
+          <div className="font-bold text-xl">Description</div>
+          <BountyLinks bounty={bounty} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: bounty.bodyHTML }}></div>
+      </div>
+      <div className="flex flex-col pt-5">
+        <div className="flex flex-row justify-between">
+          <BountyContributors bounty={bounty} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BountyCardDetails;
--- components/Bounty/BountyStatus.js
+++ components/Bounty/BountyStatus.js
@@ -1,39 +1,39 @@
 // Third Party
-import React, { useContext } from 'react';
-import StoreContext from '../../store/Store/StoreContext';
+import React, { useContext } from "react";
+import StoreContext from "../../store/Store/StoreContext";
 
 // Custom
 
 const BountyStatus = (props) => {
-	const { bounty } = props;
-	const [appState,] = useContext(StoreContext);
+  const { bounty } = props;
+  const [appState] = useContext(StoreContext);
 
-	return (
-		<>
-			<div className="font-bold">Status</div>
-			<div className="flex flex-row space-x-2 pt-2">
-				<div className="pt-1">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill={bounty.closed ? '#F0431D' : '#15FB31'}
-						viewBox="0 0 16 16"
-						width="15"
-						height="15"
-					>
-						<path d="M8 9.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-						<path
-							fillRule="evenodd"
-							d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z"
-						></path>
-					</svg>
-				</div>
-				<div className="flex space-x-1">
-					<div>{bounty.status == 'OPEN' ? 'Unclaimed' : 'Claimed'}</div>
-					<div>{appState.utils.formatDate(bounty.createdAt)}</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="font-bold">Status</div>
+      <div className="flex flex-row space-x-2 pt-2">
+        <div className="pt-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill={bounty.closed ? "#F0431D" : "#15FB31"}
+            viewBox="0 0 16 16"
+            width="15"
+            height="15"
+          >
+            <path d="M8 9.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+            <path
+              fillRule="evenodd"
+              d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z"
+            ></path>
+          </svg>
+        </div>
+        <div className="flex space-x-1">
+          <div>{bounty.status == "OPEN" ? "Unclaimed" : "Claimed"}</div>
+          <div>{appState.utils.formatDate(bounty.createdAt)}</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BountyStatus;
