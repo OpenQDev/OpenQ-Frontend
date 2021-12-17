52c8ecec1db2973bdb03906a78d3620c781e5ece
rikkdev <l.riccardo@web.de>
2021-12-17 14:52:17 +0100

    re-designed search boxes in bountyHomepage

--- components/Bounty/BountyHomepage.js
+++ components/Bounty/BountyHomepage.js
@@ -1,80 +1,98 @@
 // Third Party
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext } from "react";
 // Custom
-import BountyCard from './BountyCard';
-import StoreContext from '../../store/Store/StoreContext';
+import BountyCard from "./BountyCard";
+import StoreContext from "../../store/Store/StoreContext";
 
 const BountyHomepage = () => {
-	// State
-	const [bounties, setBounties] = useState([]);
-	const [organizationSearchTerm, setOrganizationSearchTerm] = useState('');
-	const [issueTitleSearchTerm, setIssueTitleSearchTerm] = useState('');
-	const [isLoading, setIsLoading] = useState(true);
+  // State
+  const [bounties, setBounties] = useState([]);
+  const [organizationSearchTerm, setOrganizationSearchTerm] = useState("");
+  const [issueTitleSearchTerm, setIssueTitleSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-	// Context
-	const [appState,] = useContext(StoreContext);
+  // Context
+  const [appState] = useContext(StoreContext);
 
-	// Hooks
-	useEffect(() => {
-		populateBountyData();
-	}, []);
+  // Hooks
+  useEffect(() => {
+    populateBountyData();
+  }, []);
 
-	// Methods
-	async function populateBountyData() {
-		setIsLoading(true);
+  // Methods
+  async function populateBountyData() {
+    setIsLoading(true);
 
-		const bounties = await appState.openQSubgraphClient.getAllBounties();
+    const bounties = await appState.openQSubgraphClient.getAllBounties();
 
-		const bountyIds = bounties.map(bounty => bounty.bountyId);
-		const issueData = await appState.githubRepository.getIssueData(bountyIds);
+    const bountyIds = bounties.map((bounty) => bounty.bountyId);
+    const issueData = await appState.githubRepository.getIssueData(bountyIds);
 
-		const fullBounties = [];
-		bounties.forEach(bounty => {
-			const relatedIssue = issueData.find(issue => issue.id == bounty.bountyId);
-			const mergedBounty = { ...bounty, ...relatedIssue };
-			fullBounties.push(mergedBounty);
-		});
+    const fullBounties = [];
+    bounties.forEach((bounty) => {
+      const relatedIssue = issueData.find(
+        (issue) => issue.id == bounty.bountyId
+      );
+      const mergedBounty = { ...bounty, ...relatedIssue };
+      fullBounties.push(mergedBounty);
+    });
 
-		setBounties(fullBounties);
+    setBounties(fullBounties);
 
-		setIsLoading(false);
-	}
+    setIsLoading(false);
+  }
 
-	const filterByOrg = (e) => {
-		setOrganizationSearchTerm(e.target.value);
-	};
+  const filterByOrg = (e) => {
+    setOrganizationSearchTerm(e.target.value);
+  };
 
-	const filterByIssueTitle = (e) => {
-		setIssueTitleSearchTerm(e.target.value);
-	};
+  const filterByIssueTitle = (e) => {
+    setIssueTitleSearchTerm(e.target.value);
+  };
 
-	// Render
-	if (isLoading) {
-		return <div>Loading...</div>;
-	} else {
-		return (
-			<>
-				<div className="grid grid-cols-1 gap-6 pr-20">
-					<label>Organization</label>
-					<input onKeyUp={(e) => filterByOrg(e)} type="text"></input>
-					<label>Issue Title</label>
-					<input onKeyUp={(e) => filterByIssueTitle(e)} type="text"></input>
-					{bounties.filter(bounty => {
-						return organizationSearchTerm ? bounty.owner.toLowerCase().indexOf(organizationSearchTerm.toLowerCase()) > -1 : bounty;
-					}).filter(bounty => {
-						return issueTitleSearchTerm ? bounty.title.toLowerCase().indexOf(issueTitleSearchTerm.toLowerCase()) > -1 : bounty;
-					}).map((bounty) => {
-						return (
-							<BountyCard
-								bounty={bounty}
-								key={bounty.bountyId}
-							/>
-						);
-					})}
-				</div>
-			</>
-		);
-	}
+  // Render
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <>
+        <div className="grid grid-cols-1 gap-6 pr-20">
+          <div>Organization</div>
+          <input
+            className="outline-none rounded-lg py-2 p-5 text-base bg-gray-100 shadow-inner text-black w-1/5"
+            onKeyUp={(e) => filterByOrg(e)}
+            type="text"
+            placeholder="Search for Organizations"
+          ></input>
+          <div>Issue Title</div>
+          <input
+            className="outline-none rounded-lg py-2 p-5 text-base bg-gray-100 shadow-inner text-black w-1/5"
+            onKeyUp={(e) => filterByIssueTitle(e)}
+            type="text"
+            placeholder="Search for Issues"
+          ></input>
+          {bounties
+            .filter((bounty) => {
+              return organizationSearchTerm
+                ? bounty.owner
+                    .toLowerCase()
+                    .indexOf(organizationSearchTerm.toLowerCase()) > -1
+                : bounty;
+            })
+            .filter((bounty) => {
+              return issueTitleSearchTerm
+                ? bounty.title
+                    .toLowerCase()
+                    .indexOf(issueTitleSearchTerm.toLowerCase()) > -1
+                : bounty;
+            })
+            .map((bounty) => {
+              return <BountyCard bounty={bounty} key={bounty.bountyId} />;
+            })}
+        </div>
+      </>
+    );
+  }
 };
 
 export default BountyHomepage;
