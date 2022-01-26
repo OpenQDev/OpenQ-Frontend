c13c67396684920179ed92c7a95c68b2b467dc2b
rikkdev <l.riccardo@web.de>
2022-01-26 11:31:05 +0100

    prepared app for paginating bounties and adding infinite scrolling with lazy loading

--- components/Bounty/BountyHomepage.js
+++ components/Bounty/BountyHomepage.js
@@ -1,102 +1,108 @@
 // Third Party
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext } from "react";
 // Custom
-import BountyCard from './BountyCard';
-import StoreContext from '../../store/Store/StoreContext';
-import MintBountyButton from '../MintBounty/MintBountyButton';
+import BountyCard from "./BountyCard";
+import StoreContext from "../../store/Store/StoreContext";
+import MintBountyButton from "../MintBounty/MintBountyButton";
 
 const BountyHomepage = () => {
-	// State
-	const [bounties, setBounties] = useState([]);
-	const [organizationSearchTerm] = useState('');
-	const [issueTitleSearchTerm, setIssueTitleSearchTerm] = useState('');
-	const [isLoading, setIsLoading] = useState(true);
+  // State
+  const [bounties, setBounties] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [organizationSearchTerm] = useState("");
+  const [issueTitleSearchTerm, setIssueTitleSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-	// Context
-	const [appState] = useContext(StoreContext);
+  // Constants
+  const bountiesPerPage = 10;
+  const bountiesVisited = pageNumber * bountiesPerPage;
 
-	// Hooks
-	useEffect(() => {
-		populateBountyData();
-	}, []);
+  // Context
+  const [appState] = useContext(StoreContext);
 
-	// Methods
-	async function populateBountyData() {
-		setIsLoading(true);
+  // Hooks
+  useEffect(() => {
+    populateBountyData();
+  }, []);
 
-		const bounties = await appState.openQSubgraphClient.getAllBounties();
+  // Methods
+  async function populateBountyData() {
+    setIsLoading(true);
 
-		const bountyIds = bounties.map((bounty) => bounty.bountyId);
-		const issueData = await appState.githubRepository.getIssueData(bountyIds);
+    const bounties = await appState.openQSubgraphClient.getAllBounties();
+    console.log("bounties arrived", bounties);
 
-		const fullBounties = [];
-		bounties.forEach((bounty) => {
-			const relatedIssue = issueData.find(
-				(issue) => issue.id == bounty.bountyId
-			);
-			const mergedBounty = { ...bounty, ...relatedIssue };
-			fullBounties.push(mergedBounty);
-		});
+    const bountyIds = bounties.map((bounty) => bounty.bountyId);
+    const issueData = await appState.githubRepository.getIssueData(bountyIds);
+    console.log("issues arrived,", issueData);
 
-		setBounties(fullBounties);
+    const fullBounties = [];
+    bounties.forEach((bounty) => {
+      const relatedIssue = issueData.find(
+        (issue) => issue.id == bounty.bountyId
+      );
+      const mergedBounty = { ...bounty, ...relatedIssue };
+      fullBounties.push(mergedBounty);
+    });
 
-		setIsLoading(false);
-	}
+    setBounties(fullBounties);
 
-	// const filterByOrg = (e) => {
-	// 	setOrganizationSearchTerm(e.target.value);
-	// };
+    setIsLoading(false);
+  }
 
-	const filterByIssueTitle = (e) => {
-		setIssueTitleSearchTerm(e.target.value);
-	};
+  // const filterByOrg = (e) => {
+  // 	setOrganizationSearchTerm(e.target.value);
+  // };
 
-	// Render
-	if (isLoading) {
-		return <div>Loading...</div>;
-	} else {
-		return (
-			<div className="flex justify-center items-center">
-				<div className="grid grid-cols-3 gap-3">
-					<input
-						className="col-span-2 outline-none font-mont rounded-lg py-2 p-5 border border-web-gray bg-dark-mode text-white"
-						onKeyUp={(e) => filterByIssueTitle(e)}
-						type="text"
-						placeholder="Search Issue..."
-					></input>
-					<MintBountyButton />
-					<div className="col-span-3">
-						<div className="text-gray-300 font-mont pb-2 pt-1 font-normal">
-							{bounties.length}
-							{bounties.length < 2 ? ' Bounty found' : ' Bounties found'}
-						</div>
-						{bounties
-							.filter((bounty) => {
-								return organizationSearchTerm
-									? bounty.owner
-										.toLowerCase()
-										.indexOf(organizationSearchTerm.toLowerCase()) > -1
-									: bounty;
-							})
-							.filter((bounty) => {
-								return issueTitleSearchTerm
-									? bounty.title
-										.toLowerCase()
-										.indexOf(issueTitleSearchTerm.toLowerCase()) > -1
-									: bounty;
-							})
-							.map((bounty) => {
-								return (
-									<div className="pb-3" key={bounty.bountyId}>
-										<BountyCard bounty={bounty} key={bounty.bountyId} />
-									</div>
-								);
-							})}
-					</div>
-				</div>
-			</div>
-		);
-	}
+  const filterByIssueTitle = (e) => {
+    setIssueTitleSearchTerm(e.target.value);
+  };
+
+  // Render
+  return (
+    <div className="flex justify-center items-center">
+      <div className="grid grid-cols-3 gap-3">
+        <input
+          className="col-span-2 outline-none font-mont rounded-lg py-2 p-5 border border-web-gray bg-dark-mode text-white"
+          onKeyUp={(e) => filterByIssueTitle(e)}
+          type="text"
+          placeholder="Search Issue..."
+        ></input>
+        <MintBountyButton />
+        {isLoading ? null : (
+          <div className="col-span-3">
+            <div className="text-gray-300 font-mont pb-2 pt-1 font-normal">
+              {bounties.length}
+              {bounties.length < 2 ? " Bounty found" : " Bounties found"}
+            </div>
+            {bounties
+              .slice(bountiesVisited, bountiesVisited + bountiesPerPage)
+              .filter((bounty) => {
+                return organizationSearchTerm
+                  ? bounty.owner
+                      .toLowerCase()
+                      .indexOf(organizationSearchTerm.toLowerCase()) > -1
+                  : bounty;
+              })
+              .filter((bounty) => {
+                return issueTitleSearchTerm
+                  ? bounty.title
+                      .toLowerCase()
+                      .indexOf(issueTitleSearchTerm.toLowerCase()) > -1
+                  : bounty;
+              })
+              .map((bounty) => {
+                return (
+                  <div className="pb-3" key={bounty.bountyId}>
+                    <BountyCard bounty={bounty} key={bounty.bountyId} />
+                  </div>
+                );
+              })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default BountyHomepage;
