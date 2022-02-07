ff985ffa24582b99500046bcdf23fa53630c4c58
Flaco <andrew@openq.dev>
2022-02-07 14:56:28 -0600

    updates

--- components/Loading/LoadingIcon.js
+++ components/Loading/LoadingIcon.js
@@ -4,7 +4,7 @@ import React from 'react';
 const LoadingIcon = () => {
 	return (
 		<>
-			<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+			<div className="justify-center text-white items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
 				Loading...
 			</div>
 			<div className="opacity-25 fixed inset-0 bg-black"></div>
