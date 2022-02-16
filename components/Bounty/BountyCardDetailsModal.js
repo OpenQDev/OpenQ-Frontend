c6e20e690bb831a316d60ba80b522a63e7d56981
rikkdev <l.riccardo@web.de>
2022-02-16 23:14:00 +0100

    adjusted bounty page to make view responsive for mobile

--- components/Bounty/BountyCardDetailsModal.js
+++ components/Bounty/BountyCardDetailsModal.js
@@ -1,52 +1,52 @@
 // Third Party
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 // Custom
-import BountyCardDetails from './BountyCardDetails';
+import BountyCardDetails from "./BountyCardDetails";
 
 const BountyCardDetailsModal = (props) => {
-	const { bounty, tokenValues, deposits } = props;
+  const { bounty, tokenValues, deposits } = props;
 
-	let menuRef = useRef();
+  let menuRef = useRef();
 
-	const updateModal = () => {
-		props.modalVisibility(false);
-	};
+  const updateModal = () => {
+    props.modalVisibility(false);
+  };
 
-	// close modal if clicked outside
-	useEffect(() => {
-		let handler = (event) => {
-			if (!menuRef.current.contains(event.target)) {
-				updateModal();
-			}
-		};
-		window.addEventListener('mousedown', handler);
+  // close modal if clicked outside
+  useEffect(() => {
+    let handler = (event) => {
+      if (!menuRef.current.contains(event.target)) {
+        updateModal();
+      }
+    };
+    window.addEventListener("mousedown", handler);
 
-		return () => {
-			window.removeEventListener('mousedown', handler);
-		};
-	});
+    return () => {
+      window.removeEventListener("mousedown", handler);
+    };
+  });
 
-	return (
-		<div>
-			<div className="justify-center font-mont items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-				<div className="w-auto my-6 mx-auto max-w-3xl">
-					<div
-						ref={menuRef}
-						className="rounded-lg shadow-lg  flex flex-col w-full bg-dark-mode"
-					>
-						<BountyCardDetails
-							bounty={bounty}
-							tokenValues={tokenValues}
-							deposits={deposits}
-							props={props}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className="opacity-80 fixed inset-0 bg-black"></div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="justify-center font-mont items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none p-10">
+        <div className="w-auto my-6 mx-auto max-w-3xl">
+          <div
+            ref={menuRef}
+            className="rounded-lg shadow-lg  flex flex-col w-full bg-dark-mode"
+          >
+            <BountyCardDetails
+              bounty={bounty}
+              tokenValues={tokenValues}
+              deposits={deposits}
+              props={props}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="opacity-80 fixed inset-0 bg-black"></div>
+    </div>
+  );
 };
 
 export default BountyCardDetailsModal;
