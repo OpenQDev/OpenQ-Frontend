00ceb9ad31ac02940de35c2bbf45f0ea55e84ada
Flaco <andrew@openq.dev>
2022-03-07 10:04:34 -0600

    updates demo link to open in new tab

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -11,14 +11,14 @@ const Layout = ({ children }) => {
 	const [sidebar, setSidebar] = useState(false);
 
 	const [isFirstLaunch] = useCheckFirstLaunch();
-	const [showBanner, updateShowBanner]= useState(true);
+	const [showBanner, updateShowBanner] = useState(true);
 	return (
-		<div>{showBanner&&isFirstLaunch ?
+		<div>{showBanner && isFirstLaunch ?
 			<div className="w-full bg-pink-500/20 border-pink-500 border-b h-12 text-white grid grid-cols-wide content-center px-4">
-				
-				<div className='col-start-2 col-end-3 text-center'>Welcome to <span className='font-bold'>OpenQ!</span> Since it{'\''}s your first time with us, check out our <a className='underline font-bold text-white/90' href="https://vimeo.com/677467068">demo</a>.</div>
-				<button onClick={()=>updateShowBanner(false)} className='w-6  h-6 justify-self-end text-white cursor-pointer font-bold bg-pink-500/80 hover:bg-pink-500 rounded-md text-center'>{'\×'}</button>
-			</div>:null}
+
+				<div className='col-start-2 col-end-3 text-center'>Welcome to <span className='font-bold'>OpenQ!</span> Since it{'\''}s your first time with us, check out our <a className='underline font-bold text-white/90' href="https://vimeo.com/677467068" target="_blank" rel="noreferrer">demo</a>.</div>
+				<button onClick={() => updateShowBanner(false)} className='w-6  h-6 justify-self-end text-white cursor-pointer font-bold bg-pink-500/80 hover:bg-pink-500 rounded-md text-center'>{'\×'}</button>
+			</div> : null}
 		<div className="flex flex-row">
 			<Sidebar trigger={sidebar} setTrigger={setSidebar} />
 
