963359404655c0fa8a5ba290f7a114415f96b60f
rikkdev <l.riccardo@web.de>
2022-03-16 18:46:28 +0100

    removed ref error

--- components/Layout/Sidebar.js
+++ components/Layout/Sidebar.js
@@ -1,56 +1,58 @@
 // Third Party
-import React, { useEffect, useRef } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
 // Custom
 
 const Sidebar = ({ trigger, setTrigger }) => {
-	let menuRef = useRef();
+  let menuRef = useRef();
 
-	useEffect(() => {
-		if (trigger) {
-			let handler = (event) => {
-				if (!menuRef.current.contains(event.target)) {
-					setTrigger(false);
-				}
-			};
+  useEffect(() => {
+    if (trigger) {
+      let handler = (event) => {
+        if (!menuRef.current.contains(event.target)) {
+          setTrigger(false);
+        }
+      };
 
-			window.addEventListener('mousedown', handler);
+      window.addEventListener("mousedown", handler);
 
-			return () => {
-				window.removeEventListener('mousedown', handler);
-			};
-		}
-	});
+      return () => {
+        window.removeEventListener("mousedown", handler);
+      };
+    }
+  });
 
-	return (
-		/* Sidebar
+  return (
+    /* Sidebar
 	
 			This sidebar is hidden via tailwind classes. 
 			absolute inset-y-0 left-0 transform -translate-x-full sm:relative sm:translate-x-0 transition duration-200 ease-in-out
 			*/
-		<div className={` ${!trigger ? 'invisible' : ''} md:visible`}>
-			<div className="flex fixed">
-				<div
-					ref={menuRef}
-					className="bg-dark-mode w-20 flex min-h-screen flex-col border-r border-web-gray"
-				>
-					<nav className="text-white flex flex-col space-y-4 items-center pt-1 flex-grow">
-						<div className="pb-8 pt-5 cursor-pointer">
-							<Link href="/">
-								<Image
-									src="/openq-logo.png"
-									alt="OpenQ"
-									width="31"
-									height="31"
-								/>
-							</Link>
-						</div>
-					</nav>
-				</div>
-			</div>
-		</div>
-	);
+    <div className={` ${!trigger ? "invisible" : ""} md:visible`}>
+      <div className="flex fixed">
+        <div
+          ref={menuRef}
+          className="bg-dark-mode w-20 flex min-h-screen flex-col border-r border-web-gray"
+        >
+          <nav className="text-white flex flex-col space-y-4 items-center pt-1 flex-grow">
+            <div className="pb-8 pt-5 cursor-pointer">
+              <Link href="/">
+                <a>
+                  <Image
+                    src="/openq-logo.png"
+                    alt="OpenQ"
+                    width="31"
+                    height="31"
+                  />
+                </a>
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
