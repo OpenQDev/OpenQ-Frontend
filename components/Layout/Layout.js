d0379583f135266979e3949457c4a5f899fd097e
Christopher Stevers <christopher.stevers@protonmail.com>
2022-04-01 10:27:11 -0400

    made first launch banner more responsie

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -14,9 +14,9 @@ const Layout = ({ children }) => {
 	const [showBanner, updateShowBanner] = useState(true);
 	return (
 		<div>{showBanner && isFirstLaunch ?
-			<div className="w-full bg-inactive-accent-inside border-inactive-accent border-b h-12 text-white grid grid-cols-wide content-center px-4">
+			<div className="w-full bg-inactive-accent-inside border-inactive-accent border-b text-white grid grid-cols-wide content-center py-4 items-center px-4">
 
-				<div className='col-start-2 col-end-3 text-center'>Welcome to <span className='font-bold text-tinted'>OpenQ!</span> Since it{'\''}s your first time with us, check out our <a className='underline font-bold text-tinted' href="https://vimeo.com/677467068" target="_blank" rel="noreferrer">demo</a>.</div>
+				<div className='col-start-2 col-end-3 text-center min-w-[300px]'>Welcome to <span className='font-bold text-tinted'>OpenQ!</span> Since it{'\''}s your first time with us, check out our <a className='underline font-bold text-tinted' href="https://vimeo.com/677467068" target="_blank" rel="noreferrer">demo</a>.</div>
 				<button onClick={() => updateShowBanner(false)} className='w-6  h-6 justify-self-end text-white cursor-pointer font-bold bg-inactive-accent hover:bg-active-accent rounded-md text-center'>{'\×'}</button>
 			</div> : null}
 		<div className="flex flex-row">
