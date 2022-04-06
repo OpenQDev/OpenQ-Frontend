cba41d77b608fb7a437d126b4fee867b2b25d3c1
Christopher Stevers <christopher.stevers@protonmail.com>
2022-04-06 14:32:22 -0400

    removes console.logs

--- components/Bounty/LabelsList.js
+++ components/Bounty/LabelsList.js
@@ -5,12 +5,10 @@ import Skeleton from 'react-loading-skeleton';
 // Custom
 
 const LabelsList = ({ bounty }) => {
-	
 	return (
 		<div className="flex flex-row pt-3 space-x-2">
 			<div className="space-x-2 space-y-2">
 				{bounty?.labels.map((label, index) => {
-					console.log(label);
 					return (
 						<button
 							key={index}
