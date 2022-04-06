fe1f0d921df5b9e6f95c0d92f261e8ec4af6fcd3
Christopher Stevers <christopher.stevers@protonmail.com>
2022-04-06 14:20:52 -0400

    made labels show in their proper colour and stay on one line

--- components/Bounty/LabelsList.js
+++ components/Bounty/LabelsList.js
@@ -5,19 +5,21 @@ import Skeleton from 'react-loading-skeleton';
 // Custom
 
 const LabelsList = ({ bounty }) => {
+	
 	return (
 		<div className="flex flex-row pt-3 space-x-2">
-			<div className="space-x-2">
+			<div className="space-x-2 space-y-2">
 				{bounty?.labels.map((label, index) => {
+					console.log(label);
 					return (
 						<button
 							key={index}
+							className="rounded-lg text-xs py-1 px-2 font-bold border border-purple-500 text-white truncate"
 							style={{
-								borderColor: label.color,
+								borderColor: `#${label.color}`,
 								opacity: .9,
-								color: label.color,
+								color: `#${label.color}`,
 							}}
-							className="rounded-lg text-xs py-1 px-2 font-bold border border-purple-500 text-white"
 						>
 							{label.name}
 						</button>
