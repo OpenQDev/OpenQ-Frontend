9f35092973f7ee37421fcf27a4d07eab094e7538
Christopher Stevers <christopher.stevers@protonmail.com>
2022-04-12 09:13:21 -0400

    reduced width of skeletons for responsiveness

--- components/Bounty/BountyCardHeader.js
+++ components/Bounty/BountyCardHeader.js
@@ -11,15 +11,14 @@ const BountyCardHeader = ({ bounty }) => {
 		<div className="flex flex-row space-x-20 justify-between">
 			<div className="flex flex-col">
 				<div className="text-xl text-white">
-					{bounty? `${bounty.owner}/${bounty.repoName}`: <Skeleton width={'20rem'}/>}
+					{bounty? `${bounty.owner}/${bounty.repoName}`: <Skeleton width={'10rem'}/>}
 				</div>
-				<div className="text-xl font-bold text-white">{bounty?.title||<Skeleton width={'20rem'}/>}</div>
+				<div className="text-xl font-bold text-white">{bounty?.title||<Skeleton width={'10rem'}/>}</div>
 			</div>
 			{bounty?
 				<div>
 					<Image src={bounty.avatarUrl} alt="avatarUrl" width="51" height="51" />
-				</div>:
-				<Skeleton width={51} height={51}/>
+				</div>:null
 			}
 		</div>
 	);
