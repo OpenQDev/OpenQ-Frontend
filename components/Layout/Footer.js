2350e62f2b948181ce41b4c156b5ac56fe70499a
Christopher Stevers <christopher.stevers@protonmail.com>
2022-04-18 15:18:03 -0400

    improved footer styling

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -3,9 +3,11 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div className="footer text-tinted text-center w-full py-2 lg:mx-20 bg-black" style={{ position: 'absolute', bottom: 0 }}>
-			<div>These are currently unaudited contracts. OpenQ Labs assumes no responsibility or liability for any transaction errors, faults or losses which occur while minting, funding, refunding or claiming a bounty</div>
-			<h1>Copyright 2021 OpenQ ©</h1>
+		<div className="footer text-tinted  flex justify-center w-full bg-black  py-2 lg:mx-20" style={{ position: 'absolute', bottom: '0px' }}>
+			<div className='text-center w-5/6 mx'>
+				<div>These are currently unaudited contracts. OpenQ Labs assumes no responsibility or liability for any transaction errors, faults or losses which occur while minting, funding, refunding or claiming a bounty</div>
+				<h1>Copyright 2021 OpenQ ©</h1>
+			</div>
 		</div>
 	);
 };
