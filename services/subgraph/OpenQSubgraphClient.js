51b1f7084b05df83301c38904e17a1b26e61174d
FlacoJones <andrew@openq.dev>
2022-04-19 13:57:45 -0500

    adds staging env to circleci

--- services/subgraph/OpenQSubgraphClient.js
+++ services/subgraph/OpenQSubgraphClient.js
@@ -5,10 +5,10 @@ import fetch from 'cross-fetch';
 class OpenQSubgraphClient {
 	constructor() { }
 
-	httpLink = new HttpLink({ uri: process.env.OPENQ_SUBGRAPH_HTTP_URL, fetch });
+	httpLink = new HttpLink({ uri: process.env.NEXT_PUBLIC_OPENQ_SUBGRAPH_HTTP_URL, fetch });
 
 	client = new ApolloClient({
-		uri: process.env.OPENQ_SUBGRAPH_HTTP_URL,
+		uri: process.env.NEXT_PUBLIC_OPENQ_SUBGRAPH_HTTP_URL,
 		link: this.httpLink,
 		cache: new InMemoryCache()
 	});
@@ -47,7 +47,6 @@ class OpenQSubgraphClient {
 	}
 
 	async getBounty(id, fetchPolicy = 'cache-first') {
-		console.log(fetchPolicy);
 		const lowerCasedAddress = id.toLowerCase();
 		const promise = new Promise(async (resolve, reject) => {
 			try {
