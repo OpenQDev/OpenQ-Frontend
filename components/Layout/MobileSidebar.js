40a1df78520b4e576bbd8ce0475bcf8e500a9219
Christopher Stevers <christopher.stevers@protonmail.com>
2022-04-20 09:14:26 -0400

    added reloading link to mobile sidebar

--- components/Layout/MobileSidebar.js
+++ components/Layout/MobileSidebar.js
@@ -1,24 +1,41 @@
 // Third Party
-import React from 'react';
+import React, {useContext} from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
 // Custom
 import MobileConnectButton from '../WalletConnect/MobileConnectButton.js';
 import ProfilePicture from './ProfilePicture.js';
+import StoreContext from '../../store/Store/StoreContext.js';
 
 const MobileSidebar = () => {
+	
+	const [appState] = useContext(StoreContext);
+
 	return (
 		<div className="flex justify-between items-center gap-2 w-full max-h-12 md:hidden pr-5 py-2 bg-black">
 			
 			{/* <div className="text-white text-lg font-bold font-mont">OpenQ</div> */}
 
-			<div className="pl-5">
-				<Link href="/">
-					<div className="flex flex-row space-x-4">
-						<Image src="/openq-logo.png" alt="OpenQ" width="31" height="31" />
-					</div>
-				</Link>
+			<div className="pl-5">{ appState.needsReload ?	
+				<a href="/">
+					<Image
+						src="/openq-logo.png"
+						alt="OpenQ"
+						width="31"
+						height="31"
+					/>
+				</a>:
+				<Link href={'/'}>
+					<a>
+						<Image
+							src="/openq-logo.png"
+							alt="OpenQ"
+							width="31"
+							height="31"
+						/>
+					</a>
+				</Link>}
 			</div>
 
 			<div className="flex flex-row space-x-4 items-center">
