e4e3aa201bf02604392bf3485b7e443c29501296
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2022-04-26 10:48:54 -0400

    linting

--- components/Loading/LoadingModal.js
+++ components/Loading/LoadingModal.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import LoadingIcon from './ButtonLoadingIcon';
 
-const LoadingModal = ({  loadingText, updateModal }) => {
+const LoadingModal = ({  loadingText }) => {
 	const { title, message } = loadingText;
 
 	
