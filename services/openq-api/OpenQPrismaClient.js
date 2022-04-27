c50a65f82711f25bc9679fb5a203e1a06f5059b3
Christopher Stevers <christopher.stevers@protonmail.com>
2022-04-27 11:29:44 -0400

    added tvl mutations

--- services/openq-api/OpenQPrismaClient.js
+++ services/openq-api/OpenQPrismaClient.js
@@ -1,5 +1,5 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
-import { GET_PAGINATED_TVLS } from './graphql/query';
+import { GET_PAGINATED_TVLS, CREATE_NEW_BOUNTY } from './graphql/query';
 import fetch from 'cross-fetch';
 
 class OpenQPrismaClient {
@@ -14,12 +14,12 @@ class OpenQPrismaClient {
 	});
 
 
-	async getPaginatedTVLS(id, startAt, order, first) {
+	async getPaginatedTVLS( orderBy, limit, sortOrder, cursor) {
 		const promise = new Promise(async (resolve, reject) => {
 			try {
 				const result = await this.client.query({
 					query: GET_PAGINATED_TVLS,
-					variables: { id, skip: startAt, order, first }
+					variables: { orderBy, limit, sortOrder, cursor }
 				});
 				resolve(result.data.organization);
 			} catch (e) {
@@ -29,6 +29,35 @@ class OpenQPrismaClient {
 		return promise;
 	}
 
+	async createNewBounty(  bountyId) {
+		const promise = new Promise(async (resolve, reject) => {
+			try {
+				const result = await this.client.mutate({
+					mutation: CREATE_NEW_BOUNTY,
+					variables: { bountyId, tvl:0.0 }
+				});
+				resolve(result.data.organization);
+			} catch (e) {
+				reject(e);
+			}
+		});
+		return promise;
+	}
+
+	async updateBounty(bountyId, tvl ) {
+		const promise = new Promise(async (resolve, reject) => {
+			try {
+				const result = await this.client.mutate({
+					mutation: CREATE_NEW_BOUNTY,
+					variables: { bountyId, tvl }
+				});
+				resolve(result.data.organization);
+			} catch (e) {
+				reject(e);
+			}
+		});
+		return promise;
+	}
 }
 
 
--- services/openq-api/graphql/query.js
+++ services/openq-api/graphql/query.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
-export const GET_PAGINATED_TVLS = gql`{
-	query Query( $orderBy: String, $limit: Int!, $sortOrder: String) {
-  bountiesConnection( orderBy: $orderBy, limit: $limit, sortOrder: $sortOrder) {
+export const GET_PAGINATED_TVLS = gql`
+	query GetTvls($orderBy: String, $limit: Int!, $sortOrder: String, $cursor: String) {
+  bountiesConnection( orderBy: $orderBy, limit: $limit, sortOrder: $sortOrder after: String) {
     bounties {
       tvl
       bountyId
@@ -10,5 +10,18 @@ export const GET_PAGINATED_TVLS = gql`{
     }
     cursor
   }
-}
+}`;
+
+export const CREATE_NEW_BOUNTY = gql`
+mutation CreateBounty($tvl: Float!, $bountyId: String!) {
+  createBounty(tvl: $tvl, bountyId: $bountyId) {
+    bountyId
+  }
+}`;
+
+export const UPDATE_BOUNTY = gql`
+mutation updateBounty($tvl: Float!, $bountyId: String!, ) {
+  updateBounty(tvl: $tvl, bountyId: $bountyId) {
+        count
+  }
 }`;
