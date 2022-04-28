f17d523469f4a95c1c7bbfc78c549a89c41ea29d
Christopher Stevers <christopher.stevers@protonmail.com>
2022-04-28 08:10:09 -0400

    moved token parsing + fetching function from hook to service

--- hooks/useGetTokenValues.js
+++ hooks/useGetTokenValues.js
@@ -6,37 +6,9 @@ const useGetTokenValues = (tokenBalances) => {
 	const [tokenValues, setTokenValues] = useState(null);
 	const [appState] = useContext(StoreContext);
 
-	async function getTokenValues(tokenBalances) {
-		if (tokenBalances) {
-			let tokenVolumes = {};
-			if (Array.isArray(tokenBalances)) {
-				tokenBalances.map((tokenBalance) => {
-					const tokenAddress = appState.tokenMetadata[ethers.utils.getAddress(tokenBalance.tokenAddress)].address;
-					tokenVolumes[tokenAddress] = tokenBalance.volume;
-				});
-			}
-			else {
-				const tokenAddress = appState.tokenMetadata[ethers.utils.getAddress(tokenBalances.tokenAddress)].address;
-				tokenVolumes[tokenAddress] = tokenBalances.volume;
-			}
-			const data = { tokenVolumes, network: 'polygon-pos' };
-			const url = process.env.NEXT_PUBLIC_COIN_API_URL + '/tvl';
-			//only query tvl for bounties that have deposits
-			if (JSON.stringify(data.tokenVolumes) != '{}') {
-				try {
-					const tokenValues = await appState.tokenClient.getTokenValues(data, url);
-					setTokenValues(tokenValues);
-				} catch (error) {
-					console.error(error);
-				}
-			} else {
-				setTokenValues(null);
-			}
-		}
-	}
-
-	useEffect(() => {
-		getTokenValues(tokenBalances);
+	useEffect(async() => {
+		const value =	await	appState.tokenClient.parseTokenValues(tokenBalances);
+		setTokenValues(value);
 	}, [tokenBalances]);
 
 	return [tokenValues, setTokenValues];
--- services/coins/TokenClient.js
+++ services/coins/TokenClient.js
@@ -1,7 +1,28 @@
 import axios from 'axios';
-
+import polygonMainnetTokenMetadata from '../../constants/polygon-mainnet.json';
+import mumbaiTokenMetadata from '../../constants/polygon-mumbai.json';
+import localTokenMetadata from '../../constants/local.json';
+import { ethers } from 'ethers';
 class CoinClient {
-	constructor() { }
+	constructor() {
+		switch (process.env.NEXT_PUBLIC_DEPLOY_ENV) {
+		case 'local':
+			this.tokenMetadata = localTokenMetadata;
+			break;
+		case 'docker':
+			this.tokenMetadata = localTokenMetadata;
+			break;
+		case 'development':
+			this.tokenMetadata = mumbaiTokenMetadata;
+			break;
+		case 'staging':
+			this.tokenMetadata = polygonMainnetTokenMetadata;
+			break;
+		case 'production':
+			this.tokenMetadata = polygonMainnetTokenMetadata;
+			break;
+		}	
+	}	
 
 	async getTokenValues(tokenVolumes, url) {
 		const promise = new Promise((resolve, reject) => {
@@ -15,6 +36,41 @@ class CoinClient {
 		});
 		return promise;
 	}
+	
+	parseTokenValues = async(tokenBalances) => {
+		if (tokenBalances) {
+			let tokenVolumes = {};
+			if (Array.isArray(tokenBalances)) {
+				tokenBalances.map((tokenBalance) => {
+					const tokenAddress = this.tokenMetadata[ethers.utils.getAddress(tokenBalance.tokenAddress)].address;
+					if(tokenVolumes[tokenAddress]){
+						tokenVolumes[tokenAddress] = parseInt(tokenVolumes[tokenAddress]) + parseInt(tokenBalance.volume);
+					}
+					else{
+						tokenVolumes[tokenAddress] = tokenBalance.volume;
+					}
+				});
+			}
+			else {
+				const tokenAddress = this.tokenMetadata[ethers.utils.getAddress(tokenBalances.tokenAddress)].address;
+				tokenVolumes[tokenAddress] = tokenBalances.volume;
+			}
+			const data = { tokenVolumes, network: 'polygon-pos' };
+			const url = process.env.NEXT_PUBLIC_COIN_API_URL + '/tvl';
+			//only query tvl for bounties that have deposits
+			if (JSON.stringify(data.tokenVolumes) != '{}') {
+				try {
+					const tokenValues = await this.getTokenValues(data, url);
+					return tokenValues;
+				} catch (error) {
+					console.error(error);
+				}
+			} else {
+				return null;
+			}
+		}
+	}
+
 }
 
 export default CoinClient;
