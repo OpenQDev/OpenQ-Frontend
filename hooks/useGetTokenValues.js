c10fff70838cd46e3ed39c3c09c31ac8e04ef0f8
Christopher Stevers <christopher.stevers@protonmail.com>
2022-05-06 10:11:52 -0400

    resolved issue where react state updates were getting messed up in the useEffect

--- hooks/useGetTokenValues.js
+++ hooks/useGetTokenValues.js
@@ -7,6 +7,7 @@ const useGetTokenValues = (tokenBalances) => {
 	const [appState] = useContext(StoreContext);
 
 	async function getTokenValues(tokenBalances) {
+		let didCancel = false;
 		if (tokenBalances) {
 			let tokenVolumes = {};
 			if (Array.isArray(tokenBalances)) {
@@ -25,13 +26,17 @@ const useGetTokenValues = (tokenBalances) => {
 			if (JSON.stringify(data.tokenVolumes) != '{}') {
 				try {
 					const tokenValues = await appState.tokenClient.getTokenValues(data, url);
-					setTokenValues(tokenValues);
+					if(!didCancel){
+						setTokenValues(tokenValues);
+					}
 				} catch (error) {
 					console.error(error);
+					didCancel = true;
 				}
 			} else {
 				setTokenValues(null);
 			}
+			didCancel = true;
 		}
 	}
 
