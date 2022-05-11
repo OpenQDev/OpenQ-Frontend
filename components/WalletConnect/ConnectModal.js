cf78824035be665b251eff9c0aa9ea3caf285887
Christopher Stevers <christopher.stevers@protonmail.com>
2022-05-11 12:22:49 -0400

    removed web3-gnosis connector, its too new

--- components/WalletConnect/ConnectModal.js
+++ components/WalletConnect/ConnectModal.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { gnosisSafe, injected, walletconnect } from './connectors';
+import React, { useState, useEffect } from 'react';
+import { injected, walletconnect } from './connectors';
 import useWeb3 from '../../hooks/useWeb3';
+import { GnosisSafe } from '@web3-react/gnosis-safe';
+import  { useSafeAppConnection, SafeAppConnector} from '@gnosis.pm/safe-apps-web3-react';
 
 const ConnectModal = ({closeModal})=>{
-	const {activate} = useWeb3();
+
+	const {activate, account} = useWeb3();
 
 	const handleMetaMask = async()=>{
 		await activate(injected);
@@ -14,23 +17,21 @@ const ConnectModal = ({closeModal})=>{
 		await activate( walletconnect);
 		closeModal();
 	};
-
-	const handleGnosisSafe = async()=>{
-		console.log	(	await activate(gnosisSafe));
-	};
-	
+	useEffect(()=>{
+		if(account){
+			closeModal();
+		}
+	})
+	,[account];
 	return(
 		<div>
 			<div className='text-white absolute inset-0 flex justify-center items-center'>
-				<div className='max-w-xl w-5/6 bg-dark-mode z-40  flex flex-col gap-4 p-6 text-lg items-center rounded-lg '>
+				<div className='max-w-xl wd-5/6 bg-dark-mode z-40  flex flex-col gap-4 p-6 text-lg items-center rounded-lg '>
 					<button onClick={handleMetaMask} className=' border-2 border-inactive-accent rounded-full py-2 px-4 bg-inactive-accent-inside hover:border-active-accent group-hover:bg-active-accent group-hover:border-active-accent'>
 			Metamask
 					</button>
 					<button onClick={handleWalletConnect} className='border-2 border-inactive-accent rounded-full py-2 px-4 bg-inactive-accent-inside hover:border-active-accent group-hover:border-active-accent'>
 				WalletConnect
-					</button>
-					<button onClick={handleGnosisSafe} className='border-2 border-inactive-accent rounded-full py-2 px-4 bg-inactive-accent-inside hover:border-active-accent group-hover:border-active-accent'>
-				GnosisSafe
 					</button>
 					<button onClick={closeModal} className='border-2 border-inactive-accent rounded-full py-2 px-4 bg-inactive-accent-inside group-hover:bg-active-accent hover:border-active-accent group-hover:border-active-accent'>
 					Back
--- components/WalletConnect/connectors.js
+++ components/WalletConnect/connectors.js
@@ -2,7 +2,11 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 import { GnosisSafe } from '@web3-react/gnosis-safe';
+import  { useSafeAppConnection, SafeAppConnector} from '@gnosis.pm/safe-apps-web3-react';
+import { useState, useEffect } from 'react';
 // web3-react fails to report ANY chainId if it is not included in the supportedChainIds array - so we add most common networks here... :-P
+
+
 export const injected = new InjectedConnector({ supportedChainIds: [1, 2, 3, 4, 5, 42, 10, 80001, 137, 31337, 137,  56, 97, 100, 61, 62, 63] });
 export const walletconnect = new WalletConnectConnector({
 	supportedChainIds: [137],
@@ -11,7 +15,3 @@ export const walletconnect = new WalletConnectConnector({
 	},
 	qrcode: true
 });
-export const gnosisSafe = new GnosisSafe({
-	supportedChainIds: [137],
-	rpc: {
-		137: process.env.NEXT_PUBLIC_INFURA_KEY,}});
