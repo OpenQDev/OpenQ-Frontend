e1647b725b99d0118b36481af8c04262ac308b71
Christopher Stevers <christopher.stevers@protonmail.com>
2022-05-24 08:05:23 -0400

    added norefferer to all the anchor tags wtih tart=_blank

--- components/Claim/ClaimLoadingModal.js
+++ components/Claim/ClaimLoadingModal.js
@@ -87,7 +87,7 @@ const ClaimLoadingModal = ({ confirmMethod, url, ensName, account, claimState, a
 								<div>
 									<>
 										<Link href={link[claimState]}>
-											<a className="underline break-all" target="_blank">
+											<a className="underline break-all" target="_blank" rel="noopener noreferrer" >
 												{link[claimState]}
 												<svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 relative bottom-1 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
 													<path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
--- components/FundBounty/ApproveTransferModal.js
+++ components/FundBounty/ApproveTransferModal.js
@@ -91,7 +91,7 @@ const ApproveTransferModal = ({
 							{link[approveTransferState] &&
 								<p className='break-all underline'>
 									<Link href={link[approveTransferState]}>
-										<a target={'_blank'}>
+										<a target={'_blank'} rel="noopener noreferrer">
 											{linkText[approveTransferState]||link[approveTransferState]}
 											<svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
 												<path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
