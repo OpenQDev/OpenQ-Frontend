fa4a15b87b859af8db1ee9d6784da327136e0aa1
Christopher Stevers <christopher.stevers@protonmail.com>
2022-05-26 10:42:58 -0400

    make deposits delay warning show up on bounties with deposits

--- components/Bounty/BountyCardDetails.js
+++ components/Bounty/BountyCardDetails.js
@@ -28,11 +28,6 @@ const BountyCardDetails = ({ bounty, tokenValues }) => {
 					</div>
 					<div className="lg:col-span-2  xl:col-span-3">
 						<CopyBountyAddress bounty={bounty} />
-						{bounty && bounty?.assignees && (
-							<div className="mt-2">
-								<CopyBountyAssigneeLink bounty={bounty} />
-							</div>
-						)}
 					</div>
 					<div className='lg:col-span-2 xl:col-span-3'>
 						{bounty.bountyTokenBalances ?
@@ -73,6 +68,13 @@ const BountyCardDetails = ({ bounty, tokenValues }) => {
 
 
 					</div>
+					<div className="lg:col-span-2  xl:col-span-3">
+						{bounty && bounty?.assignees && (
+							<div className="mt-2">
+								<CopyBountyAssigneeLink bounty={bounty} />
+							</div>
+						)}
+					</div>
 					{/* <div className="lg:col-span-2  xl:col-span-3 font-semibold underline flex gap-2">
 						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
 							<path strokeLinecap="round" strokeLinejoin="round" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
@@ -81,7 +83,11 @@ const BountyCardDetails = ({ bounty, tokenValues }) => {
 							<Skeleton width={'10rem'}/>
 						}
 					</div> */}
-					{bounty.bountyTokenBalances?.length != 0 && <div className='text-center font-bold text-xl py-4 col-start-1 md:col-end-3 lg:col-end-[-1] w-full'>Deposits</div>}
+					{bounty.bountyTokenBalances?.length != 0 && <div className='col-start-1 md:col-end-3 lg:col-end-[-1] w-full pt-4'>
+						<h3 className='text-center font-bold text-xl'>Deposits</h3>
+						<p className='text-tinted col-start-1 md:col-end-3 lg:col-end-[-1] w-full'>It may take up to one minute for new deposits to show up here.</p>
+					</div>}
+					
 
 					{bounty.deposits ? bounty.deposits
 						.filter((deposit) => {
