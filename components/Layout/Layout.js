9ceb6890e5fb8d34dd3a1cec54c3f1d239cb6415
FlacoJones <andrew@openq.dev>
2022-06-07 20:35:25 -0500

    updates demo vimeo link

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -16,9 +16,9 @@ import enumerableMetadata from '../../constants/polygon-mainnet-enumerable.json'
 const Layout = ({ children }) => {
 	const [gnosisSafe, setGnosisSafe] = useState();
 	const [safeInfo, setSafeInfo] = useState();
-	const { account, activate,  deactivate } = useWeb3();
+	const { account, activate, deactivate } = useWeb3();
 	const [appState] = useContext(StoreContext);
-	
+
 	useEffect(async () => {
 		const safe = new SafeAppConnector();
 		safe.getSafeInfo().then((data) => {
@@ -30,10 +30,10 @@ const Layout = ({ children }) => {
 		setGnosisSafe(safe);
 
 		// First tokens + matic
-		const firstTen = enumerableMetadata.tokens.slice( 0, 11).map(elem=>elem.address).concat('0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270');
+		const firstTen = enumerableMetadata.tokens.slice(0, 11).map(elem => elem.address).concat('0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270');
 		const network = 'polygon-pos';
 		const stringifiedTokens = firstTen.join(',');
-		const firstTenPrices = 	await axios.get(`https://api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${stringifiedTokens}&vs_currencies=usd`);
+		const firstTenPrices = await axios.get(`https://api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${stringifiedTokens}&vs_currencies=usd`);
 		appState.tokenClient.firstTenPrices = firstTenPrices.data;
 	}, []);
 
@@ -51,37 +51,37 @@ const Layout = ({ children }) => {
 		<div className='min-h-screen relative'>{showBanner && isFirstLaunch ?
 			<div className="w-full bg-inactive-accent-inside border-inactive-accent border-b  grid grid-cols-[1fr_1fr_1fr] content-center py-4 items-center px-4 pr-8">
 
-				<div className='col-start-2 col-end-3 text-center min-w-[300px]'>Welcome to <span className='font-bold text-tinted'>OpenQ!</span> Since it{'\''}s your first time with us, check out our <a className='underline font-bold text-tinted' href="https://vimeo.com/677467068" target="_blank" rel="noopener noreferrer">demo</a>.</div>
+				<div className='col-start-2 col-end-3 text-center min-w-[300px]'>Welcome to <span className='font-bold text-tinted'>OpenQ!</span> Since it{'\''}s your first time with us, check out our <a className='underline font-bold text-tinted' href="https://vimeo.com/718131976" target="_blank" rel="noopener noreferrer">demo</a>.</div>
 				<button onClick={() => updateShowBanner(false)} className='w-6 h-6 justify-self-end  cursor-pointer font-bold bg-inactive-accent hover:bg-active-accent rounded-md text-center'>{'\×'}</button>
 			</div> : null}
 
-		<div className="flex flex-row pb-56">
-			<Sidebar trigger={sidebar} setTrigger={setSidebar} />
+			<div className="flex flex-row pb-56">
+				<Sidebar trigger={sidebar} setTrigger={setSidebar} />
 
-			<div className="flex w-full flex-col md:pt-5 justify-center">
-				{/*  Mobile navbar triggered by tailwind */}
-				<MobileSidebar trigger={setSidebar} />
+				<div className="flex w-full flex-col md:pt-5 justify-center">
+					{/*  Mobile navbar triggered by tailwind */}
+					<MobileSidebar trigger={setSidebar} />
 
-				<div className="flex justify-end invisible md:visible">
-					{/* 	Profile and login components */}
-					<div className="flex flex-row items-center pr-12">
-						<div className="pr-5">
-							<ConnectButton />
-						</div>
-						<div>
-							<ProfilePicture />
+					<div className="flex justify-end invisible md:visible">
+						{/* 	Profile and login components */}
+						<div className="flex flex-row items-center pr-12">
+							<div className="pr-5">
+								<ConnectButton />
+							</div>
+							<div>
+								<ProfilePicture />
+							</div>
 						</div>
 					</div>
-				</div>
-				<div
-					className={`pt-18 justify-center sm:px-4 md:pl-24 ${sidebar ? 'opacity-20' : null
-					}`}
-				>
-					{children}
+					<div
+						className={`pt-18 justify-center sm:px-4 md:pl-24 ${sidebar ? 'opacity-20' : null
+							}`}
+					>
+						{children}
+					</div>
 				</div>
 			</div>
-		</div>
-		<Footer />
+			<Footer />
 		</div>
 	);
 };
