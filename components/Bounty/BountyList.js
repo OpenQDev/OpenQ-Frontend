c448ba84e08a83f9220b37e082d4468e1a27d030
Christopher Stevers <christopher.stevers@protonmail.com>
2022-06-09 07:24:52 -0400

    made filtering positive in bounty list

--- components/Bounty/BountyList.js
+++ components/Bounty/BountyList.js
@@ -16,6 +16,9 @@ const BountyList = ({ bounties, watchedBounties,  loading, complete, getMoreData
 	const [unfundedVisible, setUnfundedVisible] = useState(false);
 	const [claimedVisible, setClaimedVisible] = useState(false);
 	const [assignedVisible, setAssignedVisible] = useState(false);
+	const [fundedOnly, setFundedOnly] = useState(true);
+	const [unclaimedOnly, setUnclaimedOnly] = useState(true);
+	const [unassignedOnly, setUnassignedOnly] = useState(true);
 	const [sortOrder, updateSortOrder] = useState('Newest');
 	const [searchText, updateSearchText] = useState('');
 	const [tagArr, updateTagArr] = useState([]);
@@ -37,9 +40,9 @@ const BountyList = ({ bounties, watchedBounties,  loading, complete, getMoreData
 	const filter = (bounties, options = {}) => {
 		const localTagArr = options.tagArr || tagArr;
 		const localSearchText = options.searchText === undefined ? searchText : options.searchText;
-		const localShowClaimed = options.showClaimed === undefined ? claimedVisible : options.showClaimed;
-		const localShowUnfunded = options.showUnfunded === undefined ? unfundedVisible : options.showUnfunded;
-		const localShowAssigned = options.showAssigned === undefined ? assignedVisible : options.showAssigned;
+		const localUnclaimedOnly = options.unclaimedOnly === undefined ? unclaimedOnly : options.unclaimedOnly;
+		const localFundedOnly = options.fundedOnly === undefined ? fundedOnly : options.fundedOnly;
+		const localUnassignedOnly = options.unassignedOnly === undefined ? unassignedOnly : options.unassignedOnly;
 		const displayBounties = bounties.filter((bounty) => {
 			let containsSearch = true;
 			try{containsSearch = ((bounty.title + bounty.body)
@@ -63,7 +66,7 @@ const BountyList = ({ bounties, watchedBounties,  loading, complete, getMoreData
 			const isUnclaimed = bounty.status === 'OPEN';
 			const isFunded = bounty.deposits.length > 0;
 			const isAssigned = bounty.assignees?.nodes.length > 0;
-			return (containsSearch && containsTag && (localShowUnfunded || isFunded) && (localShowClaimed || isUnclaimed) && (localShowAssigned || !isAssigned ) && bounty.url);
+			return (containsSearch && containsTag && (!localFundedOnly || isFunded) && (!localUnclaimedOnly || isUnclaimed) && (!localUnassignedOnly || !isAssigned ) && bounty.url);
 			}
 			catch(err){
 				console.log(err);}
@@ -155,17 +158,20 @@ const BountyList = ({ bounties, watchedBounties,  loading, complete, getMoreData
 
 	const showUnfunded = () => {
 		setUnfundedVisible(!unfundedVisible);
-		updateSearchedBounties(orderBounties(filter(bounties, { showUnfunded: !unfundedVisible })));
+		setFundedOnly(!fundedOnly);
+		updateSearchedBounties(orderBounties(filter(bounties, { fundedOnly: !fundedOnly })));
 	};
 
 	const showClaimed = () => {
 		setClaimedVisible(!claimedVisible);
-		updateSearchedBounties(orderBounties(filter(bounties, { showClaimed: !claimedVisible })));
+		setUnclaimedOnly(!unclaimedOnly);
+		updateSearchedBounties(orderBounties(filter(bounties, { unclaimedOnly: !unclaimedOnly })));
 	};
 
 	const showAssigned = () => {
+		setUnassignedOnly(!unassignedOnly);
 		setAssignedVisible(!assignedVisible);
-		updateSearchedBounties(orderBounties(filter(bounties, { showAssigned: !assignedVisible })));
+		updateSearchedBounties(orderBounties(filter(bounties, { unassignedOnly: !unassignedOnly })));
 	}; 
 
 	const removeTag = (e) => {
@@ -236,17 +242,17 @@ const BountyList = ({ bounties, watchedBounties,  loading, complete, getMoreData
 					<Dropdown toggleFunc={handleSortBounties} toggleVal={sortOrder} names={['Newest', 'Oldest']} borderShape={'rounded-r-lg'} width={36} />
 				</div>
 				<div className='flex flex-wrap gap-4'>
-					<div onClick={showUnfunded} className="flex w-32 p-2 pr-4 gap-2 border rounded-lg justify-between border-web-gray">
-						<label htmlFor="unfunded" className=" pointer-events-none">Unfunded</label>
-						<input id="unfunded" onChange={showUnfunded} type="checkbox" className="checkbox" checked={unfundedVisible} />
+					<div onClick={showUnfunded} className="flex w-36 p-2 pr-4 gap-2 border rounded-lg justify-between border-web-gray">
+						<label htmlFor="unfunded" className=" pointer-events-none">Funded</label>
+						<input id="unfunded" onChange={showUnfunded} type="checkbox" className="checkbox" checked={fundedOnly} />
 					</div>
-					<div onClick={showClaimed} className="flex p-2 w-32 pr-4 gap-2 border rounded-lg justify-between border-web-gray">
-						<label htmlFor="claimed" className=" pointer-events-none" >Claimed</label>
-						<input id="claimed" onChange={showClaimed} type="checkbox" className="checkbox" checked={claimedVisible} />
+					<div onClick={showClaimed} className="flex p-2 w-36 pr-4 gap-2 border rounded-lg justify-between border-web-gray">
+						<label htmlFor="claimed" className=" pointer-events-none" >Unclaimed</label>
+						<input id="claimed" onChange={showClaimed} type="checkbox" className="checkbox" checked={unclaimedOnly} />
 					</div>					
-					<div onClick={showAssigned} className="flex p-2 w-32 pr-4 gap-2 border rounded-lg justify-between border-web-gray">
-						<label htmlFor="claimed" className=" pointer-events-none" >Assigned</label>
-						<input id="claimed" onChange={showAssigned} type="checkbox" className="checkbox" checked={assignedVisible} />
+					<div onClick={showAssigned} className="flex p-2 w-36 pr-4 gap-2 border rounded-lg justify-between border-web-gray">
+						<label htmlFor="claimed" className=" pointer-events-none" >Unassigned</label>
+						<input id="claimed" onChange={showAssigned} type="checkbox" className="checkbox" checked={unassignedOnly} />
 					</div>
 				</div>
 			</div>
