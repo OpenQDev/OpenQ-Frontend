2bed244bb944a6952e13b2dfda7e2b4b9aad3227
Arc_Anya <arcanyatwitch@gmail.com>
2022-06-16 17:59:36 +0200

    expanding input for extend period

--- components/RefundBounty/DepositCard.js
+++ components/RefundBounty/DepositCard.js
@@ -1,5 +1,5 @@
 // Third party
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import StoreContext from '../../store/Store/StoreContext';
 import useGetTokenValues from '../../hooks/useGetTokenValues';
 import TokenBalances from '../TokenBalances/TokenBalances';
@@ -11,8 +11,20 @@ const DepositCard = ({ deposit, refundBounty, status, isOnCorrectNetwork }) => {
 	// State
 	const [tokenValues] = useGetTokenValues(deposit);
 
+	status = 'refundable';
+	isOnCorrectNetwork = true;
+
+	const onDepositPeriodChanged = (e) => {
+		if (parseInt(e.target.value) >= 0) setDepositPeriodDays(parseInt(e.target.value));
+		if (e.target.value === '') setDepositPeriodDays('0');
+	};
+
+	const [depositPeriodDays, setDepositPeriodDays] = useState(0);
+
+	const [expanded, setExpanded] = useState(false);
+
 	return (
-		<div className={`flex flex-col items-start px-8 max-w-[90%] sm:px-6 pb-4 max-w-sm bg-web-gray/20 ${status === 'refundable' ? ' border-pink-300' : status === 'not-yet-refundable' ? '' : ' border-web-gray'} border rounded-md`}>
+		<div className={`flex flex-col items-start py-4 px-8 max-w-[90%] sm:px-6 pb-4 max-w-sm bg-web-gray/20 ${status === 'refundable' ? ' border-pink-300' : status === 'not-yet-refundable' ? '' : ' border-web-gray'} border rounded-md`}>
 			<TokenBalances
 				tokenBalances={deposit}
 				tokenValues={tokenValues}
@@ -31,17 +43,80 @@ const DepositCard = ({ deposit, refundBounty, status, isOnCorrectNetwork }) => {
 				</div>)
 			}
 			{status === 'refundable' &&
-				<ToolTip
-					outerStyles="w-full flex self-center w-1/2"
-					hideToolTip={isOnCorrectNetwork}
-					toolTipText={'Please switch to the correct network to refund this deposit.'}
-					customOffsets={[0, 46]}>
-					<button onClick={() => refundBounty(deposit.id)}
-						disabled={!isOnCorrectNetwork}
-						className={`items-left text-lg  self-center ${isOnCorrectNetwork ? 'sm-confirm-btn' : 'sm-confirm-btn-disabled cursor-not-allowed'}`} >
-						Refund
-					</button>
-				</ToolTip>
+				<div className="w-full flex flex-col space-y-2">
+					<ToolTip
+						outerStyles="w-full flex self-center w-1/2"
+						hideToolTip={isOnCorrectNetwork}
+						toolTipText={'Please switch to the correct network to refund this deposit.'}
+						customOffsets={[0, 46]}>
+						<button onClick={() => refundBounty(deposit.id)}
+							disabled={!isOnCorrectNetwork}
+							className={`items-left text-lg  self-center ${isOnCorrectNetwork ? 'sm-confirm-btn' : 'sm-confirm-btn-disabled cursor-not-allowed'}`} >
+							Refund
+						</button>
+					</ToolTip>
+
+					{expanded ?
+						<div className="w-full flex space-x-3 self-center">
+							<ToolTip
+								outerStyles="w-full flex self-center w-1/2"
+								hideToolTip={isOnCorrectNetwork}
+								toolTipText={'Please switch to the correct network to refund this deposit.'}
+								customOffsets={[0, 46]}>
+								<button onClick={() => setExpanded(!expanded)}
+									disabled={!isOnCorrectNetwork}
+									className={`items-left text-lg  self-center ${isOnCorrectNetwork ?
+										(!expanded ?
+											'sm-confirm-btn' :
+											(depositPeriodDays > 0 ?
+												'font-mont rounded-lg w-full py-2 font-semibold border border-green bg-green bg-opacity-10 hover:bg-green hover:bg-opacity-30 cursor-pointer'
+												: 'sm-confirm-btn'
+
+											)
+										)
+										: 'sm-confirm-btn-disabled cursor-not-allowed'
+										}`} >
+									Extend By
+								</button>
+							</ToolTip>
+							<div className="flex w-full flex-row justify-between items-center px-4 py-2 rounded-lg bg-dark-mode border border-web-gray ">
+								<div className=' flex items-center gap-3 w-full'>
+									<ToolTip customOffsets={[-192, -142]} outerStyles={''} mobileX={10} toolTipText={'This is the number of days that your deposit will be in escrow. After this many days, you\'re deposit will be fully refundable if the bounty has still not been claimed.'} >
+										<div className='cursor-help rounded-full border-2 border-web-gray aspect-square leading-6 h-6 box-content text-center font-bold text-web-gray'>?</div>
+									</ToolTip>
+								</div>
+
+								<div className={'px-4 font-bold fundBox-amount bg-dark-mode'}>
+									<input
+										className="font-semibold text-right /60 text-2xl number outline-none bg-dark-mode w-full flex-1"
+										autoComplete="off"
+										value={depositPeriodDays}
+										id="deposit-period"
+										onChange={onDepositPeriodChanged}
+									/>
+								</div>
+							</div>
+						</div>
+						:
+						<ToolTip
+							outerStyles="w-full flex self-center w-1/2"
+							hideToolTip={isOnCorrectNetwork}
+							toolTipText={'Please switch to the correct network to refund this deposit.'}
+							customOffsets={[0, 46]}>
+							<button onClick={() => setExpanded(!expanded)}
+								disabled={!isOnCorrectNetwork}
+								className={`items-left text-lg  self-center ${isOnCorrectNetwork ? 'sm-confirm-btn'
+									: 'sm-confirm-btn-disabled cursor-not-allowed'
+									}`} >
+								Extend
+							</button>
+						</ToolTip>}
+
+
+
+
+
+				</div>
 			}
 		</div>
 
