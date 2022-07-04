e974ea0ca36236210800a0b4a282db8cd2e3db57
FlacoJones <andrew@openq.dev>
2022-07-04 11:04:50 +0200

    ethbarcelona DEPLOY_ENV

--- services/coins/TokenClient.js
+++ services/coins/TokenClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { ethers } from 'ethers';
 import { setup } from 'axios-cache-adapter';
-import enumerable from'../../constants/polygon-mainnet-enumerable.json';
-import indexable from'../../constants/polygon-mainnet-indexable.json';
+import enumerable from '../../constants/polygon-mainnet-enumerable.json';
+import indexable from '../../constants/polygon-mainnet-indexable.json';
 import localEnumerable from '../../constants/openq-local-enumerable.json';
 import localIndexable from '../../constants/openq-local-indexable.json';
 import mumbaiEnumerable from '../../constants/openq-polygon-mumbai-enumerable.json';
@@ -13,42 +13,46 @@ import polygonMainnetIndexable from '../../constants/openq-polygon-mainnet-index
 class CoinClient {
 	constructor() {
 		switch (process.env.NEXT_PUBLIC_DEPLOY_ENV) {
-		case 'local':
-			this.openqIndexableTokens = localIndexable;
-			this.openqEnumerableTokens = localEnumerable;
-			break;
-		case 'docker':
-			this.openqIndexableTokens = localIndexable;
-			this.openqEnumerableTokens = localEnumerable;
-			break;
-		case 'development':
-			this.openqIndexableTokens = mumbaiIndexable;
-			this.openqEnumerableTokens = mumbaiEnumerable;
-			break;
-		case 'staging':
-			this.openqIndexableTokens = polygonMainnetIndexable;
-			this.openqEnumerableTokens = polygonMainnetEnumerable;
-			break;
-		case 'production':
-			this.openqIndexableTokens = polygonMainnetIndexable;
-			this.openqEnumerableTokens = polygonMainnetEnumerable;
-			break;
-		}	
-	}	
-firstTenPrices = {}
+			case 'local':
+				this.openqIndexableTokens = localIndexable;
+				this.openqEnumerableTokens = localEnumerable;
+				break;
+			case 'docker':
+				this.openqIndexableTokens = localIndexable;
+				this.openqEnumerableTokens = localEnumerable;
+				break;
+			case 'development':
+				this.openqIndexableTokens = mumbaiIndexable;
+				this.openqEnumerableTokens = mumbaiEnumerable;
+				break;
+			case 'staging':
+				this.openqIndexableTokens = polygonMainnetIndexable;
+				this.openqEnumerableTokens = polygonMainnetEnumerable;
+				break;
+			case 'production':
+				this.openqIndexableTokens = polygonMainnetIndexable;
+				this.openqEnumerableTokens = polygonMainnetEnumerable;
+				break;
+			case 'ethbarcelona':
+				this.openqIndexableTokens = polygonMainnetIndexable;
+				this.openqEnumerableTokens = polygonMainnetEnumerable;
+				break;
+		}
+	}
+	firstTenPrices = {};
 
-async getTokenValues(tokenVolumes, url) {
-	const promise = new Promise((resolve, reject) => {
-		axios.post(url, tokenVolumes)
-			.then((result) => {
-				resolve({ ...result.data });
-			})
-			.catch((error) => {
-				reject(error);
-			});
-	});
-	return promise;
-}
+	async getTokenValues(tokenVolumes, url) {
+		const promise = new Promise((resolve, reject) => {
+			axios.post(url, tokenVolumes)
+				.then((result) => {
+					resolve({ ...result.data });
+				})
+				.catch((error) => {
+					reject(error);
+				});
+		});
+		return promise;
+	}
 
 	cachingClient = setup({
 		cache: { maxAge: 6 * 1000 } // 3s
@@ -59,7 +63,7 @@ async getTokenValues(tokenVolumes, url) {
 			let tokenVolumes = {};
 			if (Array.isArray(tokenBalances)) {
 				for (let i = 0; i < tokenBalances.length; i++) {
-					const tokenMetadata =  this.getToken(tokenBalances[i].tokenAddress);
+					const tokenMetadata = this.getToken(tokenBalances[i].tokenAddress);
 					const tokenAddress = tokenMetadata.address;
 					if (tokenVolumes[tokenAddress]) {
 						tokenVolumes[tokenAddress] = {
@@ -88,19 +92,19 @@ async getTokenValues(tokenVolumes, url) {
 			//only query tvl for bounties that have deposits
 			let fetchValues = false;
 			if (JSON.stringify(data.tokenVolumes) != '{}') {
-				await 	new Promise(resolve => setTimeout(resolve, 200));
-	
-				while(!fetchValues){
-					const tokenValues = {tokenPrices:{}, tokens: {}, total: 0};
+				await new Promise(resolve => setTimeout(resolve, 200));
+
+				while (!fetchValues) {
+					const tokenValues = { tokenPrices: {}, tokens: {}, total: 0 };
 					let total = 0;
-					for(let key in tokenVolumes){
+					for (let key in tokenVolumes) {
 						const lowercaseKey = key.toLowerCase();
-						if(this.firstTenPrices[lowercaseKey] && !fetchValues){
+						if (this.firstTenPrices[lowercaseKey] && !fetchValues) {
 							const multiplier = parseInt(tokenVolumes[key].volume) / Math.pow(10, tokenVolumes[key].decimals);
 							const value = this.firstTenPrices[lowercaseKey].usd;
 							tokenValues.tokens[lowercaseKey] = value * multiplier;
-							tokenValues.tokenPrices[lowercaseKey] =  Math.round(parseFloat(value) * 100) / 100;
-							total = total + value*multiplier;
+							tokenValues.tokenPrices[lowercaseKey] = Math.round(parseFloat(value) * 100) / 100;
+							total = total + value * multiplier;
 						}
 						else {
 							console.log(lowercaseKey, this.firstTenPrices);
@@ -108,11 +112,11 @@ async getTokenValues(tokenVolumes, url) {
 						}
 					}
 					tokenValues.total = Math.round(parseFloat(total) * 100) / 100;
-					if(JSON.stringify(tokenValues) !== '{"tokenPrices":{},"tokens":{},"total":0}' && !fetchValues){
+					if (JSON.stringify(tokenValues) !== '{"tokenPrices":{},"tokens":{},"total":0}' && !fetchValues) {
 						return tokenValues;
 					}
 				}
-				
+
 				try {
 					const tokenValues = await this.getTokenValues(data, url);
 					return tokenValues;
@@ -127,10 +131,10 @@ async getTokenValues(tokenVolumes, url) {
 
 
 	async getTokenMetadata(cursor, limit, list) {
-		if(list === 'polygon'){
-			return enumerable.tokens.slice(cursor, cursor+limit);
+		if (list === 'polygon') {
+			return enumerable.tokens.slice(cursor, cursor + limit);
 		}
-		if(this.openqEnumerableTokens.length && list === 'constants'){
+		if (this.openqEnumerableTokens.length && list === 'constants') {
 			return this.openqEnumerableTokens;
 		}
 		else return [];
@@ -138,10 +142,10 @@ async getTokenValues(tokenVolumes, url) {
 
 	getToken(address) {
 		const checkSummedAddress = ethers.utils.getAddress(address);
-		if(indexable[address.toLowerCase()]){		
+		if (indexable[address.toLowerCase()]) {
 			return indexable[address.toLowerCase()];
 		}
-		if(this.openqIndexableTokens[checkSummedAddress]){
+		if (this.openqIndexableTokens[checkSummedAddress]) {
 			return this.openqIndexableTokens[checkSummedAddress];
 		}
 		return {
