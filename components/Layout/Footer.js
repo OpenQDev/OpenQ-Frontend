e0a6c7524139b95720e0284cbd7e21f7ed61f653
FlacoJones <andrew@openq.dev>
2022-07-04 15:53:09 +0200

    removes footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -12,12 +12,6 @@ const Footer = () => {
 	const iH = 16;
 	return (
 		<div className="text-tinted justify-center w-full md:pl-20" style={{ position: 'absolute', bottom: '0px' }}>
-			<div className='pt-2 border-t border-web-gray px-12'>
-				<div className='w-full'>
-					<div>These are currently unaudited contracts. OpenQ Labs assumes no responsibility or liability for any transaction errors, faults or losses which occur while minting, funding, refunding or claiming a bounty.</div>
-				
-				</div>
-			</div>
 			<div className="flex flex-wrap items-center justify-between pb-2 px-12">
 				<div className='flex-0'>Copyright {year} OpenQ ©</div>
 				<div className='flex flex-wrap justify-content-end w-max text-right'>
@@ -29,16 +23,16 @@ const Footer = () => {
 						</Link>
 						<Link href={'https://github.com/OpenQDev/'}>
 							<a target={'_blank'} rel="noopener noreferrer">
-								<Image src={'/social-icons/github-logo-white.svg'}  width={iW} height={iH}/>
+								<Image src={'/social-icons/github-logo-white.svg'} width={iW} height={iH} />
 							</a>
 						</Link>
 						<Link href={'https://discord.gg/puQVqEvVXn'}>
 							<a target={'_blank'} rel="noopener noreferrer">
-								<Image src={'/social-icons/discord.svg'}  width={iW} height={iH}/>
+								<Image src={'/social-icons/discord.svg'} width={iW} height={iH} />
 							</a>
 						</Link>
 					</div>
-					<CopyAddressToClipboard data={'0x06b306C85E5F33b1b2D971822ce0eD42FB7aB9a1'} styles="pt-0 w-64"/>
+					<CopyAddressToClipboard data={'0x06b306C85E5F33b1b2D971822ce0eD42FB7aB9a1'} styles="pt-0 w-64" />
 				</div>
 			</div>
 		</div>);
