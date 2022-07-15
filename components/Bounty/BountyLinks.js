d49bb77176777ea2c37f8fdd9727046140b00d5d
Christopher Stevers <christopher.stevers@protonmail.com>
2022-07-15 12:23:44 -0400

    removes console.log

--- components/Bounty/BountyLinks.js
+++ components/Bounty/BountyLinks.js
@@ -34,7 +34,6 @@ const BountyLinks = ({ bounty, hideBountyLink, bountyAddress }) => {
 	const watchBounty = async () => {
 		try {
 			const response = await axios.get(`${process.env.NEXT_PUBLIC_AUTH_URL}/hasSignature?address=${account}`, { withCredentials: true });
-			console.log(response);
 			if (response.data.status===false) {
 				const signature = await signMessage();
 				await axios.post(`${process.env.NEXT_PUBLIC_AUTH_URL}/verifySignature`,
