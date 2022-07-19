2f894c0373e8e47e3f1e286e2d4fd0556e06babd
Christopher Stevers <christopher.stevers@protonmail.com>
2022-07-19 06:17:27 -0400

    progress returning ViewStream

--- pages/stream/ViewStream.js
+++ pages/stream/ViewStream.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import useWeb3 from '../../hooks/useWeb3';
+import StoreContext from '../../store/Store/StoreContext';
+import Stream from './Stream';
 
-const ViewStream = () => {
+const ViewStreams = () => {
+	const {account} = useWeb3();
+	const [superfluidData, setSuperfluidData ] = useState();
+	const [appState] = useContext(StoreContext);
+	useEffect(async()=>{
+		if(account){
+			const value = await appState.superfluidClient.viewAccount(account);
+			console.log(value);
+			setSuperfluidData(value.data.account);
+		}
+	},[account]);
+
 	return (
-		<div className="mt-8">Here I normally see my streams</div>
-	)
-}
+		<div className="mt-8">
+			
+			<section>
+				<h2 className='text-center font-bold text-tinted text-3xl'>Your Inflows</h2>
+				<div className='my-8'>	{superfluidData&&superfluidData.inflows.map((elem, index)=><Stream key={index}  direction={'in'} stream ={elem}/>)}</div>
+			</section>
 
-export default ViewStream
+			<section>
+				<h2 className='text-center font-bold text-tinted text-3xl'>Your Outflows</h2>
+				<div className='my-8'>	{superfluidData&&superfluidData.outflows.map((elem, index)=><Stream key={index} direction={'out'} stream ={elem}/>)}</div>
+			</section>
+		
+		</div>
+	);
+};
+
+export default ViewStreams;
--- pages/stream/index.js
+++ pages/stream/index.js
@@ -1,27 +1,27 @@
-import React, { useState, useContext, useEffect } from "react";
-import Toggle from "../../components/Utils/Toggle";
-import CreateStream from "./CreateStream";
-import ViewStream from "./ViewStream";
+import React, { useState, useContext, useEffect } from 'react';
+import Toggle from '../../components/Utils/Toggle';
+import CreateStream from './CreateStream';
+import ViewStream from './ViewStream';
 
 
 const stream = () => {
-  const [internalMenu, setInternalMenu] = useState("View");
+	const [internalMenu, setInternalMenu] = useState('View');
 
-  const handleToggle = (e) => {
-    setInternalMenu(e);
-  };
+	const handleToggle = (e) => {
+		setInternalMenu(e);
+	};
 
-  return (
-    <div className="flex flex-col font-mont justify-center items-center pt-7">
-      <Toggle
-        toggleFunc={handleToggle}
-        toggleVal={internalMenu}
-        names={["View", "Stream"]}
-      />
-      {internalMenu == "View" ? <ViewStream /> : null}
-			{internalMenu == "Stream" ? <CreateStream /> : null}
-    </div>
-  );
+	return (
+		<div className="flex flex-col font-mont justify-center items-center pt-7">
+			<Toggle
+				toggleFunc={handleToggle}
+				toggleVal={internalMenu}
+				names={['View', 'Stream']}
+			/>
+			{internalMenu == 'View' ? <ViewStream /> : null}
+			{internalMenu == 'Stream' ? <CreateStream /> : null}
+		</div>
+	);
 };
 
 export default stream;
