7cafc8a7048a96b132794b494fb967cc214bcd2b
ArcAnya <arcanyatwitch@gmail.com>
2022-07-29 15:13:07 +0200

    refund and extend deposit button new design & btn-primary colors as GitHub

--- components/FundBounty/FundPage.js
+++ components/FundBounty/FundPage.js
@@ -210,7 +210,7 @@ const FundPage = ({ bounty, refreshBounty }) => {
 
 				<div className="flex w-full input-field-big ">
 					<div className=' flex items-center gap-3 w-full text-primary whitespace-nowrap'>
-						<ToolTipNew customOffsets={[-192, -142]} outerStyles={''} mobileX={10} toolTipText={'This is the number of days that your deposit will be in escrow. After this many days, you\'re deposit will be fully refundable if the bounty has still not been claimed.'} >
+						<ToolTipNew mobileX={10} toolTipText={'This is the number of days that your deposit will be in escrow. After this many days, you\'re deposit will be fully refundable if the bounty has still not been claimed.'} >
 							<div className='cursor-help rounded-full border border-gray-700 aspect-square leading-4 h-4 box-content text-center font-bold text-gray-700'>?</div>
 						</ToolTipNew>
 						<span>Deposit Locked Period</span>
