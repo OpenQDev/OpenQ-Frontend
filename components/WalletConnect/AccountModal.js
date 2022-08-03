e3fb4b8504eaedc54e3c0f931f3f50867267d5f2
ArcAnya <arcanyatwitch@gmail.com>
2022-08-03 11:11:02 +0200

    get rid of LegacyBehavior warning - set onClick on child of Link in AccountModal

--- components/WalletConnect/AccountModal.js
+++ components/WalletConnect/AccountModal.js
@@ -50,8 +50,8 @@ const AccountModal = ({ chainId, account, ensName, deactivate, setIsConnecting, 
 						<SignOutIcon className='w-4 h-4 ml-2 ' />
 						<span>Disconnect</span>
 					</button>
-					<Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/user/${account}`} onClick={disconnectAccount}>
-						<a className='flex md:hover:bg-[#1f6feb] h-8 items-center w-full hover:text-white text-[#c9d1d9] self-start gap-4 p-2 mb-2'>
+					<Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/user/${account}`}>
+						<a onClick={disconnectAccount} className='flex md:hover:bg-[#1f6feb] h-8 items-center w-full hover:text-white text-[#c9d1d9] self-start gap-4 p-2 mb-2'>
 							<PersonIcon className='w-4 h-4 ml-2' />
 							<span>Profile</span>
 						</a>
