61fbaca1c1b460bbd483673bfdba4495c8bfc13c
ArcAnya <arcanyatwitch@gmail.com>
2022-08-03 14:28:38 +0200

    Galaxy Fold - BountyAlreadyMintedMessage to be fully visible

--- components/MintBounty/BountyAlreadyMintedMessage.js
+++ components/MintBounty/BountyAlreadyMintedMessage.js
@@ -6,8 +6,8 @@ export default function BountyAlreadyMintedMessage({ bountyAddress, claimed, id 
 
 	const { safe } = useWeb3();
 	return (
-		<div className="flex flex-row items-center space-x-1">
-			<div className="pt-4 text-white">Bounty is already {claimed ? 'claimed' : 'minted'}, view</div>
+		<div className="flex flex-row items-center space-x-1 pt-4 mx-4">
+			<div className="text-white text-center">Bounty is already {claimed ? 'claimed' : 'minted'}, view 
 			<Link
 				href={`/?address=${id}/${bountyAddress}}`}
 				as={`/bounty/${id}/${bountyAddress}`}
@@ -15,11 +15,12 @@ export default function BountyAlreadyMintedMessage({ bountyAddress, claimed, id 
 				<a
 					target={safe ? '_self' : '_blank'}
 					rel="noreferrer"
-					className="cursor-pointer text-link pt-4"
+					className="cursor-pointer text-link"
 				>
-					here.
+				&nbsp;here.
 				</a>
 			</Link>
+			</div>
 		</div>
 	);
 }
