040ecd992497374cd1181918569310094216da75
ArcAnya <arcanyatwitch@gmail.com>
2022-08-04 16:54:46 +0200

    suffix logic for winners according to tier

--- components/MintBounty/TierInput.js
+++ components/MintBounty/TierInput.js
@@ -1,16 +1,26 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const TierInput = ({tier}) => {
 
 	const [prize, setPrize] = useState();
+	const [suffix, setSuffix] = useState();
 
+	function handleSuffix(tier) {
+			const s = ['th', 'st', 'nd', 'rd'];
+			const v = tier % 100;
+			setSuffix(tier + (s[(v - 20) % 10] || s[v] || s[0]));
+	}
+
+	useEffect(() => {
+		handleSuffix(tier);
+	}, [])
 
 	return (
 		<div className='flex-1 w-full mt-2 ml-4'>
 			<input
 				className={`flex-1 input-field w-full`}
 				id={tier}
-				placeholder={`${tier} winner`}
+				placeholder={`${suffix} winner`}
 				autoComplete="off"
 				type="text"
 				value={prize}
