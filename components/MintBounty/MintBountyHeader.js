58bf9d8d38f20ddbd8e3b5a071291cdd1aae8fe1
ArcAnya <arcanyatwitch@gmail.com>
2022-08-04 21:10:38 +0200

    modify text for Ongoing

--- components/MintBounty/MintBountyHeader.js
+++ components/MintBounty/MintBountyHeader.js
@@ -5,7 +5,11 @@ export default function MintBountyHeader({type}) {
 		<div className="flex flex-col items-center justify-center p-5 rounded-t">
 			<h3 className="text-3xl text-center font-semibold">Deploy {type} Contract</h3>
 			<h3 className="text-2xl pt-3 w-5/6 text-center text-gray-300">
-				Create a{type==='Atomic' || type==='Ongoing'? 'n' : null} {type} Contract to send funds to any GitHub issue
+				{type === 'Ongoing' ? 
+				'Pay out a fixed amount to any contributors who submit work to this bounty, as many times as you like'
+				: `Create a${type==='Atomic'? 'n' : ''} ${type} Contract to send funds to any GitHub issue`
+			}
+				
 			</h3>
 		</div>
 	);
