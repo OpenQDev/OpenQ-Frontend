f34a95f6185c39fe0dc06cab195c40b611323318
ArcAnya <arcanyatwitch@gmail.com>
2022-08-04 11:57:42 +0200

    when no issues watched - empty (remove visible 0)

--- components/User/AboutModules/Watching.js
+++ components/User/AboutModules/Watching.js
@@ -6,7 +6,7 @@ import BountyCardLean from '../../BountyCard/BountyCardLean';
 
 const Watching = ({watchedBounties, }) => {
 	return (<div className='sm:w-3/4  max-w-[960px] mx-4 sm:mx-auto'>
-		{watchedBounties.length &&
+		{watchedBounties.length > 0 &&
 									<div className='py-6 border-border-gray flex flex-wrap items-stretch w-full font-semibold text-lg'>
 										{watchedBounties.map((bounty, index)=>
 											<BountyCardLean unWatchable={true} key={index} bounty={bounty}/>)}
