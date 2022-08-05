46e0a3e61906d72776e3be4a85742f0da2bf5be4
Christopher Stevers <christopher.stevers@protonmail.com>
2022-08-05 14:57:56 -0400

    made universal search animated

--- components/Layout/Navigation.js
+++ components/Layout/Navigation.js
@@ -146,9 +146,9 @@ const Navigation = () => {
 						</button>
 
 						<div className="md:flex hidden  content-center  items-center">
-							<div className='flex-col justify-center mr-2 h-7  group'>
+							<div className='flex-col justify-center mr-2 h-7 group '>
 								<input
-									className="md:flex hidden pr-24 items-center input-field"
+									className={`md:flex hidden pr-24 items-center focus:w-80 w-60  left-0 input-field transition-all  ease-in-out duration-700 ${quickSearch && 'focus:w-96'}`}
 									onChange={handleSearch}
 									value={quickSearch}
 									type="text"
