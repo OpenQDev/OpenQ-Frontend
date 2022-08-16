2cf9f20fc496489ce067d7fd9c2bb5500b0102e7
ArcAnya <arcanyatwitch@gmail.com>
2022-08-16 10:31:45 +0200

    correct effect: contract wizard disappears when minted

--- components/MintBounty/MintBountyModal.js
+++ components/MintBounty/MintBountyModal.js
@@ -127,11 +127,11 @@ const MintBountyModal = ({ modalVisibility, type, hideSubmenu }) => {
 				toggleType,
 				data
 			);
+			modalVisibility(false);
 			sessionStorage.setItem('justMinted', true);
 			router.push(
 				`${process.env.NEXT_PUBLIC_BASE_URL}/bounty/${issue.id}/${bountyAddress.toLowerCase()}`
 			);
-			modalVisibility(false);
 		} catch (error) {
 			console.log('error in mintbounty', error);
 			const { message, title } = appState.openQClient.handleError(error);
