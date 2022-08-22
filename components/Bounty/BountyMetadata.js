49c6e6fe2a6476bc23665a92e991773bdd9310e5
ArcAnya <arcanyatwitch@gmail.com>
2022-08-22 16:33:40 +0200

    adding TokenBalances component to display on ongoing contracts

--- components/Bounty/BountyMetadata.js
+++ components/Bounty/BountyMetadata.js
@@ -5,24 +5,27 @@ import Image from 'next/image';
 import LabelsList from './LabelsList';
 import CopyBountyAddress from './CopyBountyAddress';
 import StoreContext from '../../store/Store/StoreContext';
+import TokenBalances from '../TokenBalances/TokenBalances';
 
-const BountyMetadata = ({bounty, setInternalMenu, price, budget, split})=>{
+const BountyMetadata = ({ bounty, setInternalMenu, price, budget, split, tokenValues }) => {
 	const [appState] = useContext(StoreContext);
-	let type= 'Atomic Contract';
+	const tokenBalances = { tokenAddress: bounty.payoutTokenAddress, volume: bounty.payoutTokenVolume };
 
-	switch(bounty.bountyType){
-	case '1':
-		type='Repeating Contract';
-		break;
-	case '2':
-		type='Contest Contract';
-		break;
-	case '3':
-		type='Atomic Contract';
-		break;
+	let type = 'Atomic Contract';
+
+	switch (bounty.bountyType) {
+		case '1':
+			type = 'Repeating Contract';
+			break;
+		case '2':
+			type = 'Contest Contract';
+			break;
+		case '3':
+			type = 'Atomic Contract';
+			break;
 
 	}
-	return( 
+	return (
 
 		<ul className='md:max-w-[300px] w-full md:pl-4'>
 			<li className='border-b border-web-gray py-3'>
@@ -30,48 +33,63 @@ const BountyMetadata = ({bounty, setInternalMenu, price, budget, split})=>{
 				<div className='text-xs font-semibold text-primary leading-loose' >{type}</div>
 			</li>
 			<li className='border-b border-web-gray py-3'>
-				{(price || price === 0 )&&
-				<>
-					<div className='text-xs font-semibold text-muted'>TVL</div>
-					<button className='text-xs font-semibold text-primary' onClick={()=>setInternalMenu('Fund')}>{ appState.utils.formatter.format(
-						price
-					)}</button>
-				</>}
+				{(split || split === 0) &&
+					<>
+						<div className='text-xs font-semibold text-muted'>Payout Amount</div>
+						<button className='text-xs font-semibold text-primary' onClick={() => setInternalMenu('Claim')}>
+						<TokenBalances
+							lean={true}
+							tokenBalances={tokenBalances}
+							tokenValues={tokenValues}
+							singleCurrency={true}
+						/></button>
+					</>}
 			</li>
-			{(budget!==0)&& <li className='border-b border-web-gray py-3'>
-				{(budget || budget === 0 )&&
-				<>
-					<div className='text-xs font-semibold text-muted'>Budget</div>
-					<button className='text-xs font-semibold text-primary' onClick={()=>setInternalMenu('Fund')}>{ appState.utils.formatter.format(
-						budget
-					)}</button>
-				</>}
+			<li className='border-b border-web-gray py-3'>
+				{(price || price === 0) &&
+					<>
+						<div className='text-xs font-semibold text-muted'>TVL</div>
+						<button className='text-xs font-semibold text-primary' onClick={() => setInternalMenu('Fund')}>{appState.utils.formatter.format(
+							price
+						)}</button>
+					</>}
+			</li>
+			{(budget !== 0) && <li className='border-b border-web-gray py-3'>
+				{(budget || budget === 0) &&
+					<>
+						<div className='text-xs font-semibold text-muted'>Budget</div>
+						<button className='text-xs font-semibold text-primary' onClick={() => setInternalMenu('Fund')}>{appState.utils.formatter.format(
+							budget
+						)}</button>
+					</>}
 			</li>}
-			{bounty.assignees.length >0 && <li className='border-b border-web-gray py-3'>
+			{bounty.assignees.length > 0 && <li className='border-b border-web-gray py-3'>
 				<div className='text-xs font-semibold text-muted'>Assignees</div>
-					
-				{bounty.assignees.map((assignee, index)=>{return 						<div key={index} className='flex gap-2 py-3'><Image className='rounded-lg inline-block py-4' height={24} width={24} src={assignee.avatarUrl}/>
-					<div className='inline-block text-xs pt-1 font-semibold'>{assignee.name}</div>
-							
-				</div>;})}
+
+				{bounty.assignees.map((assignee, index) => {
+					return <div key={index} className='flex gap-2 py-3'><Image className='rounded-lg inline-block py-4' height={24} width={24} src={assignee.avatarUrl} />
+						<div className='inline-block text-xs pt-1 font-semibold'>{assignee.name}</div>
+
+					</div>;
+				})}
 			</li>}
 			<li className='border-b border-web-gray py-3'>
 				<div className='text-xs font-semibold text-muted'>Labels</div>
-				{bounty.labels.length>0 ? <LabelsList bounty={bounty} />:
+				{bounty.labels.length > 0 ? <LabelsList bounty={bounty} /> :
 					<span className='text-sm'>No labels</span>
 				}
 			</li>
 			<li className='border-b border-web-gray py-3 text sm'><Link href={`https://polygonscan.com/address/${bounty.bountyAddress}`}>
 				<div className='text-xs font-semibold  cursor-pointer text-muted'>Polygonscan</div>
-			</Link>	
-			{bounty.bountyAddress&&
-					
-						<CopyBountyAddress styles="text-sm pt-2" address={bounty.bountyAddress} />
-			}
+			</Link>
+				{bounty.bountyAddress &&
+
+					<CopyBountyAddress styles="text-sm pt-2" address={bounty.bountyAddress} />
+				}
 			</li>
 			<li className='border-b border-web-gray py-3'>
 				{bounty?.prs?.some(pr => pr.source['__typename'] === 'PullRequest' && pr.source.url) > 0 ? <ul>
-						
+
 					<div className='text-xs font-semibold text-muted'>Linked Pull Requests</div>
 					{bounty.prs.filter((pr) => {
 						return pr.source['__typename'] === 'PullRequest' && pr.source.url;
@@ -83,13 +101,14 @@ const BountyMetadata = ({bounty, setInternalMenu, price, budget, split})=>{
 										{pr.source.title}
 									</a>
 								</Link>
-								<span> 
+								<span>
 									{pr.source.merged ? ' (merged)' : ' (not merged)'}</span>
-							</li>;}
+							</li>;
+						}
 					})}
 				</ul> : <span className='text-xs font-semnibold text-muted'>No linked pull requests</span>}
 			</li>
-			
+
 		</ul>
 	);
 };
