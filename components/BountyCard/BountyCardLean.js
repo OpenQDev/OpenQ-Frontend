8c823626246b93777d830946e55317f3b76f5e58
Christopher Stevers <christopher.stevers@protonmail.com>
2022-08-24 15:15:18 -0400

    added watching count to bounties

--- components/BountyCard/BountyCardLean.js
+++ components/BountyCard/BountyCardLean.js
@@ -36,6 +36,8 @@ const BountyCardLean = ({ bounty, loading, index, length, unWatchable }) => {
 	const payoutTotal = payoutValues?.total;
 	const refundTotal = refundValues?.total;
 	const price = tokenTotal - payoutTotal - refundTotal;
+	const watchingState = useState(bounty.watchingCount);
+	const [watchingUsers] = watchingState;
 	// Hooks
 
 	const [authState] = useAuth();
@@ -58,7 +60,7 @@ const BountyCardLean = ({ bounty, loading, index, length, unWatchable }) => {
 	// Render
 	return (
 		<div className={loading ? 'pointer-events-none cursor-normal relative w-full' : 'w-full'}>
-			<BountyCardDetailsModal unWatchable={unWatchable} TVL={TVL} bounty={bounty} closeModal={closeModal} showModal={isModal && bounty} price={price} />
+			<BountyCardDetailsModal unWatchable={unWatchable} TVL={TVL} bounty={bounty} watchingState={watchingState} closeModal={closeModal} showModal={isModal && bounty} tokenValues={tokenValues} price={price} />
 			<div onClick={openModal}
 				className={
 					`flex flex-col  md:px-4 py-4 border-web-gray cursor-pointer ${index !== length - 1 && 'border-b'}`
@@ -115,7 +117,7 @@ const BountyCardLean = ({ bounty, loading, index, length, unWatchable }) => {
 								className={'stroke-muted inline-block mr-1 -mt-1 fill-muted'} viewBox="0 0 16 16" width="16" height="16"><path d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z">
 								</path>
 							</svg>
-							<span>{0}
+							<span>{watchingUsers}
 							</span></span>
 
 
@@ -154,7 +156,12 @@ const BountyCardLean = ({ bounty, loading, index, length, unWatchable }) => {
 											<PersonAddIcon />
 										</span> :
 
-										bounty.bountyType === '2' &&
+										bounty.bountyType === '2' ?
+											<span className='font-semibold flex flex-end items-center content-center gap-1 w-max'>
+												<div className='whitespace-nowrap'>Weighted</div>
+												<PeopleIcon />
+											</span>:
+											bounty.bountyType === '3' &&
 										<span className='font-semibold flex flex-end items-center content-center gap-1 w-max'>
 											<div className='whitespace-nowrap'>Weighted</div>
 											<PeopleIcon />
