7d7c17ea3dab1f56ef81c09e3b334e86fa88df5e
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2022-08-30 13:56:19 -0400

    progress on auth service and got tests running

--- __tests__/BountyCard/MiniBountyCard.test.js
+++ __tests__/BountyCard/MiniBountyCard.test.js
@@ -6,48 +6,20 @@ import React from 'react';
 
 import { render, screen } from '../../test-utils';
 import MiniBountyCard from '../../components/User/AboutModules/MiniBountyCard';
-import mocks from '../../__mocks__/mock-server.json';
-import InitialState from '../../store/Store/InitialState';
- 
+
 
 describe('MiniBountyCard', ( ) => {
 	
-	const subgraphBounty = {
-		'__typename': 'Bounty',
-		'bountyAddress': '0x13f7816057de7256daf5028eaf8e79775d3a27a3',
-		'bountyId': 'I_kwDOGWnnz85I9Ahl',
-		'bountyMintTime': '1654041044',
-		'bountyClosedTime': null,
-		'status': 'OPEN',
-		'claimedTransactionHash': null,
-		'deposits': [
+	const payout = 
 			{
-				'__typename': 'Deposit',
-				'id': '0xd024e550ba670d71f23f336c63ed0aacda946c6836f416028ffa0888b1a4b691',
-				'refunded': false,
-				'refundTime': null,
-				'expiration': '2592000',
-				'tokenAddress': '0x5fbdb2315678afecb367f032d93f642f64180aa3',
-				'volume': '12000000000000000000',
-				'sender': {
-					'__typename': 'User',
-					'id': '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+				'bounty': {
+					'issuer': {
+						'id': '0x46e09468616365256f11f4544e65ce0c70ee624b'
+					}
 				},
-				'receiveTime': '1642021044'
-			}
-		],
-		'issuer': {
-			'__typename': 'User',
-			'id': '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
-		},
-		'bountyTokenBalances': [
-			{
-				'__typename': 'BountyFundedTokenBalance',
-				'volume': '12000000000000000000',
-				'tokenAddress': '0x5fbdb2315678afecb367f032d93f642f64180aa3'
-			}
-		]
-	};
+				'volume': '600000000000000000',
+				'tokenAddress': '0x0000000000000000000000000000000000000000'
+			};
 	beforeEach(()=>{
 		const observe = jest.fn();
 		const disconnect = jest.fn();
@@ -61,7 +33,7 @@ describe('MiniBountyCard', ( ) => {
 		
 	it('should render MiniBountyCard with link', ()=>{
 		// ARRANGE
-		render(<MiniBountyCard bounty={subgraphBounty} />);
+		render(<MiniBountyCard payout={payout} />);
 
 		// ASSERT
 		expect(screen.getByRole('link')).toBeInTheDocument();
