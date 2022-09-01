79dd0d24f93b1a45e7b86142bf155c8da188949f
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2022-09-01 16:00:54 -0400

    added test for avatar pack and link tag

--- components/Utils/AvatarPack.js
+++ components/Utils/AvatarPack.js
@@ -12,8 +12,10 @@ const AvatarPack = ({ avatars }) => {
 				return <div key={avatar.login} className="w-8">
 					<ToolTipNew toolTipText={avatar.login}>
 						<div className='h-8 w-8 rounded-full bg-black cursor-pointer overflow-hidden position relative hover:z-10 z-0 hover:border-gray-700 hover:border border border-transparent'>
-							<a target="_blank" rel="noopener noreferrer" href={avatar.url}><Image height={32} width={32} src={avatar.avatarUrl} />
-							</a>
+							<Link href={avatar.url}>
+								<a target="_blank" rel="noopener noreferrer"><Image height={32} width={32} src={avatar.avatarUrl} />
+								</a>
+							</Link>
 						</div>
 					</ToolTipNew>
 				</div>;
