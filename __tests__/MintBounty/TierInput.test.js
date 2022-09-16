ea74f8e170e8c924103aedcf9222fafd1002980f
Christopher Stevers <christopher.stevers@protonmail.com>
2022-09-16 08:15:28 -0400

    updated tests

--- __tests__/MintBounty/TierInput.test.js
+++ __tests__/MintBounty/TierInput.test.js
@@ -4,7 +4,6 @@
 import React from 'react';
 import { render, screen } from '../../test-utils';
 import TierInput from '../../components/MintBounty/TierInput';
-import userEvent from '@testing-library/user-event';
 import InitialState from '../../store/Store/InitialState';
 
 InitialState.openQClient.shouldSleep = 200;
@@ -13,12 +12,9 @@ const mockTierVolumeChange = jest.fn();
 describe('MintBountyButton', () => {
   it('should open wizard and direct to discord server', async () => {
     // ARRANGE
-    const user = userEvent.setup();
-    render(<TierInput tier={2} tierVolume={80} onTierVolumeChange={mockTierVolumeChange} />);
+    render(<TierInput tier={2} tierVolumes={{ 2: 80 }} onTierVolumeChange={mockTierVolumeChange} />);
 
     // ACT
-    const tierInput = screen.getByRole('textbox');
-    await user.type(tierInput, 'hi');
-    expect(mockTierVolumeChange).toHaveBeenCalledTimes(2);
+    expect(screen.getByText(/2nd place/).toBeInTheDocument());
   });
 });
