4e94904dffa7aeb00e8c15bdaa781e7d4c227069
ArcAnya <arcanyatwitch@gmail.com>
2022-09-21 09:42:35 +0200

    refactor: ClaimPerToken last refactor with volume from use memo object

--- components/Claim/ClaimPerToken.js
+++ components/Claim/ClaimPerToken.js
@@ -20,44 +20,13 @@ const ClaimPerToken = ({ bounty, tokenAddress, claimant, type }) => {
     return { tokenAddress: tokenAddress, volume: volume } || null;
   }
 
-  const claimantVolume = () => {
-    return formatVolume(
-      filterAndAggregate(bounty.payouts.filter((payout) => payout.closer.id == claimant))?.volume || 0
-    );
-  };
-
   const claimantBalancesObj = useMemo(
     () => filterAndAggregate(bounty.payouts.filter((payout) => payout.closer.id == claimant)),
     [claimant, tokenAddress]
   );
   const [claimantBalances] = useGetTokenValues(claimantBalancesObj);
+  const claimantVolume = formatVolume(claimantBalancesObj?.volume || 0);
   const claimantValue = claimantBalances?.total;
-
-  const claimedVolume = () => {
-    return formatVolume(filterAndAggregate(bounty.payouts)?.volume || 0);
-  };
-
-  const unlockedDepositVolume = () => {
-    return formatVolume(
-      filterAndAggregate(
-        bounty.deposits
-          ?.filter((deposit) => !deposit.refunded)
-          .filter((deposit) => {
-            return parseInt(deposit.receiveTime) + parseInt(deposit.expiration) < Math.floor(Date.now() / 1000);
-          })
-      )?.volume || 0
-    );
-  };
-
-  const refundableVolume = () => {
-    const refundable =
-      parseFloat(claimedVolume()) > parseFloat(unlockedDepositVolume()) ? 0 : unlockedDepositVolume() - claimedVolume();
-    return parseFloat(refundable).toFixed(1);
-  };
-
-  const claimedBalancesObj = useMemo(() => filterAndAggregate(bounty.payouts), [bounty]);
-  const [balanceValuesclaimedBalances] = useGetTokenValues(claimedBalancesObj);
-  const claimedBalances = balanceValuesclaimedBalances?.total;
 
   const unlockedDepositsObj = useMemo(
     () =>
@@ -71,7 +40,19 @@ const ClaimPerToken = ({ bounty, tokenAddress, claimant, type }) => {
     [bounty]
   );
   const [unlockedDepositsValues] = useGetTokenValues(unlockedDepositsObj);
+  const unlockedDepositVolume = formatVolume(unlockedDepositsObj?.volume || 0);
   const unlockedDepositValue = unlockedDepositsValues?.total;
+
+  const claimedBalancesObj = useMemo(() => filterAndAggregate(bounty.payouts), [bounty]);
+  const [balanceValuesclaimedBalances] = useGetTokenValues(claimedBalancesObj);
+  const claimedVolume = formatVolume(claimedBalancesObj?.volume || 0);
+  const claimedBalances = balanceValuesclaimedBalances?.total;
+
+  const refundableVolume = () => {
+    const refundable =
+      parseFloat(claimedVolume) > parseFloat(unlockedDepositVolume) ? 0 : unlockedDepositVolume - claimedVolume;
+    return parseFloat(refundable).toFixed(1);
+  };
 
   const refundableValue = () => {
     const refundable = claimedBalances > unlockedDepositValue ? 0 : unlockedDepositValue - claimedBalances;
@@ -82,23 +63,17 @@ const ClaimPerToken = ({ bounty, tokenAddress, claimant, type }) => {
   const [stillClaimableValues] = useGetTokenValues(stillClaimableObj);
   const stillClaimable = stillClaimableValues?.total ? stillClaimableValues?.total : 0;
 
-  const refundVolume = () => {
-    return formatVolume(filterAndAggregate(bounty.refunds)?.volume || 0);
-  };
-
   const refundedObj = useMemo(() => filterAndAggregate(bounty.refunds), [tokenAddress]);
   const [refundedValues] = useGetTokenValues(refundedObj);
+  const refundVolume = formatVolume(refundedObj?.volume || 0);
   const refundedValue = refundedValues?.total ? refundedValues?.total : 0;
-
-  const totalDepositVolume = () => {
-    return formatVolume(filterAndAggregate(bounty.deposits)?.volume || 0);
-  };
 
   const balanceObjDeposits = useMemo(() => filterAndAggregate(bounty.deposits), [bounty]);
   const [balanceValuesDeposits] = useGetTokenValues(balanceObjDeposits);
+  const totalDepositVolume = formatVolume(balanceObjDeposits?.volume || 0);
   const totalDepositValue = balanceValuesDeposits?.total ? balanceValuesDeposits?.total : 0;
 
-  const currentDepositVolume = totalDepositVolume() - refundVolume();
+  const currentDepositVolume = totalDepositVolume - refundVolume;
 
   let volumeDisplay = 0;
   let percentDisplay = 0;
@@ -106,32 +81,32 @@ const ClaimPerToken = ({ bounty, tokenAddress, claimant, type }) => {
 
   switch (type) {
     case 'perClaimant':
-      volumeDisplay = claimantVolume();
-      percentDisplay = claimantVolume() / totalDepositVolume();
+      volumeDisplay = claimantVolume;
+      percentDisplay = claimantVolume / totalDepositVolume;
       valueDisplay = claimantValue;
       break;
     case 'allClaimants':
-      volumeDisplay = claimedVolume();
-      percentDisplay = claimedVolume() / totalDepositVolume();
+      volumeDisplay = claimedVolume;
+      percentDisplay = claimedVolume / totalDepositVolume;
       valueDisplay = claimedBalances;
       break;
     case 'stillClaimable':
-      volumeDisplay = parseFloat(currentDepositVolume - claimedVolume()).toFixed(1);
-      percentDisplay = parseFloat((currentDepositVolume - claimedVolume()) / totalDepositVolume());
+      volumeDisplay = parseFloat(currentDepositVolume - claimedVolume).toFixed(1);
+      percentDisplay = parseFloat((currentDepositVolume - claimedVolume) / totalDepositVolume);
       valueDisplay = stillClaimable;
       break;
     case 'refundable':
       volumeDisplay = refundableVolume();
-      percentDisplay = parseFloat(refundableVolume() / totalDepositVolume());
+      percentDisplay = parseFloat(refundableVolume() / totalDepositVolume);
       valueDisplay = refundableValue();
       break;
     case 'refunded':
-      volumeDisplay = refundVolume();
-      percentDisplay = parseFloat(refundVolume() / totalDepositVolume());
+      volumeDisplay = refundVolume;
+      percentDisplay = parseFloat(refundVolume / totalDepositVolume);
       valueDisplay = refundedValue;
       break;
     case 'total':
-      volumeDisplay = totalDepositVolume();
+      volumeDisplay = totalDepositVolume;
       percentDisplay = 1;
       valueDisplay = totalDepositValue;
       break;
