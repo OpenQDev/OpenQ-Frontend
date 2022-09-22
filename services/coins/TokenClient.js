ecee43b22590f9ae99e448ad0bc591b3a4898920
Christopher Stevers <christopher.stevers@protonmail.com>
2022-09-22 12:06:54 -0400

    reverts changes to token client that were causeing stuff to freeze

--- services/coins/TokenClient.js
+++ services/coins/TokenClient.js
@@ -93,72 +93,69 @@ class CoinClient {
   }
 
   parseTokenValues = async (tokenBalances) => {
-    const promise = new Promise(async (resolve, reject) => {
-      if (tokenBalances) {
-        let tokenVolumes = {};
-        if (Array.isArray(tokenBalances)) {
-          for (let i = 0; i < tokenBalances.length; i++) {
-            const tokenMetadata = this.getToken(tokenBalances[i].tokenAddress);
-            const tokenAddress = tokenMetadata.address;
-            if (tokenVolumes[tokenAddress]) {
-              tokenVolumes[tokenAddress] = {
-                volume: parseInt(tokenVolumes[tokenAddress]) + parseInt(tokenBalances[i].volume),
-                decimals: tokenMetadata.decimals,
-              };
-            } else {
-              tokenVolumes[tokenAddress.toLowerCase()] = {
-                volume: tokenBalances[i].volume,
-                decimals: tokenMetadata.decimals,
-              };
-            }
+    if (tokenBalances) {
+      let tokenVolumes = {};
+      if (Array.isArray(tokenBalances)) {
+        for (let i = 0; i < tokenBalances.length; i++) {
+          const tokenMetadata = this.getToken(tokenBalances[i].tokenAddress);
+          const tokenAddress = tokenMetadata.address;
+          if (tokenVolumes[tokenAddress]) {
+            tokenVolumes[tokenAddress] = {
+              volume: parseInt(tokenVolumes[tokenAddress]) + parseInt(tokenBalances[i].volume),
+              decimals: tokenMetadata.decimals,
+            };
+          } else {
+            tokenVolumes[tokenAddress.toLowerCase()] = {
+              volume: tokenBalances[i].volume,
+              decimals: tokenMetadata.decimals,
+            };
           }
-        } else {
-          const tokenMetadata = await this.getToken(tokenBalances.tokenAddress);
-          tokenVolumes[tokenMetadata.address] = {
-            volume: tokenBalances.volume,
-            decimals: tokenMetadata.decimals,
-          };
         }
-        const data = { tokenVolumes, network: 'polygon-pos' };
-        const url = this.url + '/tvl';
-        //only query tvl for bounties that have deposits
-        let fetchValues = false;
-        if (JSON.stringify(data.tokenVolumes) != '{}') {
-          await new Promise((resolve) => setTimeout(resolve, 200));
-
-          while (!fetchValues) {
-            const tokenValues = { tokenPrices: {}, tokens: {}, total: 0 };
-            let total = 0;
-            for (let key in tokenVolumes) {
-              const lowercaseKey = key.toLowerCase();
-              if (this.firstTenPrices[lowercaseKey] && !fetchValues) {
-                const multiplier = parseInt(tokenVolumes[key].volume) / Math.pow(10, tokenVolumes[key].decimals);
-                const value = this.firstTenPrices[lowercaseKey].usd;
-                tokenValues.tokens[lowercaseKey] = value * multiplier;
-                tokenValues.tokenPrices[lowercaseKey] = Math.round(parseFloat(value) * 100) / 100;
-                total = total + value * multiplier;
-              } else {
-                fetchValues = true;
-              }
-            }
-            tokenValues.total = Math.round(parseFloat(total) * 100) / 100;
-            if (JSON.stringify(tokenValues) !== '{"tokenPrices":{},"tokens":{},"total":0}' && !fetchValues) {
-              resolve(tokenValues);
-            }
-          }
-
-          try {
-            const tokenValues = await this.getTokenValues(data, url);
-            return tokenValues;
-          } catch (error) {
-            reject(error);
-          }
-        } else {
-          resolve(null);
-        }
+      } else {
+        const tokenMetadata = await this.getToken(tokenBalances.tokenAddress);
+        tokenVolumes[tokenMetadata.address] = {
+          volume: tokenBalances.volume,
+          decimals: tokenMetadata.decimals,
+        };
       }
-    });
-    return promise;
+      const data = { tokenVolumes, network: 'polygon-pos' };
+      const url = this.url + '/tvl';
+      //only query tvl for bounties that have deposits
+      let fetchValues = false;
+      if (JSON.stringify(data.tokenVolumes) != '{}') {
+        await new Promise((resolve) => setTimeout(resolve, 200));
+
+        while (!fetchValues) {
+          const tokenValues = { tokenPrices: {}, tokens: {}, total: 0 };
+          let total = 0;
+          for (let key in tokenVolumes) {
+            const lowercaseKey = key.toLowerCase();
+            if (this.firstTenPrices[lowercaseKey] && !fetchValues) {
+              const multiplier = parseInt(tokenVolumes[key].volume) / Math.pow(10, tokenVolumes[key].decimals);
+              const value = this.firstTenPrices[lowercaseKey].usd;
+              tokenValues.tokens[lowercaseKey] = value * multiplier;
+              tokenValues.tokenPrices[lowercaseKey] = Math.round(parseFloat(value) * 100) / 100;
+              total = total + value * multiplier;
+            } else {
+              fetchValues = true;
+            }
+          }
+          tokenValues.total = Math.round(parseFloat(total) * 100) / 100;
+          if (JSON.stringify(tokenValues) !== '{"tokenPrices":{},"tokens":{},"total":0}' && !fetchValues) {
+            return tokenValues;
+          }
+        }
+
+        try {
+          const tokenValues = await this.getTokenValues(data, url);
+          return tokenValues;
+        } catch (error) {
+          throw new Error(error);
+        }
+      } else {
+        return null;
+      }
+    }
   };
 
   async getTokenMetadata(cursor, limit, list) {
