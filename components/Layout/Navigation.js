78a06e1f5b56ff43177f5824a1f0fcae9f768274
ArcAnya <arcanyatwitch@gmail.com>
2022-10-03 15:49:45 +0200

    remove contract wizard navbar & add non-profit link

--- components/Layout/Navigation.js
+++ components/Layout/Navigation.js
@@ -14,7 +14,6 @@ import { ThreeBarsIcon } from '@primer/octicons-react';
 import LinkDropdown from '../Utils/LinkDropdown';
 import { useRouter } from 'next/router';
 import NavLinks from './NavLinks';
-import ContractWizard from '../ContractWizard/ContractWizard';
 import OpenQSocials from './OpenQSocials';
 import LoadingBar from '../Loading/LoadingBar';
 import LoadingThread from '../Loading/LoadingThread.js';
@@ -28,7 +27,6 @@ const Navigation = () => {
   const [quickSearch, setQuickSearch] = useState('');
   const [items, setItems] = useState([]);
   const [searchable, setSearchable] = useState();
-  const [showWizard, setShowWizard] = useState(false);
   const [loadingBar, setLoadingBar] = useState(false);
   const [changeText, setChangeText] = useState(false);
   const { bountyMinted, authService, openQSubgraphClient, openQPrismaClient, utils, githubRepository, tokenClient } =
@@ -151,11 +149,7 @@ const Navigation = () => {
   return (
     <div className='bg-nav-bg py-1 '>
       <FirstTimeBanner />
-
       <LoadingThread />
-
-      {/* Desktop view */}
-
       <div className='flex visible relative'>
         <div className='flex w-full lg:py-1 justify-between mx-8'>
           <div className='flex space-x-5 items-center'>
@@ -182,12 +176,6 @@ const Navigation = () => {
                 {quickSearch && <LinkDropdown items={items} />}
               </div>
               <NavLinks />
-              <button onClick={() => setShowWizard(true)}>
-                <div className='mx-2 text-[0.8rem] tracking-wider md:hover:text-primary text-muted font-bold hover:cursor-pointer'>
-                  Contract Wizard
-                </div>
-              </button>
-              {showWizard && <ContractWizard wizardVisibility={setShowWizard} />}
             </div>
           </div>
           <div className='flex items-center text-[0.8rem] lg:text-[1rem]'>
@@ -213,15 +201,7 @@ const Navigation = () => {
               ></input>
               {quickSearch && <LinkDropdown items={items} />}
             </div>
-
             <NavLinks />
-
-            <button onClick={() => setShowWizard(true)}>
-              <div className='flex text-[0.8rem] pt-1 border-t border-gray-700 tracking-wider text-nav-text font-bold'>
-                Contract Wizard
-              </div>
-            </button>
-            {showWizard && <ContractWizard wizardVisibility={setShowWizard} />}
           </div>
         </div>
       ) : null}
