35c08a87779ba4d823de2d380a95467105215cb3
ArcAnya <arcanyatwitch@gmail.com>
2022-10-03 12:23:19 +0200

    mintbountymodal to show switch network instead of deploy-contract, incl test adjustment

--- __tests__/MintBounty/MintBountyModalButton.test.js
+++ __tests__/MintBounty/MintBountyModalButton.test.js
@@ -9,13 +9,20 @@ import MintBountyModalButton from '../../components/MintBounty/MintBountyModalBu
 
 describe('MintBountyModalButton', () => {
   const mintBounty = jest.fn();
-  it('should be able to be disabled.', () => {
+  it('should be disabled when on correct network but not mintable yet.', () => {
     // ARRANGE
-    render(<MintBountyModalButton account={true} enableMint={false} />);
+    render(<MintBountyModalButton account={true} enableMint={false} isOnCorrectNetwork={true} />);
 
     expect(screen.getByRole('button', { name: 'Deploy Contract' }).disabled).toBe(true);
   });
-  it('should work when enabled.', async () => {
+  it('should be enabled to switch network.', () => {
+    // ARRANGE
+    render(<MintBountyModalButton account={true} enableMint={false} isOnCorrectNetwork={false} />);
+
+    const button = screen.queryByRole('button', { name: 'Use Localhost:8545 Network' });
+    expect(button).toBeInTheDocument();
+  });
+  it('should disappear when enabled transaction pending.', async () => {
     // ARRANGE
     render(
       <MintBountyModalButton mintBounty={mintBounty} account={true} enableMint={true} transactionPending={true} />
--- components/MintBounty/MintBountyModalButton.js
+++ components/MintBounty/MintBountyModalButton.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import LoadingIcon from '../Loading/ButtonLoadingIcon';
+import chainIdDeployEnvMap from '../../components/WalletConnect/chainIdDeployEnvMap';
 
-export default function MintBountyModalButton({ enableMint, transactionPending, mintBounty, account }) {
+export default function MintBountyModalButton({
+  enableMint,
+  transactionPending,
+  mintBounty,
+  account,
+  isOnCorrectNetwork,
+}) {
   return (
     <div className='flex flex-row w-full h-[38px] justify-center'>
       <button
         className={`flex w-full items-center justify-center ${
-          enableMint ? 'btn-primary cursor-pointer' : 'btn-primary cursor-not-allowed'
+          enableMint || !isOnCorrectNetwork ? 'btn-primary cursor-pointer' : 'btn-primary cursor-not-allowed'
         }`}
         type='button'
         onClick={() => mintBounty()}
-        disabled={!enableMint}
+        disabled={!enableMint && isOnCorrectNetwork}
       >
-        {transactionPending ? <LoadingIcon bg='colored' /> : account ? 'Deploy Contract' : 'Connect Wallet'}
+        {transactionPending ? (
+          <LoadingIcon bg='colored' />
+        ) : !isOnCorrectNetwork ? (
+          `Use ${chainIdDeployEnvMap[process.env.NEXT_PUBLIC_DEPLOY_ENV]['networkName']} Network`
+        ) : account ? (
+          'Deploy Contract'
+        ) : (
+          'Connect Wallet'
+        )}
       </button>
     </div>
   );
