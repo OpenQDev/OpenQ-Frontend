0ce03f6627aa5a1554c1b250194629c34c2d35fe
Christopher Stevers <christopher.stevers@protonmail.com>
2022-10-24 08:33:48 -0400

    updated AboutModules names

--- __tests__/User/AboutModules/DepositList.test.js
+++ __tests__/User/AboutModules/DepositList.test.js
@@ -4,7 +4,7 @@
 import React from 'react';
 
 import { render, screen } from '../../../test-utils';
-import DepositList from '../../../components/User/AboutModules/DepositList';
+import DepositList from '../../../components/User/OverviewTab/DepositList';
 
 describe('DepositList', () => {
   const deposits = [
