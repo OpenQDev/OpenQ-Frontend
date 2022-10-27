e9256f09a4120817c0ec1af9bec549af6d5bb426
Christopher Stevers <christopher.stevers@protonmail.com>
2022-10-27 12:37:38 -0400

    nft fund modal hide

--- components/FundBounty/NFTFundModal.js
+++ components/FundBounty/NFTFundModal.js
@@ -50,26 +50,28 @@ const NFTFundModal = ({ setPickedNft }) => {
 
   return (
     <>
-      <div>
-        <button
-          data-testid={nfts.length === 1 ? 'fetched' : 'waiting'}
-          onClick={() => setShowModal(true)}
-          className='flex gap-2 py-[7px] btn-default content-center items-center '
-        >
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='16' height='16' className='fill-muted'>
-            <path
-              fillRule='evenodd'
-              d='M1.75 2.5a.25.25 0 00-.25.25v10.5c0 .138.112.25.25.25h.94a.76.76 0 01.03-.03l6.077-6.078a1.75 1.75 0 012.412-.06L14.5 10.31V2.75a.25.25 0 00-.25-.25H1.75zm12.5 11H4.81l5.048-5.047a.25.25 0 01.344-.009l4.298 3.889v.917a.25.25 0 01-.25.25zm1.75-.25V2.75A1.75 1.75 0 0014.25 1H1.75A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25zM5.5 6a.5.5 0 11-1 0 .5.5 0 011 0zM7 6a2 2 0 11-4 0 2 2 0 014 0z'
-            ></path>
-          </svg>
-          <svg xmlns='http://www.w3.org/2000/svg' className='fill-primary' viewBox='0 0 16 16' width='16' height='16'>
-            <path
-              fillRule='evenodd'
-              d='M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z'
-            ></path>
-          </svg>
-        </button>
-      </div>
+      {showModal && (
+        <div>
+          <button
+            data-testid={nfts.length === 1 ? 'fetched' : 'waiting'}
+            onClick={() => setShowModal(true)}
+            className='flex gap-2 py-[7px] btn-default content-center items-center '
+          >
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='16' height='16' className='fill-muted'>
+              <path
+                fillRule='evenodd'
+                d='M1.75 2.5a.25.25 0 00-.25.25v10.5c0 .138.112.25.25.25h.94a.76.76 0 01.03-.03l6.077-6.078a1.75 1.75 0 012.412-.06L14.5 10.31V2.75a.25.25 0 00-.25-.25H1.75zm12.5 11H4.81l5.048-5.047a.25.25 0 01.344-.009l4.298 3.889v.917a.25.25 0 01-.25.25zm1.75-.25V2.75A1.75 1.75 0 0014.25 1H1.75A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25zM5.5 6a.5.5 0 11-1 0 .5.5 0 011 0zM7 6a2 2 0 11-4 0 2 2 0 014 0z'
+              ></path>
+            </svg>
+            <svg xmlns='http://www.w3.org/2000/svg' className='fill-primary' viewBox='0 0 16 16' width='16' height='16'>
+              <path
+                fillRule='evenodd'
+                d='M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z'
+              ></path>
+            </svg>
+          </button>
+        </div>
+      )}
 
       {showModal && (
         <>
