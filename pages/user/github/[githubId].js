654ac969fa2e42d5b7d1a8aead6ecef8cf9b010a
ArcAnya <arcanyatwitch@gmail.com>
2022-11-02 12:53:21 +0100

    adds basic input and button to connect address to GitHub

--- pages/user/github/[githubId].js
+++ pages/user/github/[githubId].js
@@ -1,21 +1,20 @@
 // Third party
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 
 // Custom
-import AboutFunder from '../../../components/User/AboutFunder';
+import AboutFreelancer from '../../../components/User/AboutFreelancer';
 import UnexpectedErrorModal from '../../../components/Utils/UnexpectedErrorModal';
 import WrappedGithubClient from '../../../services/github/WrappedGithubClient';
 // import WrappedOpenQSubgraphClient from '../../../services/subgraph/WrappedOpenQSubgraphClient';
-// import WrappedOpenQPrismaClient from '../../../services/openq-api/WrappedOpenQPrismaClient';
-import Logger from '../../../services/logger/Logger';
-import useWeb3 from '../../../hooks/useWeb3';
+import logger from '../../../services/logger/Logger';
 import useAuth from '../../../hooks/useAuth';
+import StoreContext from '../../../store/Store/StoreContext';
+import useWeb3 from '../../../hooks/useWeb3';
 
-const account = ({ githubId, user, userData, organizations, renderError }) => {
+const account = ({ githubId, user, organizations, renderError }) => {
   // TODO => useAuth => only user can see connection and connect to different wallet
-  const { account } = useWeb3();
-  const [authState] = useAuth();
+  /* const [authState] = useAuth();
   console.log(authState.githubId);
   return (
     <div className=' md:grid grid-cols-wide gap-4 justify-center col-start-2 pt-12'>
@@ -28,31 +27,99 @@ const account = ({ githubId, user, userData, organizations, renderError }) => {
         <UnexpectedErrorModal error={renderError} />
       )}
     </div>
+  ); */
+
+  const [authState] = useAuth();
+  const { account } = useWeb3();
+  const { signedAccount } = authState;
+  const [appState] = useContext(StoreContext);
+  const [starredOrganizations, setStarredOrganizations] = useState([]);
+  const [watchedBounties, setwatchedBounties] = useState([]);
+  const [relAccount, setRelAccount] = useState(account);
+
+  useEffect(async () => {
+    const userOffChainData = await appState.openQPrismaClient.getUser(account);
+    let starredOrganizations = [];
+    setwatchedBounties(userOffChainData?.watchedBounties.nodes);
+    //get starred organizations.
+    try {
+      if (userOffChainData) {
+        const subgraphOrgs = await appState.openQSubgraphClient.getOrganizationsByIds(
+          userOffChainData.starredOrganizationIds
+        );
+        const githubOrgIds = subgraphOrgs.map((bounty) => bounty.id);
+        const githubOrganizations = await appState.githubRepository.fetchOrganizationsByIds(githubOrgIds);
+        starredOrganizations = githubOrganizations.map((organization) => {
+          const subgraphOrg = subgraphOrgs.find((org) => {
+            return org.id === organization.id;
+          });
+
+          return { ...organization, ...subgraphOrg, starred: true };
+        });
+        setStarredOrganizations(starredOrganizations);
+      }
+    } catch (err) {
+      appState.logger.error(err);
+    }
+  }, []);
+
+  function onInput(e) {
+    if (ethers.utils.isAddress(e.target.value)) {
+      const checkSummedAddress = ethers.utils.getAddress(e.target.value);
+      setRelAccount(checkSummedAddress);
+    }
+  }
+
+  return (
+    <div className=' gap-4 justify-center pt-6'>
+      {user ? (
+        <div className='flex flex-col gap-4 px-8'>
+          <p>
+            You MUST sign up with Github in order to receive prize payouts in our seasonal hackathons! Funds will be
+            sent to the address you put here. You can change this at any time
+          </p>
+          <div className='flex gap-4'>
+            <input
+              className={'flex-1 input-field w-full'}
+              id='name'
+              placeholder='Enter your wallet address...'
+              autoComplete='off'
+              type='text'
+              defaultValue={account}
+              onChange={(e) => onInput(e)}
+            />
+            <button className='btn-primary'>Associate Ethereum Address to your Github {relAccount}</button>
+            {console.log(user)}
+          </div>
+          {/* <AboutFreelancer
+            showWatched={account === signedAccount}
+            starredOrganizations={starredOrganizations}
+            watchedBounties={watchedBounties}
+            user={user}
+            account={account}
+            organizations={organizations}
+          /> */}
+        </div>
+      ) : (
+        <UnexpectedErrorModal error={renderError} />
+      )}
+    </div>
   );
 };
 
 export const getServerSideProps = async (context) => {
   const githubId = context.params.githubId;
-  if (githubId == 'loading') {
-    return { props: { githubId } };
-  }
-
   const githubRepository = new WrappedGithubClient();
   githubRepository.instance.setGraphqlHeaders();
-  const logger = new Logger();
-
   let renderError = '';
-  let userData = {};
+  let user = {};
   try {
-    userData = await githubRepository.instance.fetchUserById(githubId);
+    user = await githubRepository.instance.fetchUserById(githubId);
   } catch (err) {
     logger.error(err);
     return { props: { renderError: `${githubId} is not a valid GitHub ID.` } };
   }
-
-  console.log('down here', githubId);
-
-  return { props: { githubId, renderError, userData } };
+  return { props: { githubId, renderError, user } };
 };
 
 export default account;
