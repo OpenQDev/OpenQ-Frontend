6ff040aa214f09c690f17b2c5127529df50170fa
Christopher Stevers <christopher.stevers@protonmail.com>
2022-11-10 06:11:09 -0500

    widened large modal

--- components/Utils/ModalLarge.js
+++ components/Utils/ModalLarge.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Cross from '../svg/cross';
 
 const ModalLarge = ({ title, children, footerLeft, footerRight, setShowModal, resetState }) => {
@@ -7,18 +7,36 @@ const ModalLarge = ({ title, children, footerLeft, footerRight, setShowModal, re
     resetState();
     setShowModal(false);
   };
+
+  useEffect(() => {
+    // Courtesy of https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
+    function handleClickOutside(event) {
+      if (modal.current && !modal?.current.contains(event.target)) {
+        setShowModal(false);
+        updateModal();
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [modal]);
   return (
     <div>
       <div className='flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50'>
-        <div ref={modal} className='flex w-[480px] h-[600px] border border-gray-700 rounded-sm bg-[#161B22]'>
+        <div ref={modal} className='flex w-[640px] h-[600px] border border-gray-700 rounded-sm bg-[#161B22]'>
           <div className='flex flex-col relative w-full'>
             <button data-testid='cross' className='absolute top-4 right-4 cursor-pointer' onClick={() => updateModal()}>
               <Cross />
             </button>
-            <div className='py-2 px-4 border-b border-gray-700 text-2xl w-full h-[50px]'>{title}</div>
+            <div className='py-2 px-4 border-b border-gray-700 text-2xl w-full'>{title}</div>
             <div className='w-full h-[500px] overflow-x-hidden overflow-y-auto'>{children}</div>
-            <div className='flex justify-between items-center py-2 px-4 border-t border-gray-700 w-full h-[50px]'>
-              <div>{footerLeft}</div>
+            <div className='flex justify-between items-center py-2 px-4 flex-wrap border-t border-gray-700 w-full '>
+              <div className='my-1'>{footerLeft}</div>
               <div>{footerRight}</div>
             </div>
           </div>
