3f2c37b4433d0bee8c096fff4fdf85c4691eef2e
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2022-11-11 13:56:54 -0500

    Update ToolTipNew.js

--- components/Utils/ToolTipNew.js
+++ components/Utils/ToolTipNew.js
@@ -16,7 +16,7 @@ const ToolTipNew = ({
       {children}
       <div className={`justify-center w-full relative hidden z-50 group-hover:block  ${outerStyles} `}>
         <div className='flex flex-col items-center'>
-          <div className={`flex mt-1 tooltip-triangle absolute ${triangleStyles}`}></div>
+          <div className={`flex mt-0.5 md:mt-1 tooltip-triangle absolute ${triangleStyles}`}></div>
           <div className={`flex tooltip absolute ${relativePosition}`}>
             <div className={`${innerStyles}`}>{toolTipText}</div>
           </div>
