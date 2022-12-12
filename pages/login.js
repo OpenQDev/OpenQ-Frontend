9d1bd7770044c5316e3ab2829d2d36a511f5174a
Christopher Stevers <christopher.stevers@protonmail.com>
2022-12-12 14:59:03 -0500

    added small margin

--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,7 @@ const Login = () => {
         <Image src='/SpaceScapes/login.png' width={1000} height={200} />
       </div>
       <div className='text-xl w-full xl:w-1/2 text-center flex  gap-4 justify-center items-center   justify-items-center content-center'>
-        <div className='w-96 space-y-4'>
+        <div className='w-96 space-y-4 mx-4'>
           <h1 className='my-2 text-2xl'>Let's get you started!</h1>
           <EmailLogin />
           <div className='text-muted'>or</div>
