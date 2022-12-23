8bf30b6e160c49b71a9ee525cb52b4ea5a520b42
Christopher Stevers <christopher.stevers@protonmail.com>
2022-12-23 12:09:06 -0500

    fixed issue where watcing was test was throwing errors

--- __tests__/User/AboutModules/Watching.test.js
+++ __tests__/User/AboutModules/Watching.test.js
@@ -6,7 +6,6 @@ import { render, screen } from '../../../test-utils';
 import Watching from '../../../components/User/WatchingTab/Watching';
 import userEvent from '@testing-library/user-event';
 import Constants from '../../../test-utils/constant';
-import { waitFor } from '@testing-library/react';
 
 describe('Watching', () => {
   const bounties = Constants.bounties;
@@ -20,43 +19,26 @@ describe('Watching', () => {
       disconnect,
     }));
   });
-  /* it('should render Watching', async () => {
-    // ARRANGE
-    render(<Watching watchedBounties={bounties} complete={true} />);
 
-    // ASSERT
-    await waitFor(async () => {
-      bounties.forEach(async (bounty) => {
-        const orgName = await screen.findByText(`${bounty.owner.toLowerCase()}/${bounty.repoName.toLowerCase()}`);
-        // ACT
-        expect(orgName).toBeInTheDocument();
-      });
-
-      const nullish = [...screen.queryAllByRole(/null/), ...screen.queryAllByRole(/undefined/)];
-      expect(nullish).toHaveLength(0);
-    });
-  });
-*/
   it('should let user open BountyCardDetailsModal', async () => {
     // ARRANGE
     const user = userEvent.setup();
     render(<Watching watchedBounties={bounties} complete={true} />);
 
     // ASSERT
-    await waitFor(async () => {
-      bounties.forEach(async (bounty) => {
-        const orgName = await screen.findByText(`${bounty.owner.toLowerCase()}/${bounty.repoName.toLowerCase()}`);
-        await user.click(orgName);
-        if (bounty.status == 1) {
-          const bountyStatus = await screen.findAllByText(/Closed/i);
-          expect(bountyStatus[0]).toBeInTheDocument();
-        }
-        const link = await screen.findAllByText(/Full Contract/i);
-        expect(link[0]).toBeInTheDocument();
-        // should not have null or undefined values
-        const nullish = [...screen.queryAllByRole(/null/), ...screen.queryAllByRole(/undefined/)];
-        expect(nullish).toHaveLength(0);
-      });
-    });
+    for (let i = 0; i < bounties.length; i++) {
+      const bounty = bounties[i];
+      const orgName = await screen.findAllByText(`${bounty.owner.toLowerCase()}/${bounty.repoName.toLowerCase()}`);
+      await user.click(orgName[i]);
+      if (bounty.status == 1) {
+        const bountyStatus = await screen.findAllByText(/Closed/i);
+        expect(bountyStatus[0]).toBeInTheDocument();
+      }
+      const link = await screen.findAllByText(/Full Contract/i);
+      expect(link[0]).toBeInTheDocument();
+      // should not have null or undefined values
+      const nullish = [...screen.queryAllByRole(/null/), ...screen.queryAllByRole(/undefined/)];
+      expect(nullish).toHaveLength(0);
+    }
   });
 });
