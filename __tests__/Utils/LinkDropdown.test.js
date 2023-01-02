932f57d565ebeefa9ed236935ebb2a180d539dc9
ArcAnya <arcanyatwitch@gmail.com>
2023-01-02 15:48:44 +0100

    part of Utils snapshot tests

--- __tests__/Utils/LinkDropdown.test.js
+++ __tests__/Utils/LinkDropdown.test.js
@@ -2,36 +2,18 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { render, screen } from '../../test-utils';
 import LinkDropdown from '../../components/Utils/LinkDropdown';
-import nextRouter from 'next/router';
-// Test cases for full balances, empty balances, and undefined balances.
-
-nextRouter.useRouter = jest.fn();
-const push = jest.fn(() => {
-  return { catch: jest.fn };
-});
+import renderer from 'react-test-renderer';
 
 describe('LinkDropdown', () => {
   // Test cases for
 
-  beforeEach(() => {
-    nextRouter.useRouter.mockImplementation(() => ({
-      query: { type: null },
-      prefetch: jest.fn(() => {
-        return { catch: jest.fn };
-      }),
-      push,
-    }));
-  });
   const items = [
     { name: 'View', url: '/' },
     { name: 'Fund', url: '/asd' },
   ];
-  it('should display LinkDropdown', async () => {
-    // ARRANGE
-    render(<LinkDropdown items={items} />);
-    // ASSERT
-    expect(screen.getAllByTestId('link')[0]).toBeInTheDocument();
+  it('should match DOM Snapshot', () => {
+    const tree = renderer.create(<LinkDropdown items={items} />);
+    expect(tree.toJSON()).toMatchSnapshot('and should display LinkDropdown');
   });
 });
--- __tests__/Utils/Toggle.test.js
+++ __tests__/Utils/Toggle.test.js
@@ -5,30 +5,24 @@ import React from 'react';
 import { render, screen } from '../../test-utils';
 import Toggle from '../../components/Utils/Toggle';
 import userEvent from '@testing-library/user-event';
+import renderer from 'react-test-renderer';
 // Test cases for full balances, empty balances, and undefined balances.
 
 describe('Toggle', () => {
   // Test cases for
   const mockToggle = jest.fn();
-  beforeEach(() => {});
   const names = ['foo', 'bar'];
 
-  it('should display toggle', async () => {
-    // ARRANGE
-    render(<Toggle toggleFunc={mockToggle} names={names} toggleVal={names[0]} />);
-    // ASSERT
-    expect(screen.getByText(names[0])).toHaveClass('bg-[#21262d]');
-    expect(screen.getByText(names[1])).not.toHaveClass('bg-[#21262d]');
+  it('should match DOM Snapshot & foo should have class bg-[#21262d] while bar should not', () => {
+    const tree = renderer.create(<Toggle toggleFunc={mockToggle} names={names} toggleVal={names[0]} />);
+    expect(tree.toJSON()).toMatchSnapshot();
   });
-  it('should display toggle', async () => {
-    // ARRANGE
-    render(<Toggle toggleFunc={mockToggle} names={names} toggleVal={names[1]} />);
-    // ASSERT
-    expect(screen.getByText(names[0])).not.toHaveClass('bg-[#21262d]');
-    expect(screen.getByText(names[1])).toHaveClass('bg-[#21262d]');
+  it('should match DOM Snapshot & foo should not have class bg-[#21262d] while bar should', () => {
+    const tree = renderer.create(<Toggle toggleFunc={mockToggle} names={names} toggleVal={names[1]} />);
+    expect(tree.toJSON()).toMatchSnapshot();
   });
 
-  it('should display toggle', async () => {
+  it('should call toggleFunc', async () => {
     // ARRANGE
     const user = userEvent.setup();
     render(<Toggle toggleFunc={mockToggle} names={names} toggleVal={names[1]} />);
