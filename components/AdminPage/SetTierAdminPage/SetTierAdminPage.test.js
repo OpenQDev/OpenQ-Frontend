166ad9b12746c6ae7efead91e18eeb4ab4bf10b5
Christopher Stevers <christopher.stevers@protonmail.com>
2023-01-03 14:38:58 -0500

    fixed issues with errors being thrown by SetTierAdminPage tests

--- components/AdminPage/SetTierAdminPage/SetTierAdminPage.test.js
+++ components/AdminPage/SetTierAdminPage/SetTierAdminPage.test.js
@@ -5,7 +5,7 @@ import InitialState from '../../../store/Store/InitialState';
 import Constants from '../../../test-utils/constant';
 import userEvent from '@testing-library/user-event';
 import MockOpenQClient from '../../../services/ethers/MockOpenQClient';
-
+import { waitFor } from '@testing-library/react';
 describe('SetTierAdminPage', () => {
   beforeEach(() => {
     InitialState.openQClient.reset();
@@ -16,6 +16,36 @@ describe('SetTierAdminPage', () => {
       disconnect,
     }));
   });
+
+  it('should render nothing if bounty type is 0', async () => {
+    const { asFragment } = render(<SetTierAdminPage bounty={Constants.bounty0} />);
+
+    await waitFor(() => {
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+  it('should render nothing if bounty type is 1', async () => {
+    const { asFragment } = render(<SetTierAdminPage bounty={Constants.bounty1} />);
+    await waitFor(() => {
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+  it('should match DOM and render "Weight per Tier (%)", "1st place winner" etc. if bounty type is 2', async () => {
+    const { asFragment } = render(<SetTierAdminPage bounty={Constants.bounty2} />);
+    await waitFor(() => {
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  it('should match DOM and render "Which token?", "Volumes:" if bounty type is 3', async () => {
+    await waitFor(async () => {
+      const { asFragment } = render(<SetTierAdminPage bounty={Constants.bounty3} />);
+
+      expect(asFragment()).toMatchSnapshot();
+      await expect(screen.findByText(/Which token\?/)).resolves.toBeInTheDocument();
+    });
+  });
+
   it('should allow user to update budget', async () => {
     const bounty = Constants.bounty2;
     // ARRANGE
@@ -25,35 +55,37 @@ describe('SetTierAdminPage', () => {
       ...InitialState,
       openQClient: new MockOpenQClient({ setTier }),
     };
-    render(<SetTierAdminPage refreshBounty={() => {}} bounty={bounty} />, {}, customInitialState);
+    await waitFor(async () => {
+      render(<SetTierAdminPage refreshBounty={() => {}} bounty={bounty} />, {}, customInitialState);
 
-    // ACT
-    await user.click(screen.getByRole('button', { name: /visual/i }));
-    const textBoxes = screen.getAllByRole('textbox');
+      // ACT
+      await user.click(screen.getByRole('button', { name: /visual/i }));
+      const textBoxes = screen.getAllByRole('textbox');
 
-    await user.type(textBoxes[1], '{backspace}50');
+      await user.type(textBoxes[1], '{backspace}50');
 
-    await user.type(textBoxes[2], '{backspace}30');
+      await user.type(textBoxes[2], '{backspace}30');
 
-    await user.type(textBoxes[3], '{backspace}20');
+      await user.type(textBoxes[3], '{backspace}20');
 
-    await user.click(await screen.findByRole('button', { name: 'Set New Payout Schedule' }));
+      await user.click(await screen.findByRole('button', { name: 'Set New Payout Schedule' }));
 
-    // ASSERT
-    expect(await screen.findByText(/Updating Payout Schedule.../)).toBeInTheDocument();
-    expect(screen.getByText(/our request is being processed.../)).toBeInTheDocument();
+      // ASSERT
+      expect(await screen.findByText(/Updating Payout Schedule.../)).toBeInTheDocument();
+      expect(screen.getByText(/our request is being processed.../)).toBeInTheDocument();
 
-    expect(await screen.findByText(/Updating Payout.../)).toBeInTheDocument();
-    expect(screen.getByText(/our request is being processed.../)).toBeInTheDocument();
-    expect(await screen.findByText(/The payout schedule for this issue has been updated./i)).toBeInTheDocument();
-    expect(await screen.findByText(/payout schedule set to/i)).toBeInTheDocument();
-    expect(await screen.findByText(/1st winner:/i)).toBeInTheDocument();
-    expect(await screen.findByText(/2nd winner:/i)).toBeInTheDocument();
-    expect(await screen.findByText(/3rd winner:/i)).toBeInTheDocument();
-    expect(await screen.findByText(/^50.%$/i)).toBeInTheDocument();
-    expect(await screen.findByText(/^30.%$/i)).toBeInTheDocument();
-    expect(await screen.findByText(/^20.%$/i)).toBeInTheDocument();
-    expect(setTier).toBeCalledWith(bounty.bountyId, [50, 30, 20]);
+      expect(await screen.findByText(/Updating Payout.../)).toBeInTheDocument();
+      expect(screen.getByText(/our request is being processed.../)).toBeInTheDocument();
+      expect(await screen.findByText(/The payout schedule for this issue has been updated./i)).toBeInTheDocument();
+      expect(await screen.findByText(/payout schedule set to/i)).toBeInTheDocument();
+      expect(await screen.findByText(/1st winner:/i)).toBeInTheDocument();
+      expect(await screen.findByText(/2nd winner:/i)).toBeInTheDocument();
+      expect(await screen.findByText(/3rd winner:/i)).toBeInTheDocument();
+      expect(await screen.findByText(/^50.%$/i)).toBeInTheDocument();
+      expect(await screen.findByText(/^30.%$/i)).toBeInTheDocument();
+      expect(await screen.findByText(/^20.%$/i)).toBeInTheDocument();
+      expect(setTier).toBeCalledWith(bounty.bountyId, [50, 30, 20]);
+    });
   });
   it('should allow user to update tiers on fixed bounty', async () => {
     const bounty = Constants.bounty3;
@@ -64,51 +96,35 @@ describe('SetTierAdminPage', () => {
       ...InitialState,
       openQClient: new MockOpenQClient({ setTier }),
     };
-    render(<SetTierAdminPage refreshBounty={() => {}} bounty={bounty} />, {}, customInitialState);
+    await waitFor(async () => {
+      render(<SetTierAdminPage refreshBounty={() => {}} bounty={bounty} />, {}, customInitialState);
 
-    // ACT
-    const textBoxes = screen.getAllByRole('textbox');
+      // ACT
+      const textBoxes = screen.getAllByRole('textbox');
 
-    await user.type(textBoxes[1], '{backspace}50');
+      await user.type(textBoxes[1], '{backspace}50');
 
-    await user.type(textBoxes[2], '{backspace}30');
+      await user.type(textBoxes[2], '{backspace}30');
 
-    await user.type(textBoxes[3], '{backspace}20');
+      await user.type(textBoxes[3], '{backspace}20');
 
-    await user.click(await screen.findByRole('button', { name: 'Set New Payout Schedule' }));
+      await user.click(await screen.findByRole('button', { name: 'Set New Payout Schedule' }));
 
-    // ASSERT
-    expect(await screen.findByText(/Updating Payout Schedule.../)).toBeInTheDocument();
-    expect(screen.getByText(/our request is being processed.../)).toBeInTheDocument();
+      // ASSERT
+      expect(await screen.findByText(/Updating Payout Schedule.../)).toBeInTheDocument();
+      expect(screen.getByText(/our request is being processed.../)).toBeInTheDocument();
 
-    expect(await screen.findByText(/Updating Payout.../)).toBeInTheDocument();
-    expect(screen.getByText(/our request is being processed.../)).toBeInTheDocument();
-    expect(await screen.findByText(/The payout schedule for this issue has been updated./i)).toBeInTheDocument();
-    expect(await screen.findByText(/payout schedule set to/i)).toBeInTheDocument();
-    expect(await screen.findByText(/1st winner:/i)).toBeInTheDocument();
-    expect(await screen.findByText(/2nd winner:/i)).toBeInTheDocument();
-    expect(await screen.findByText(/3rd winner:/i)).toBeInTheDocument();
-    expect(await screen.findByText(/^50 Matic/i)).toBeInTheDocument();
-    expect(await screen.findByText(/^30 Matic/i)).toBeInTheDocument();
-    expect(await screen.findByText(/^20 Matic/i)).toBeInTheDocument();
-    expect(setTier).toBeCalledWith(bounty.bountyId, [50, 30, 20], Constants.maticAddress);
+      expect(await screen.findByText(/Updating Payout.../)).toBeInTheDocument();
+      expect(screen.getByText(/our request is being processed.../)).toBeInTheDocument();
+      expect(await screen.findByText(/The payout schedule for this issue has been updated./i)).toBeInTheDocument();
+      expect(await screen.findByText(/payout schedule set to/i)).toBeInTheDocument();
+      expect(await screen.findByText(/1st winner:/i)).toBeInTheDocument();
+      expect(await screen.findByText(/2nd winner:/i)).toBeInTheDocument();
+      expect(await screen.findByText(/3rd winner:/i)).toBeInTheDocument();
+      expect(await screen.findByText(/^50 Matic/i)).toBeInTheDocument();
+      expect(await screen.findByText(/^30 Matic/i)).toBeInTheDocument();
+      expect(await screen.findByText(/^20 Matic/i)).toBeInTheDocument();
+      expect(setTier).toBeCalledWith(bounty.bountyId, [50, 30, 20], Constants.maticAddress);
+    });
   });
-
-  /*
-  it('should render nothing if bounty type is 0', () => {
-    const { asFragment } = render(<SetTierAdminPage bounty={Constants.bounty0} />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-  it('should render nothing if bounty type is 1', () => {
-    const { asFragment } = render(<SetTierAdminPage bounty={Constants.bounty1} />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-  it('should match DOM and render "Weight per Tier (%)", "1st place winner" etc. if bounty type is 2', () => {
-    const { asFragment } = render(<SetTierAdminPage bounty={Constants.bounty2} />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-  it('should match DOM and render "Which token?", "Volumes:" if bounty type is 3', () => {
-    const { asFragment } = render(<SetTierAdminPage bounty={Constants.bounty3} />);
-    expect(asFragment()).toMatchSnapshot();
-  });*/
 });
