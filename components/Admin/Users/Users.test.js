c800890c38a8fb3c3e5871ca6ed7c12063e8063e
Christopher Stevers <christopher.stevers@protonmail.com>
2023-01-17 14:37:16 -0500

    updated tests

--- components/Admin/Users/Users.test.js
+++ components/Admin/Users/Users.test.js
@@ -4,19 +4,13 @@
 import React from 'react';
 import { render, screen } from '../../../test-utils';
 import Users from '.';
-import userEvent from '@testing-library/user-event';
 
 describe('Users', () => {
   it('should display the Users', async () => {
-    const user = userEvent.setup();
     // ARRANGE
     render(<Users />);
 
     // ASSERT
-    expect(screen.getByText('To view user data please input OPENQ_API_SECRET')).toBeInTheDocument();
-    const secretField = screen.getByLabelText(/OPENQ_API_SECRET/i);
-    await user.type(secretField, 'testSecret');
-    await user.click(screen.getByRole('button', { name: 'Submit' }));
     expect(await screen.findByText(/Github/i)).toBeInTheDocument();
     expect(await screen.findByText(/discord/i)).toBeInTheDocument();
     expect(await screen.findByText(/Created At/)).toBeInTheDocument();
