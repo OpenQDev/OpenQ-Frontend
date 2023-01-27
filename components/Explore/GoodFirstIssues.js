d548e979b12564f620ae952e279904b03b387fc0
mktcode <kontakt@markus-kottlaender.de>
2023-01-27 19:53:26 +0100

    read issues via gun

--- components/Explore/GoodFirstIssues.js
+++ components/Explore/GoodFirstIssues.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Gun from 'gun/gun';
 import { ChevronRightIcon } from '@primer/octicons-react';
 import Image from 'next/image';
 import FancyButton from './FancyButton';
@@ -8,7 +9,34 @@ import CardFooter from './Card/CardFooter';
 import CardHeader from './Card/CardHeader';
 import StarButton from './StarButton';
 
+import reposWhitelist from './reposWhitelist.json';
+
+const gun = new Gun({
+  peers: ['http://192.168.178.29:4200/gun', 'https://phorum-relay.mktcode.uber.space/gun'],
+});
+
 export default function GoodFirstIssues() {
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    reposWhitelist.forEach((ownerAndRepo) => {
+      gun.get(ownerAndRepo).once((repo) => {
+        try {
+          const repoIssues = JSON.parse(repo.issuesJson);
+          setIssues((currentIssues) => {
+            const issuesWithDuplicates = [...currentIssues, ...repoIssues];
+            const uniqueIssues = issuesWithDuplicates.filter((issue, index, self) => {
+              return self.findIndex((i) => i.id === issue.id) === index;
+            });
+            return uniqueIssues;
+          });
+        } catch (e) {
+          console.log('error parsing issues coming from gun', e, repo.issuesJson);
+        }
+      });
+    });
+  }, []);
+
   return (
     <div className='my-24 w-full'>
       <div className='text-center mb-10'>
@@ -20,8 +48,8 @@ export default function GoodFirstIssues() {
         </FancyButton>
       </div>
       <div className='border border-dark-1 rounded-lg p-5 bg-dark-3 flex space-x-5 overflow-x-auto custom-scrollbar custom-scrollbar-horizontal snap-x snap-mandatory'>
-        {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
-          <Card key={i}>
+        {issues.map((issue) => (
+          <Card key={issue.id}>
             <CardHeader>
               <Image
                 src='https://avatars.githubusercontent.com/u/77402538?v=4'
@@ -30,7 +58,7 @@ export default function GoodFirstIssues() {
                 height={24}
                 className='mr-2 rounded-full'
               />
-              <div className='pr-3 mr-auto'>OpenQ</div>
+              <div className='pr-3 mr-auto'>{issue.title}</div>
               <StarButton />
             </CardHeader>
             <CardBody>asd</CardBody>
