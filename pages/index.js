83a14f9af65fad477fdf357bb600a6e70ec44f27
mktcode <kontakt@markus-kottlaender.de>
2023-01-30 03:25:46 +0100

    re-added fetching bounties

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import WrappedGithubClient from '../services/github/WrappedGithubClient';
+import WrappedOpenQSubgraphClient from '../services/subgraph/WrappedOpenQSubgraphClient';
+import WrappedOpenQPrismaClient from '../services/openq-api/WrappedOpenQPrismaClient';
+import Utils from '../services/utils/Utils';
+import Logger from '../services/logger/Logger';
+import UnexpectedErrorModal from '../components/Utils/UnexpectedErrorModal';
 import ExploreHeader from '../components/Explore/Header';
 import ExploreSearch from '../components/Explore/Search';
 import ExploreHackathons from '../components/Explore/Hackathons';
@@ -7,18 +13,56 @@ import ExploreGoodFirstIssues from '../components/Explore/GoodFirstIssues';
 import ExploreNewsletter from '../components/Explore/Newsletter';
 import ExploreBlog from '../components/Explore/Blog';
 
-export default function Index() {
+export default function Index({ fullBounties, renderError }) {
+  console.log('fullBounties', fullBounties);
   return (
     <main className='bg-dark-mode flex-col explore'>
-      <ExploreHeader />
-      <div className='flex flex-col items-center max-w-screen-2xl mx-auto px-5 lg:px-10'>
-        <ExploreSearch />
-        <ExploreHackathons />
-        <ExploreMarketplace />
-        <ExploreGoodFirstIssues />
-        <ExploreNewsletter />
-        <ExploreBlog />
-      </div>
+      {renderError ? (
+        <UnexpectedErrorModal error={renderError} />
+      ) : (
+        <>
+          <ExploreHeader />
+          <div className='flex flex-col items-center max-w-screen-2xl mx-auto px-5 lg:px-10'>
+            <ExploreSearch />
+            <ExploreHackathons />
+            <ExploreMarketplace />
+            <ExploreGoodFirstIssues />
+            <ExploreNewsletter />
+            <ExploreBlog />
+          </div>
+        </>
+      )}
     </main>
   );
 }
+
+export const getServerSideProps = async () => {
+  const githubRepository = new WrappedGithubClient();
+  const openQSubgraphClient = new WrappedOpenQSubgraphClient();
+  const openQPrismaClient = new WrappedOpenQPrismaClient();
+  const utils = new Utils();
+  const logger = new Logger();
+  const batch = 10;
+  let fullBounties = [];
+  let renderError = '';
+  try {
+    [fullBounties] = await utils.fetchBounties(
+      {
+        openQSubgraphClient: openQSubgraphClient.instance,
+        githubRepository: githubRepository.instance,
+        openQPrismaClient: openQPrismaClient.instance,
+      },
+      batch
+    );
+  } catch (err) {
+    logger.error(err, null, '[index]3.js');
+    renderError = JSON.stringify(err);
+  }
+
+  return {
+    props: {
+      fullBounties,
+      renderError,
+    },
+  };
+};
