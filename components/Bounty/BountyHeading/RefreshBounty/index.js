8004776793b94c8351e87d4290168a0f3d18f4b4
Anya <75732239+ArcAnya@users.noreply.github.com>
2023-01-31 09:25:11 +0000

    adding tooltip to RefreshButton

--- components/Bounty/BountyHeading/RefreshBounty/index.js
+++ components/Bounty/BountyHeading/RefreshBounty/index.js
@@ -3,6 +3,7 @@ import React, { useContext, useState } from 'react';
 import StoreContext from '../../../../store/Store/StoreContext';
 import LoadingIcon from '../../../Loading/ButtonLoadingIcon';
 import Github from '../../../svg/github';
+import ToolTipNew from '../../../Utils/ToolTipNew';
 const RefreshBounty = ({ bounty, refreshGithubBounty }) => {
   const [appState] = useContext(StoreContext);
   const [completed, setCompleted] = useState(true);
@@ -28,13 +29,20 @@ const RefreshBounty = ({ bounty, refreshGithubBounty }) => {
   // get github proxy with id in query params use axios
   return (
     <>
-      <button
-        onClick={handleRefresh}
-        className={` whitespace-nowrap btn-primary flex flex-row space-x-3 items-center justify-center leading-tight sm:w-min px-3 h-8`}
+      <ToolTipNew
+        relativePosition={'w-40'}
+        outerStyles={'relative bottom-1'}
+        innerStyles={'whitespace-normal'}
+        toolTipText={'Fetch latest issue description from GitHub.'}
       >
-        {completed ? <Github className={'w-6 h-4'} /> : <LoadingIcon />}
-        <div>Refresh</div>
-      </button>
+        <button
+          onClick={handleRefresh}
+          className={` whitespace-nowrap btn-primary flex flex-row space-x-3 items-center justify-center leading-tight sm:w-min px-3 h-8`}
+        >
+          {completed ? <Github className={'w-6 h-4'} /> : <LoadingIcon />}
+          <div>Refresh</div>
+        </button>
+      </ToolTipNew>
     </>
   );
 };
