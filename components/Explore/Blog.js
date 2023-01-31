e009eaf2c9bf94564581ad486c1d7b32955a5c07
mktcode <kontakt@markus-kottlaender.de>
2023-01-31 14:08:59 +0100

    added blog elements

--- components/Explore/Blog.js
+++ components/Explore/Blog.js
@@ -1,32 +1,55 @@
 import React from 'react';
-import Image from 'next/image';
-import Card from './Card/Card';
-import CardBody from './Card/CardBody';
-import CardFooter from './Card/CardFooter';
-import CardHeader from './Card/CardHeader';
-import StarButton from './StarButton';
+import Link from 'next/link';
+import { ChevronRightIcon } from '@primer/octicons-react';
+import ImageTeaser from './ImageTeaser/ImageTeaser';
+import ImageTeaserHeadline from './ImageTeaser/ImageTeaserHeadline';
+import ImageTeaserLink from './ImageTeaser/ImageTeaserLink';
+import ImageTeaserText from './ImageTeaser/ImageTeaserText';
 
 export default function Blog() {
+  const articles = [
+    {
+      title: 'Trust me bro',
+      url: 'https://medium.com/openqdev/trust-me-bro-8fd8cf0a0e11',
+      imageSrc: '/explore/blog-1.png',
+      teaserText: 'How to get along with people you’ve never met',
+    },
+    {
+      title: 'Web 2.0 DAO — Part I',
+      url: 'https://medium.com/openqdev/web-2-0-dao-part-i-4cec53dd0ec3',
+      imageSrc: '/explore/blog-2.png',
+      teaserText: 'How I’d (Fail) To Build a DAO in 2010',
+    },
+    {
+      title: 'Web 2.0 DAO — Part II',
+      url: 'https://medium.com/openqdev/web-2-0-dao-part-ii-6ac736d899d6',
+      imageSrc: '/explore/blog-3.png',
+      teaserText: 'How I’d (Successfully) Build a DAO in 2022',
+    },
+    {
+      title: 'The Legend of the Code Vending Machine',
+      url: 'https://medium.com/openqdev/the-legend-of-the-code-vending-machine-b30d3d573c4f',
+      imageSrc: '/explore/blog-4.png',
+      teaserText: 'Why no code is still a no go',
+    },
+  ]
   return (
     <div className='my-24 w-full'>
       <h1 className='text-center mb-10 mx-auto'>What we are thinking about</h1>
       <div className='border border-dark-1 rounded-lg p-5 bg-dark-3 flex space-x-5 overflow-x-auto custom-scrollbar custom-scrollbar-horizontal snap-x snap-mandatory'>
-        {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
-          <Card key={i}>
-            <CardHeader>
-              <Image
-                src='https://avatars.githubusercontent.com/u/77402538?v=4'
-                alt='OpenQ'
-                width={24}
-                height={24}
-                className='mr-2 rounded-full'
-              />
-              <div className='pr-3 mr-auto'>OpenQ</div>
-              <StarButton />
-            </CardHeader>
-            <CardBody>asd</CardBody>
-            <CardFooter>asd</CardFooter>
-          </Card>
+        {articles.map((article) => (
+          <Link key={article.title} href={article.url}>
+            <ImageTeaser imageSrc={article.imageSrc}>
+              <ImageTeaserLink>
+                Learn more
+                <ChevronRightIcon className='ml-2 w-5 h-5' />
+              </ImageTeaserLink>
+              <ImageTeaserHeadline>{article.title}</ImageTeaserHeadline>
+              <ImageTeaserText>
+                {article.teaserText}
+              </ImageTeaserText>
+            </ImageTeaser>
+          </Link>
         ))}
       </div>
     </div>
