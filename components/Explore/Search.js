0326c00c32867792ee965fb7dab111e3d439206c
mktcode <kontakt@markus-kottlaender.de>
2023-02-02 22:09:39 +0100

    added search placeholder

--- components/Explore/Search.js
+++ components/Explore/Search.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Search() {
   return (
     <div className='flex bg-dark-2 bg-opacity-75 px-3 py-2 w-full border border-dark-1 rounded-sm space-x-3'>
-      <input className='bg-dark-4 border border-dark-1 rounded-sm px-3 py-2 grow min-w-0 outline-none' />
+      <input className='bg-dark-4 border border-dark-1 rounded-sm px-3 py-2 grow min-w-0 outline-none' placeholder='Search' />
       <button>Ctrl K</button>
     </div>
   );
