b736c00b69170bfe79dd9582d1a606ddaa63986d
mktcode <kontakt@markus-kottlaender.de>
2023-02-09 17:37:06 +0100

    styling

--- components/Explore/Card/CardHeader.js
+++ components/Explore/Card/CardHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CardHeader({ children }) {
   return (
-    <div className='flex items-center justify-center border-b border-dark-1 px-6 py-3 bg-dark-2 text-sm'>
+    <div className='flex items-center justify-center border-b border-dark-1 px-4 py-2 md:px-6 md:py-3 bg-dark-2 text-sm'>
       {children}
     </div>
   );
--- components/Explore/GoodFirstIssues.js
+++ components/Explore/GoodFirstIssues.js
@@ -47,16 +47,16 @@ export default function GoodFirstIssues() {
     <div className='w-full pt-12 lg:pt-40'>
       <div className='text-center mb-10'>
         <h1 className='mx-auto'>Contribute to Open-Source in web3.</h1>
-        <h3 className='text-3xl text-zinc-400 mt-4'>Find good first issues.</h3>
+        <h3 className='text-2xl sm:text-3xl text-zinc-400 mt-4'>Find good first issues.</h3>
         <FancyButton className='mt-4 mx-auto'>
           Find more
           <ChevronRightIcon className='ml-2 w-5 h-5' />
         </FancyButton>
       </div>
-      <div className='flex flex-col space-y-5 md:space-y-0 md:flex-row md:border md:border-dark-1 md:rounded-sm md:p-5 md:bg-dark-3 md:space-x-5 md:overflow-x-auto md:custom-scrollbar md:custom-scrollbar-horizontal md:snap-x md:snap-mandatory'>
+      <div className='flex sm:border sm:border-dark-1 sm:rounded-sm pb-2 sm:p-5 sm:bg-dark-3 space-x-5 overflow-x-auto custom-scrollbar custom-scrollbar-horizontal'>
         {issues.map((issue) => (
-          <Link key={issue.id} href={issue.url} target='_blank'>
-            <Card className='md:!w-96'>
+          <Link key={issue.id} href={issue.url} target='_blank' className='min-w-full max-w-[24rem] sm:min-w-[24rem]'>
+            <Card>
               <CardHeader>
                 <Image
                   src='https://avatars.githubusercontent.com/u/77402538?v=4'
@@ -65,19 +65,19 @@ export default function GoodFirstIssues() {
                   height={27}
                   className='mr-2 rounded-full'
                 />
-                <div className='pr-3 mr-auto text-link-colour font-bold'>{issue.repo.name}</div>
+                <div className='pr-3 mr-auto text-link-colour font-bold whitespace-nowrap'>{issue.repo.name}</div>
                 <StarButton count={issue.repo.stars} />
               </CardHeader>
               <CardBody>
-                <div className='text-xs text-gray-400'>
+                <div className='text-xs text-gray-400 truncate'>
                   <IssueOpenedIcon className='text-green-400 mr-2' />
                   {issue.title}
                 </div>
-                <div className='flex space-x-2 mt-5 mb-2'>
+                <div className='flex space-x-2 mt-3'>
                   {issue.labels.map((label) => (
                     <span
                       key={label.id}
-                      className='inline-block text-[10px] rounded-full px-2 leading-5 text-white text-opacity-75'
+                      className='inline-block text-[10px] rounded-full px-2 leading-5 text-white text-opacity-75 whitespace-nowrap'
                       style={{ backgroundColor: `#${label.color}` }}
                     >
                       {label.name}
@@ -88,9 +88,9 @@ export default function GoodFirstIssues() {
               <CardFooter>
                 <RepoLanguage language={issue.repo.language} />
                 {issue.assignee ? (
-                  <div className='text-xs text-gray-400'>Assigned to {issue.assignee}</div>
+                  <div className='text-xs text-gray-400 whitespace-nowrap'>Assigned to {issue.assignee}</div>
                 ) : (
-                  <div className='text-xs text-gray-400'>No one assigned</div>
+                  <div className='text-xs text-gray-400 whitespace-nowrap'>No one assigned</div>
                 )}
               </CardFooter>
             </Card>
