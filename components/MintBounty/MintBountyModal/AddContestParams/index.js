f3a4c029c330f7ea742467eba6a4bc40ce40b62c
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-02-09 21:52:47 +0000

    fixed bug in mintModal where default value was very low.

--- components/MintBounty/MintBountyModal/AddContestParams/index.js
+++ components/MintBounty/MintBountyModal/AddContestParams/index.js
@@ -113,7 +113,7 @@ const AddContestParams = () => {
             tierArr={tierArr}
             setSum={noop}
             setEnableContest={noop}
-            initialVolumes={['1', '1', '1']}
+            initialVolumes={['1000000000000000000', '1000000000000000000', '1000000000000000000']}
           />
         </TokenProvider>
       ) : null}
