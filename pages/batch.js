22d8f08c876c289ee7d2b2edf1f11ad142d8893d
FlacoJones <andrew@openq.dev>
2023-02-17 11:30:34 -0600

    adds copy and download button for mint bounty batch json

--- pages/batch.js
+++ pages/batch.js
@@ -2,14 +2,28 @@ import React, { useState, useContext } from 'react';
 import Papa from 'papaparse';
 import { ethers } from 'ethers';
 import StoreContext from '../store/Store/StoreContext';
-import mintBountyTemplate from "../constants/mintBountyTemplate.json"
-import mintBountyTransactionTemplate from "../constants/mintBountyTransactionTemplate.json"
+import mintBountyTemplate from '../constants/mintBountyTemplate.json';
+import mintBountyTransactionTemplate from '../constants/mintBountyTransactionTemplate.json';
 
 function CsvUploader() {
   const [csvData, setCsvData] = useState([]);
-  const [jsonData, setJsonData] = useState(null);
+  const [mintBountyBatchData, setMintBountyBatchData] = useState(null);
 
   const [appState] = useContext(StoreContext);
+
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(JSON.stringify(mintBountyBatchData));
+  };
+
+  const handleDownload = () => {
+    const element = document.createElement('a');
+    const file = new Blob([JSON.stringify(mintBountyBatchData)], { type: 'application/json' });
+    element.href = URL.createObjectURL(file);
+    element.download = 'data.json';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
 
   let abiCoder = new ethers.utils.AbiCoder();
 
@@ -27,7 +41,7 @@ function CsvUploader() {
         const jsonObject = {};
         row.forEach((cell, index) => {
           if (headers[index] === 'payoutSchedule') {
-            // Remove spaces from the Payout Schedule array
+            // Remove spaces from the payoutSchedule array
             const payouts = cell
               .split(',')
               .map((payout) => payout.trim())
@@ -39,50 +53,63 @@ function CsvUploader() {
         });
         return jsonObject;
       });
-      
-			const jsonData = JSON.stringify(jsonRows, null, 2);
-      setJsonData(jsonData);
 
-			const transactions = []
+      const jsonData = JSON.stringify(jsonRows, null, 2);
+
+      const transactions = [];
 
       const jsonDataParsed = JSON.parse(jsonData);
 
-			for (const element of jsonDataParsed) {
-				const issueUrl = element.githubIssueUrl
+      for (const element of jsonDataParsed) {
+        const issueUrl = element.githubIssueUrl;
+        const githubSponsorUrl = element.githubSponsorUrl;
 
-				const resource = await appState.githubRepository.fetchIssueByUrl(issueUrl);
+        const resource = await appState.githubRepository.fetchIssueByUrl(issueUrl);
+        const githubSponsorResource = await appState.githubRepository.getOrgByUrl(githubSponsorUrl);
 
-				const bountyId = resource.id;
-				const organizationId = resource.repository.owner.id;
-				
-				const payoutSchedule = JSON.parse(element.payoutSchedule)
-				const payoutTokenAddress = element.payoutTokenAddress
-				
-				const invoiceRequired = element.invoiceRequired
-				const kycRequired = element.kycRequired
-				const supportingDocumentsRequired = element.supportingDocumentsRequired
+        const bountyId = resource.id;
+        const organizationId = resource.repository.owner.id;
 
-				const mintBountyTransactionTemplateCopy = mintBountyTransactionTemplate
-				
-				mintBountyTransactionTemplateCopy.contractInputsValues._bountyId = bountyId
-				mintBountyTransactionTemplateCopy.contractInputsValues._organization = organizationId
+        const sponsorOrganizationName = githubSponsorResource.login;
+        const sponsorOrganizationLogo = githubSponsorResource.avatarUrl;
 
-				const initializationSchema = ['uint256[]', 'address', 'bool', 'bool', 'bool', 'string', 'string', 'string'];
-				const initializationData = [payoutSchedule, payoutTokenAddress, invoiceRequired, kycRequired, supportingDocumentsRequired, 'po', 'po', 'po'];
+        const payoutSchedule = JSON.parse(element.payoutSchedule);
+        const payoutTokenAddress = element.payoutTokenAddress;
 
-				const tieredFixedEncoded = abiCoder.encode(initializationSchema, initializationData);
-				let tieredFixed = [3, tieredFixedEncoded];
+        const invoiceRequired = element.invoiceRequired;
+        const kycRequired = element.kycRequired;
+        const supportingDocumentsRequired = element.supportingDocumentsRequired;
 
-				mintBountyTransactionTemplateCopy.contractInputsValues._initOperation = `[${tieredFixed}]`
+        const mintBountyTransactionTemplateCopy = mintBountyTransactionTemplate;
 
-				transactions.push(mintBountyTransactionTemplateCopy)
+        mintBountyTransactionTemplateCopy.contractInputsValues._bountyId = bountyId;
+        mintBountyTransactionTemplateCopy.contractInputsValues._organization = organizationId;
 
-				console.log(mintBountyTransactionTemplateCopy)
-			};
+        const initializationSchema = ['uint256[]', 'address', 'bool', 'bool', 'bool', 'string', 'string', 'string'];
+        const initializationData = [
+          payoutSchedule,
+          payoutTokenAddress,
+          invoiceRequired,
+          kycRequired,
+          supportingDocumentsRequired,
+          '63da6f261d7d7b7cad0bc19d',
+          sponsorOrganizationName,
+          sponsorOrganizationLogo,
+        ];
 
-			mintBountyTemplate.transactions = transactions
+        const tieredFixedEncoded = abiCoder.encode(initializationSchema, initializationData);
+        let tieredFixed = [3, tieredFixedEncoded];
 
-			console.log(mintBountyTemplate)
+        mintBountyTransactionTemplateCopy.contractInputsValues._initOperation = `[${tieredFixed}]`;
+
+        transactions.push(mintBountyTransactionTemplateCopy);
+
+        console.log(mintBountyTransactionTemplateCopy);
+      }
+
+      mintBountyTemplate.transactions = transactions;
+
+      setMintBountyBatchData(mintBountyTemplate);
     };
 
     reader.readAsText(file);
@@ -114,10 +141,12 @@ function CsvUploader() {
           </table>
         </div>
       )}
-      {jsonData && (
+      {mintBountyBatchData && (
         <div>
           <h2>JSON Data</h2>
-          <pre>{jsonData}</pre>
+          <pre>{JSON.stringify(mintBountyBatchData)}</pre>
+          <button onClick={handleCopyToClipboard}>Copy to Clipboard</button>
+          <button onClick={handleDownload}>Download JSON</button>
         </div>
       )}
     </div>
