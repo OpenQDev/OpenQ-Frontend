bd2b2444880703e08f4314c439e6a5eddfbbfe9a
FlacoJones <andrew@openq.dev>
2023-02-17 10:42:06 -0600

    converts csv to json

--- pages/batch.js
+++ pages/batch.js
@@ -1,24 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import Papa from 'papaparse';
+import { ethers } from 'ethers'
+import StoreContext from '../store/Store/StoreContext';
 
-const Batch = () => {
-	const [sent, setSent] = useState(false);
+function CsvUploader() {
+  const [csvData, setCsvData] = useState([]);
+  const [jsonData, setJsonData] = useState(null);
 
-	const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-    setSent(false);
+	const [appState,] = useContext(StoreContext)
+
+	let abiCoder = new ethers.utils.AbiCoder;
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const csvData = Papa.parse(event.target.result).data;
+      setCsvData(csvData);
+
+      // Convert CSV data to JSON
+      const headers = csvData[0];
+      const rows = csvData.slice(1);
+      const jsonRows = rows.map((row) => {
+        const jsonObject = {};
+        row.forEach((cell, index) => {
+          if (headers[index] === 'payoutSchedule') {
+            // Remove spaces from the Payout Schedule array
+            const payouts = cell.split(',').map((payout) => payout.trim()).join();
+            jsonObject[headers[index]] = payouts;
+          } else {
+            jsonObject[headers[index]] = cell;
+          }
+        });
+        return jsonObject;
+      });
+      const jsonData = JSON.stringify(jsonRows, null, 2);
+      setJsonData(jsonData);
+
+			const jsonDataParsed = JSON.parse(jsonData)
+
+			const resource = await appState.githubRepository.fetchIssueByUrl(jsonDataParsed[0].githubIssueUrl)
+
+			const bountyId = resource.id
+			const organizationId = resource.repository.owner.id
+    };
+
+    reader.readAsText(file);
   };
 
   return (
     <div>
-			<h1>BATCH</h1>
-			<input
-                onChange={handleFileChange}
-                disabled={sent}
-                type='file'
-                className='absolute invisible w-full top-0 bottom-0 z-10'
-                id='file input'
-              />
-		</div>
+      <input type="file" onChange={handleFileUpload} />
+      {csvData.length > 0 && (
+        <div>
+          <h2>CSV Data</h2>
+          <table>
+            <thead>
+              <tr>
+                {csvData[0].map((header) => (
+                  <th key={header}>{header}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {csvData.slice(1).map((row, index) => (
+                <tr key={index}>
+                  {row.map((cell, index) => (
+                    <td key={index}>{cell}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {jsonData && (
+        <div>
+          <h2>JSON Data</h2>
+          <pre>{jsonData}</pre>
+        </div>
+      )}
+    </div>
   );
-};
-export default Batch;
+}
+
+export default CsvUploader;
