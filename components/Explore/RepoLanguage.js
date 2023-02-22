a96be94a201493f55924aa909247e76d319432b4
mktcode <kontakt@markus-kottlaender.de>
2023-02-22 16:33:13 +0100

    linting

--- components/Explore/RepoLanguage.js
+++ components/Explore/RepoLanguage.js
@@ -3,14 +3,12 @@ import React from 'react';
 export default function Index({ language }) {
   return (
     <div className='flex items-center mr-3'>
-      {
-        (language) && (
-          <>
-            <span className='inline-block w-3 h-3 mr-1 rounded-full' style={{ backgroundColor: language.color }} />
-            {language.name}
-          </>
-        )
-      }
+      {language && (
+        <>
+          <span className='inline-block w-3 h-3 mr-1 rounded-full' style={{ backgroundColor: language.color }} />
+          {language.name}
+        </>
+      )}
     </div>
   );
 }
