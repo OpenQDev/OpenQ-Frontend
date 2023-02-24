1ad57a7886b4ed63123d0ed14e7ff6e747f97271
Anya <75732239+ArcAnya@users.noreply.github.com>
2023-02-24 09:33:56 +0000

    make sure image has enough padding aruond on explore page

--- components/Explore/Header.js
+++ components/Explore/Header.js
@@ -5,7 +5,7 @@ import { ChevronRightIcon } from '@primer/octicons-react';
 
 export default function Header() {
   return (
-    <header className='flex justify-between items-center max-w-screen-2xl mx-auto lg:pt-24 lg:pb-6 px-4 md:px-12 lg:px-24 relative z-0'>
+    <header className='flex justify-between items-center max-w-screen-2xl mx-auto lg:pt-8 lg:pb-6 px-4 md:px-12 lg:px-24 relative z-0 md:min-h-[590px]'>
       <div className='lg:pr-10 z-10'>
         <Link href='https://medium.com/openqdev/introducing-openq-on-polygon-1b096f74e949' target='_blank'>
           <div className='flex items-center bg-dark-4 bg-opacity-70 border border-gray-600 rounded-md sm:rounded-full px-3 md:px-6 py-2 mt-5 max-w-2xl'>
@@ -31,7 +31,7 @@ export default function Header() {
         </p>
       </div>
       <Image
-        className='opacity-10 md:opacity-30 xl:opacity-100 absolute -right-50 top-0 sm:right-0 sm:-top-10 lg:right-20 lg:top-10 z-0 sm:min-w-[450px]'
+        className='opacity-10 md:opacity-30 xl:opacity-100 absolute xl:static -right-50 top-0 sm:right-0 sm:-top-10 lg:right-20 lg:top-10 z-0 min-w-[450px]'
         src='/explore/planet.png'
         alt=''
         width={571}
