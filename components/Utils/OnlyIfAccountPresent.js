b3141951e364db5324a7888d234a01d0f04f0b31
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-02-28 23:46:09 +0000

    added correct network to OnlyIFAccountPresent

--- components/Utils/OnlyIfAccountPresent.js
+++ components/Utils/OnlyIfAccountPresent.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import useIsOnCorrectNetwork from '../../hooks/useIsOnCorrectNetwork';
 import useWeb3 from '../../hooks/useWeb3';
 import ConnectButton from '../WalletConnect/ConnectButton';
 
 const OnlyIfAccountPresent = ({ children }) => {
   const { account } = useWeb3();
-  if (account) return children;
+  const [isOnCorrectNetwork] = useIsOnCorrectNetwork();
+  if (account && isOnCorrectNetwork) return children;
   return <ConnectButton />;
 };
 export default OnlyIfAccountPresent;
