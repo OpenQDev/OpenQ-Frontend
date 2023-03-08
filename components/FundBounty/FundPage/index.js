830ee6206be0cacd71148c8abed9eb2ac96b1587
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-03-08 19:50:52 +0000

    removed crowdfunding entirely

--- components/FundBounty/FundPage/index.js
+++ components/FundBounty/FundPage/index.js
@@ -255,8 +255,7 @@ const FundPage = () => {
             </div>
           ) : (
             <div className='p-8 sm:p-0'>
-              Sorry crowdfunding isn't available for invoiceable bounties such as this one, please connect the account (
-              {shortenAddress(bounty.issuer.id)}) that minted this bounty to fund it.
+              Please connect the account ({shortenAddress(bounty.issuer.id)}) that minted this bounty to fund it.
             </div>
           )}
 
