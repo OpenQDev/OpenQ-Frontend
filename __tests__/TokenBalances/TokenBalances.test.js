ee35052faa8a6452ec5803aeb553769770388fae
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-03-10 13:16:54 +0000

    refactored TokenBalances component

--- __tests__/TokenBalances/TokenBalances.test.js
+++ __tests__/TokenBalances/TokenBalances.test.js
@@ -2,15 +2,24 @@
 /**
  * @jest-environment jsdom
  */
+
+/*
+
+                  lean={true}
+                  tokenBalances={payoutBalances}
+                  tokenValues={payoutValues}
+                  singleCurrency={true}
+                  small={true}
+                   tokenBalances={[deposit]} tokenValues={tokenValues} lean={true} singleCurrency={true}
+                   
+          <TokenBalances tokenBalances={tokenBalances} tokenValues={tokenValues} />
+          <TokenBalances lean={true} tokenBalances={deposit} tokenValues={tokenValues} singleCurrency={true} />
+*/
 import React from 'react';
 import { render, screen } from '../../test-utils';
 import TokenBalances from '../../components/TokenBalances/TokenBalances';
 // Test cases for full balances, empty balances, and undefined balances.
-const tokenValues = {
-  tokenPrices: { '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39': 0.67 },
-  tokens: { '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39': 8.040000000000001 },
-  total: 8.04,
-};
+
 const tokenBalances = [
   {
     __typename: 'BountyFundedTokenBalance',
@@ -20,10 +29,10 @@ const tokenBalances = [
 ];
 
 // Test cases for
-const test = (tokenBalances, tokenValues) => {
+const test = (tokenBalances) => {
   it('should render balances', async () => {
     // ARRANGE
-    render(<TokenBalances tokenBalances={tokenBalances} lean={true} singleCurrency={true} tokenValues={tokenValues} />);
+    render(<TokenBalances tokenBalances={tokenBalances} />);
 
     // ASSERT
     expect(await screen.findByText(/12.0/)).toBeInTheDocument();
@@ -34,5 +43,5 @@ const test = (tokenBalances, tokenValues) => {
 };
 
 describe('TokenBalances', () => {
-  test(tokenBalances, tokenValues);
+  test(tokenBalances);
 });
