7fa1cf41f88ad869ac926162aaa6dd1103a37fb0
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-03-13 13:15:27 +0000

    updated test

--- components/Search/SearchBar/SearchBar.test.js
+++ components/Search/SearchBar/SearchBar.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import SearchBar from '../../components/Search/SearchBar';
+import SearchBar from '.';
 import renderer from 'react-test-renderer';
 
 describe('SearchBar', () => {
