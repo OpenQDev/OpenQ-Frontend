bff7ed39caa14857d8f4ccbb28b93108848dab35
FlacoJones <andrew@openq.dev>
2023-03-20 13:58:22 -0500

    adds test for payoutsched

--- __tests__/Batch/batch.test.js
+++ __tests__/Batch/batch.test.js
@@ -1,5 +1,11 @@
+import { convertPayoutScheduleToBigInt } from '../../lib/batchUtils'
+
 describe('Batch Mint', () => {
-  it('should pass', () => {
-    expect(true).toEqual(true);
+  it('should pass', async () => {
+		const payoutScheduleString = '[375.0, 375.0, 375.0, 375.0, 750.0, 750.0, 750.0, 750.0, 833.33, 833.33, 833.33]'
+		
+		const convertedToBigInt = convertPayoutScheduleToBigInt(payoutScheduleString, 6)
+
+    expect(convertedToBigInt.toString()).toEqual('375000000,375000000,375000000,375000000,750000000,750000000,750000000,750000000,833330000,833330000,833330000');
   });
 });
