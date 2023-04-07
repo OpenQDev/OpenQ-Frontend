c65e7b17c81f51fbf36e88eabf8568cdb6449e8a
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-04-07 11:56:07 +0000

    added coinbase wallet

--- components/WalletConnect/connectors.js
+++ components/WalletConnect/connectors.js
@@ -1,6 +1,7 @@
 import { WalletConnect } from '@web3-react/walletconnect-v2';
 import { initializeConnector } from '@web3-react/core';
 import { MetaMask } from '@web3-react/metamask';
+import { CoinbaseWallet } from '@web3-react/coinbase-wallet';
 
 export const [walletConnect, walletConnectHooks] = initializeConnector(
   (actions) =>
@@ -17,3 +18,15 @@ function onError(error) {
   console.debug(`web3-react error: ${error}`);
 }
 export const [metaMask, metaMaskHooks] = initializeConnector((actions) => new MetaMask({ actions, onError }));
+
+export const [coinbaseWallet, coinbaseHooks] = initializeConnector(
+  (actions) =>
+    new CoinbaseWallet({
+      actions,
+      options: {
+        projectId: 'a6cc11517a10f6f12953fd67b1eb67e7',
+        chains: [137],
+        appName: 'OpenQ',
+      },
+    })
+);
