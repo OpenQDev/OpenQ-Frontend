df9c13a96d40605995ad564a1b3c01f1fbcfa3ed
Anya <75732239+ArcAnya@users.noreply.github.com>
2023-04-11 16:22:37 +0000

    add github info, token formatting, general design

--- components/Claim/ClaimsTracking/ClaimsPerBounty/IndividualClaim/index.js
+++ components/Claim/ClaimsTracking/ClaimsPerBounty/IndividualClaim/index.js
@@ -1,13 +1,20 @@
-import React /* , { useContext, useEffect, useState } */ from 'react';
-/* import StoreContext from '../../../../../store/Store/StoreContext'; */
+import React, { useContext, useEffect, useState } from 'react';
+import StoreContext from '../../../../../store/Store/StoreContext';
+import Link from 'next/link';
+import { ethers } from 'ethers';
 
 const IndividualClaim = ({ payout, bounty, index }) => {
-  /*  const appState = useContext(StoreContext);
-  const [githubUser, setGithubUser] = useState(undefined);
+  const appState = useContext(StoreContext);
+  const token = appState[0].tokenClient.getToken(bounty?.payoutTokenAddress);
+  const formattedToken = ethers.utils.formatUnits(
+    ethers.BigNumber.from(payout.toString()),
+    parseInt(token.decimals) || 18
+  );
+  const [githubUser, setGithubUser] = useState('');
   useEffect(() => {
     if (bounty.tierWinners[index]) {
       const getGithubUser = async () => {
-        const githubUser = await appState.githubRepository.fetchUserById(bounty.tierWinners[index]);
+        const githubUser = await appState[0].githubRepository.fetchUserById(bounty.tierWinners[index]);
         if (githubUser) setGithubUser(githubUser);
       };
       try {
@@ -17,13 +24,27 @@ const IndividualClaim = ({ payout, bounty, index }) => {
       }
     }
   }, [bounty]);
-  console.log(githubUser, 'githubUser', appState.githubRepository); */
+  console.log(bounty, token);
   return (
-    <div className='flex items-center gap-4'>
-      <div className='text-sm'>TierWinner: {bounty.tierWinners[index] || 'Not Yet Assigned'}</div>
-      <div className='text-sm'>Payout: {payout}</div>
-      <div className='text-sm'>W8/W9: {bounty.supportingDocumentsCompleted?.[index]?.toString().toUpperCase()}</div>
-      <div className='text-sm'>Claimed: {bounty.claims?.some((claim) => claim.tier == index) ? 'TRUE' : 'FALSE'}</div>
+    <div className='items-center gap-4 grid grid-cols-[2fr_1fr_1fr_1fr]'>
+      {githubUser?.url ? (
+        <Link href={githubUser?.url} target='_blank' className=' text-link-colour hover:underline '>
+          {githubUser.id}
+        </Link>
+      ) : (
+        <div className=' text-red-400'> Not Yet Assigned</div>
+      )}
+      <div className='flex justify-center'>
+        {formattedToken} {token.symbol}
+      </div>
+      <div className={`flex justify-center ${bounty.supportingDocumentsCompleted?.[index] ? '' : 'text-red-400'}`}>
+        {bounty.supportingDocumentsCompleted?.[index]?.toString().toUpperCase()}
+      </div>
+      <div
+        className={`flex justify-center ${bounty.claims?.some((claim) => claim.tier == index) ? '' : 'text-red-400'}`}
+      >
+        {bounty.claims?.some((claim) => claim.tier == index) ? 'TRUE' : 'FALSE'}
+      </div>
     </div>
   );
 };
--- components/Claim/ClaimsTracking/ClaimsPerBounty/index.js
+++ components/Claim/ClaimsTracking/ClaimsPerBounty/index.js
@@ -10,15 +10,21 @@ const ClaimsPerBounty = ({ item }) => {
       <div className='flex items-center gap-4 mb-2'>
         {item.alternativeName && (
           <div className='break-word text-xl inline gap-1 pb-1'>
-            <span className='whitespace-nowrap  text-link-colour'>{item.alternativeName}</span>
+            <span className='whitespace-nowrap  text-link-colour hover:underline'>{item.alternativeName}</span>
           </div>
         )}
         <Link href={`/contract/${item?.bountyId}/${item?.bountyAddress}`} target='_blank'>
-          <div className='font-bold text-lg'>{item?.title || ''}</div>
+          <div className='font-bold text-lg text-link-colour hover:underline'>{item?.title || ''}</div>
         </Link>
         <Link href={item?.url} target='_blank'>
           <Github />
         </Link>
+      </div>
+      <div className='items-center gap-4 grid grid-cols-[2fr_1fr_1fr_1fr] border-b border-web-gray pb-2 mb-2 font-bold'>
+        <div className=''>TierWinner</div>
+        <div className='flex justify-center'>PayoutSchedule</div>
+        <div className='flex justify-center'>W8/W9 Completed</div>
+        <div className='flex justify-center'>Claimed</div>
       </div>
       {item.payoutSchedule?.map((payout, index) => {
         return (
