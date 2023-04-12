c05c993824614241d62d1f7bba1341b34d99efec
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-04-12 12:56:24 +0000

    modified test globbing patter

--- components/Admin/Users/index.jsx
+++ components/Admin/Users/index.jsx
@@ -38,7 +38,6 @@ const Users = () => {
           </thead>
           <tbody>
             {users.map((user, index) => {
-              console.log(user.createdA, 'created atet');
               const dateCreated = user.createdAt
                 ? new Date(parseInt(user.createdAt)).toString().split(' ').slice(0, 4).join(' ')
                 : null;
