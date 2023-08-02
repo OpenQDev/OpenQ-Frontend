a84c11fc6273fdfc51d96b092463bfd60bcf65cd
FlacoJones <andrew@openq.dev>
2023-08-02 17:29:40 -0400

    updates

--- components/Sales/drm/devrel-hero.jsx
+++ components/Sales/drm/devrel-hero.jsx
@@ -19,10 +19,10 @@ const DevRelHero = () => {
           <div className='flex flex-col justify-center items-center lg:text-center px-8'>
             <div className='text-gray-400 text-sm font-normal'>Developer Relationship Management Platform</div>
             <Heading className='2xl:text-[48px]  pt-3 2xl:w-[50rem]'>
-              Supercharge developer relations without manual work
+              Supercharge developer relations
             </Heading>
             <div className='text-gray-800 text-2xl 2xl:text-2xl pt-5 2xl:w-[48rem]'>
-              OpenQ's DRM Platform gives your DevRel team direct access to every beat in the pulse of your developer
+              OpenQ's DRM Platform grants your DevRel team direct access to every beat in the pulse of your developer
               ecosystem
             </div>
           </div>
