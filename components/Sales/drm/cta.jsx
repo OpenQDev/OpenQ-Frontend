9e24d99ef285af0123674ef0cedc99f8c16906d4
FlacoJones <andrew@openq.dev>
2023-08-02 18:13:08 -0400

    updates

--- components/Sales/drm/cta.jsx
+++ components/Sales/drm/cta.jsx
@@ -8,10 +8,13 @@ const Cta = () => {
   return (
     <div>
       <div className='flex flex-col bg-white justify-center items-center lg:text-center  px-8'>
-        <Heading className=' pt-3 max-w-[80rem]'>Built by DevRels for DevRels</Heading>
+        <Heading className=' pt-3 max-w-[80rem]'>👷 Built by DevRels for DevRels</Heading>
         <Paragraph className='pt-8 max-w-[48rem]'>
-          The OpenQ DRM is shaped by valuable feedback from over a hundred companies. <br/><br/><br/><br/>Join our closed beta waitlist and
-          embrace the future of dev rel.
+          The OpenQ DRM was shaped by valuable feedback from over a hundred companies.<br />
+          <br />
+          <br />
+          <br />
+          Join our closed beta waitlist and embrace the future of dev rel.
         </Paragraph>
         <div className='flex flex-col sm:flex-row justify-center items center gap-8 md:gap-x-10 pt-16'>
           <Typeform>
