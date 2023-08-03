348b41e88a0007cdcf79491fec1da860183bea40
FlacoJones <andrew@openq.dev>
2023-08-03 15:46:40 -0400

    adds twitter title and such

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -26,9 +26,12 @@ const Blog = ({ latestBlogPosts }) => {
   return (
     <>
       <Head>
+        <meta name='twitter:site' content='@openqlabs' />
+        <meta name='twitter:title' content='By DevRels for DevRels' />
+        <meta name='twitter:description' content="CRMs just aren't cutting it for developer ecosytems" />
         <meta property='twitter:card' content='summary_large_image' />
-        <meta property='og:image' content={'/landingpage/home/blog-image.png'} />
         <meta property='twitter:image' content={'/landingpage/home/blog-image.png'} />
+        <meta property='og:image' content={'/landingpage/home/blog-image.png'} />
       </Head>
       <div className={`w-full`}>
         <div
